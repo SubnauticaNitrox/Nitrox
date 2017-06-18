699fc02270eca25a69f6aa11c1151938c72e5fae
Dave Carruth <dave@balancedcms.com>
2017-06-17 23:40:35 -0700

    * Migrating chunk logic into its own file

---
+++ Client/LoadedChunks.cs
@@ -0,0 +1,37 @@
+﻿using NitroxModel.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NitroxClient
+{
+    public class LoadedChunks
+    {
+        private static int CHUNK_SIZE = 16;
+
+        private HashSet<Int3> chunks = new HashSet<Int3>();
+
+        public void AddChunk(Int3 position)
+        {
+            chunks.Add(position);
+        }
+
+        public void RemoveChunk(Int3 position)
+        {
+            chunks.Remove(position);
+        }
+
+        public bool IsLoadedChunk(Int3 chunk)
+        {
+            return chunks.Contains(chunk);
+        }
+
+        public Int3 GetChunk(Vector3 position)
+        {
+            return new Int3((int)Math.Floor(position.X / CHUNK_SIZE) * CHUNK_SIZE,
+                            (int)Math.Floor(position.Y / CHUNK_SIZE) * CHUNK_SIZE,
+                            (int)Math.Floor(position.Z / CHUNK_SIZE) * CHUNK_SIZE);
+        }
+    }
+}
--- ClientTester/Program.cs
+++ ClientTester/Program.cs
@@ -25,8 +25,8 @@ namespace ClientTester
             while (true)
             {
                 Console.ReadLine();
-                mplayer1.SendChatMessage("Get gud noob");
-               //  mplayer1.pickupItem(new Vector3(-50.6f, -4.8f, -38.0f), "Coral_reef_purple_mushrooms_01_04(Clone)", "AcidMushroom");
+               // mplayer1.SendChatMessage("Get gud noob");
+                 mplayer1.pickupItem(new Vector3(-50.6f, -4.8f, -38.0f), "Coral_reef_purple_mushrooms_01_04(Clone)", "AcidMushroom");
                 //mplayer1.dropItem("BaseFoundation", new Vector3(-55.25951f, -1.23748684f, -24.0218639f), new Vector3(0, 0, 0));
               // mplayer1.buildItem("BaseFoundation", new Vector3(-52f, -4.6f, -21.6f), new Quaternion(0, 1, 0, 0));
                // Console.ReadLine();
