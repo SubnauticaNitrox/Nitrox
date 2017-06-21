9a3807c956d31f2f1e1198e48692fc27178d1a45
Dave Carruth <dave@balancedcms.com>
2017-06-20 22:22:08 -0700

    Revert "* Standardizing packages to .net 3.5"
    This reverts commit c0671a1093ab375420aafe8fbc9fca1f775a88bb.

--- NitroxTest/Client/Communication/ChunkAwarePacketReceiverTest.cs
+++ NitroxTest/Client/Communication/ChunkAwarePacketReceiverTest.cs
@@ -4,6 +4,7 @@ using NitroxClient.Communication;
 using NitroxModel.Packets;
 using NitroxTest.Model;
 using System;
+using Moq;
 using NitroxClient.Map;
 using System.Collections.Generic;
 using NitroxModel.DataStructures;
