87308630fc2f69e3d3326d0c0b0cfb72175a0c06
Dave Carruth <dave@balancedcms.com>
2017-07-06 20:42:56 -0700

    * Refactored managed objects into a centralized MultiplayerObjectManager.  This class will keep track of game objects and their GUIDs. * Added basic framework for tech type fabrication in constructors.

--- NitroxClient/Communication/Packets/Processors/VehicleMovementProcessor.cs
+++ NitroxClient/Communication/Packets/Processors/VehicleMovementProcessor.cs
@@ -1,5 +1,7 @@
 ﻿using NitroxClient.Communication.Packets.Processors.Base;
 using NitroxClient.GameLogic;
+using NitroxClient.GameLogic.ManagedObjects;
+using NitroxClient.MonoBehaviours;
 using NitroxModel.DataStructures.Util;
 using NitroxModel.Packets;
 using System;
@@ -12,19 +14,73 @@ namespace NitroxClient.Communication.Packets.Processors
 {
     public class VehicleMovementProcessor : GenericPacketProcessor<VehicleMovement>
     {
-        private VehicleGameObjectManager vehicleGameObjectManager;
+        private MultiplayerObjectManager multiplayerObjectManager;
         private PlayerGameObjectManager playerGameObjectManager;
 
-        public VehicleMovementProcessor(VehicleGameObjectManager vehicleGameObjectManager, PlayerGameObjectManager playerGameObjectManager)
+        public VehicleMovementProcessor(MultiplayerObjectManager multiplayerObjectManager, PlayerGameObjectManager playerGameObjectManager)
         {
-            this.vehicleGameObjectManager = vehicleGameObjectManager;
+            this.multiplayerObjectManager = multiplayerObjectManager;
             this.playerGameObjectManager = playerGameObjectManager;
         }
 
         public override void Process(VehicleMovement vehicleMovement)
         {
             playerGameObjectManager.HidePlayerGameObject(vehicleMovement.PlayerId);
-            vehicleGameObjectManager.UpdateVehiclePosition(vehicleMovement.Guid, vehicleMovement.TechType, ApiHelper.Vector3(vehicleMovement.PlayerPosition), ApiHelper.Quaternion(vehicleMovement.Rotation));
-        }        
+
+            Optional<GameObject> opGameObject = multiplayerObjectManager.GetManagedObject(vehicleMovement.Guid);
+
+            if(opGameObject.IsEmpty())
+            {
+                opGameObject = createVehicle(vehicleMovement.TechType, vehicleMovement.Guid);
+
+                if(opGameObject.IsEmpty())
+                {
+                    return;
+                }
+
+                multiplayerObjectManager.SetupManagedObject(vehicleMovement.Guid, opGameObject.Get());
+            }
+
+            GameObject gameObject = opGameObject.Get();
+            gameObject.transform.position = ApiHelper.Vector3(vehicleMovement.PlayerPosition);
+            gameObject.transform.rotation = ApiHelper.Quaternion(vehicleMovement.Rotation);            
+        }
+
+        private Optional<GameObject> createVehicle(String techTypeString, String guid)
+        {
+            Optional<TechType> opTechType = ApiHelper.TechType(techTypeString);
+            
+            if (opTechType.IsEmpty())
+            {
+                Console.WriteLine("Unknown tech type: " + techTypeString);
+                return Optional<GameObject>.Empty();
+            }
+
+            TechType techType = opTechType.Get();
+
+            GameObject techPrefab = TechTree.main.GetGamePrefab(techType);
+
+            if (techPrefab != null)
+            {
+                GameObject gameObject = (GameObject)UnityEngine.Object.Instantiate(techPrefab, Vector3.zero, Quaternion.FromToRotation(Vector3.up, Vector3.up));
+                gameObject.SetActive(true);
+                CrafterLogic.NotifyCraftEnd(gameObject, techType);
+                gameObject.SendMessage("StartConstruction", SendMessageOptions.DontRequireReceiver);
+
+                Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+                rigidBody.isKinematic = false;
+
+                ManagedMultiplayerObject managedObject = gameObject.AddComponent<ManagedMultiplayerObject>();
+                managedObject.ChangeGuid(guid);
+
+                return Optional<GameObject>.Of(gameObject);
+            }
+            else
+            {
+                Console.WriteLine("No prefab for tech type: " + techType);
+            }
+
+            return Optional<GameObject>.Empty();
+        }
     }
 }
---
+++ NitroxClient/GameLogic/MultiplayerObjectManager.cs
@@ -0,0 +1,38 @@
+﻿using NitroxClient.MonoBehaviours;
+using NitroxModel.DataStructures.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace NitroxClient.GameLogic.ManagedObjects
+{
+    public class MultiplayerObjectManager
+    {
+        private Dictionary<String, GameObject> objectsByGuid = new Dictionary<String, GameObject>();
+
+        public Optional<GameObject> GetManagedObject(String guid)
+        {
+            if (objectsByGuid.ContainsKey(guid))
+            {
+                return Optional<GameObject>.Of(objectsByGuid[guid]);
+            }
+            
+            return Optional<GameObject>.Empty();
+        }
+
+        public void SetupManagedObject(String guid, GameObject gameObject)
+        {
+            ManagedMultiplayerObject managedObject = gameObject.GetComponent<ManagedMultiplayerObject>();
+
+            if(managedObject == null)
+            {
+                managedObject = gameObject.AddComponent<ManagedMultiplayerObject>();
+                managedObject.ChangeGuid(guid);
+            }
+
+            objectsByGuid.Add(guid, gameObject);
+        }
+    }
+}
