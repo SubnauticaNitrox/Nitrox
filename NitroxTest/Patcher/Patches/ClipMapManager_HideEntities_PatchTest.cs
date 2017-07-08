35dea9db9d9bfe3e5d9bc047875efebe8a8667ff
Marijn <marijns95@gmail.com>
2017-07-08 17:23:59 +0200

    Fix NRE in HideEntities patch.

--- NitroxTest/Patcher/Patches/ClipMapManager_HideEntities_PatchTest.cs
+++ NitroxTest/Patcher/Patches/ClipMapManager_HideEntities_PatchTest.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NitroxPatcher.Patches;
 using Harmony;
-using System.Reflection.Emit;
-using System.Reflection;
-using Harmony.ILCopying;
-using NitroxModel.Helper;
-using NitroxPatcher;
 using NitroxTest.Patcher.Test;
 
 namespace NitroxTest.Patcher.Patches
@@ -17,13 +9,15 @@ namespace NitroxTest.Patcher.Patches
     [TestClass]
     public class ClipMapManager_HideEntities_PatchTest
     {
+        // TODO: Pass a valid ILGenerator instead of null.
+
         [TestMethod]
         public void Sanity()
         {
             List<CodeInstruction> instructions = PatchTestHelper.GenerateDummyInstructions(100);
             instructions.Add(new CodeInstruction(ClipMapManager_HideEntities_Patch.INJECTION_OPCODE, null));
 
-            IEnumerable<CodeInstruction> result = ClipMapManager_HideEntities_Patch.Transpiler(null, instructions);
+            IEnumerable<CodeInstruction> result = ClipMapManager_HideEntities_Patch.Transpiler(null, null, instructions);
             Assert.AreEqual(106, PatchTestHelper.GetInstructionCount(result));
         }
 
@@ -32,7 +26,7 @@ namespace NitroxTest.Patcher.Patches
         {
             List<CodeInstruction> beforeInstructions = PatchTestHelper.GetInstructionsFromMethod(ClipMapManager_HideEntities_Patch.TARGET_METHOD);
 
-            IEnumerable<CodeInstruction> result = ClipMapManager_HideEntities_Patch.Transpiler(ClipMapManager_HideEntities_Patch.TARGET_METHOD, beforeInstructions);
+            IEnumerable<CodeInstruction> result = ClipMapManager_HideEntities_Patch.Transpiler(ClipMapManager_HideEntities_Patch.TARGET_METHOD, null, beforeInstructions);
 
             Assert.IsTrue(beforeInstructions.Count < PatchTestHelper.GetInstructionCount(result));
         }
