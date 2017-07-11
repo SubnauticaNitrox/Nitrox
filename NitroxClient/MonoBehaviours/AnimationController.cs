8b0e90d1651ae266927a3a940f15d04563216973
Karsen Beck <karsen.beck@gmail.com>
2017-07-10 23:11:00 -0500

    Movement animations

--- NitroxClient/MonoBehaviours/AnimationController.cs
+++ NitroxClient/MonoBehaviours/AnimationController.cs
@@ -8,12 +8,34 @@ namespace NitroxClient.MonoBehaviours
 {
     public class AnimationController : MonoBehaviour
     {
+        public static readonly float SMOOTHING_SPEED = 0.95f;
         Animator animator;
+
+        public void Start()
+        {
+            animator = gameObject.GetComponent<Animator>();
+        }
 
         public void Update()
         {
-            animator = gameObject.GetComponent<Animator>();
             animator.SetBool("is_underwater", true);
+        }
+
+        Vector3 lastPosition = Vector3.zero;
+        Vector3 velocity = Vector3.zero;
+        float speed;
+        public void FixedUpdate()
+        {
+            velocity += (transform.position - lastPosition) * 2f;
+            speed = velocity.magnitude;
+            lastPosition = transform.position;
+
+            animator.SetFloat("move_speed", speed);
+            animator.SetFloat("move_speed_x", velocity.x);
+            animator.SetFloat("move_speed_y", velocity.y);
+            animator.SetFloat("move_speed_z", velocity.z);
+
+            velocity *= SMOOTHING_SPEED;
         }
     }
 }
