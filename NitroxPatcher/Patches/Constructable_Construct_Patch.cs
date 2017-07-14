6ed9506d00ba379fbee585079b5081298cac740f
Sunrunner37 <Sunrunner@nitroxmod.com>
2017-07-13 20:24:23 -0700

    * Fixed injection bug inside constructable_constuct code. * Re-enabling construction code paths and unit tests

--- NitroxPatcher/Patches/Constructable_Construct_Patch.cs
+++ NitroxPatcher/Patches/Constructable_Construct_Patch.cs
@@ -34,18 +34,17 @@ namespace NitroxPatcher.Patches
                 {
                     /*
                      * Multiplayer.PacketSender.ChangeConstructionAmount(base.gameObject.transform.position, this.constructedAmount, resourceID, resourceID2);
-                     */
-                     /*
+                     */                     
                     yield return new ValidatedCodeInstruction(OpCodes.Ldsfld, typeof(Multiplayer).GetField("PacketSender", BindingFlags.Static | BindingFlags.Public));
                     yield return new ValidatedCodeInstruction(OpCodes.Ldarg_0);
-                    yield return new ValidatedCodeInstruction(OpCodes.Call, typeof(GameObject).GetMethod("get_gameObject", BindingFlags.Public | BindingFlags.Instance));
+                    yield return new ValidatedCodeInstruction(OpCodes.Call, typeof(Component).GetMethod("get_gameObject", BindingFlags.Public | BindingFlags.Instance));
                     yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(GameObject).GetMethod("get_transform"));
                     yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(Transform).GetMethod("get_position"));
                     yield return new ValidatedCodeInstruction(OpCodes.Ldarg_0);
-                    yield return new ValidatedCodeInstruction(OpCodes.Ldsfld, typeof(Constructable).GetField("constructedAmount", BindingFlags.Public | BindingFlags.Instance));
+                    yield return new ValidatedCodeInstruction(OpCodes.Ldfld, typeof(Constructable).GetField("constructedAmount", BindingFlags.Public | BindingFlags.Instance));
                     yield return new ValidatedCodeInstruction(OpCodes.Ldloc_1);
                     yield return new ValidatedCodeInstruction(OpCodes.Ldloc_2);
-                    yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(PacketSender).GetMethod("ChangeConstructionAmount")); */                 
+                    yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(PacketSender).GetMethod("ChangeConstructionAmount"));              
                 }
             }
         }
