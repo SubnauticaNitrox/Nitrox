33353c02688516ae8bd0c21592fc438fe92356b5
Karsen Beck <karsen.beck@gmail.com>
2017-07-19 01:57:44 -0500

    Username selection

--- NitroxClient/MonoBehaviours/MultiplayerButton.cs
+++ NitroxClient/MonoBehaviours/MultiplayerButton.cs
@@ -191,6 +191,8 @@ namespace NitroxClient.MonoBehaviours
     public class JoinServerScript : MonoBehaviour
     {
         public string serverIp = "";
+        string username = "username";
+        bool showingUsername = false;
         public void Awake()
         {
             DontDestroyOnLoad(gameObject);
@@ -198,7 +200,21 @@ namespace NitroxClient.MonoBehaviours
 
         public void Start()
         {
-            StartCoroutine(JoinServerWait(serverIp));
+            showingUsername = true;
+        }
+
+        public void OnGUI()
+        {
+            if (!showingUsername)
+            {
+                return;
+            }
+            username = GUI.TextField(new Rect(Screen.width / 2 - 250, Screen.height / 2 - 25, 500, 50), username);
+            if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height / 2 + 25, 500, 50), "Add server"))
+            {
+                StartCoroutine(JoinServerWait(serverIp));
+                showingUsername = false;
+            }
         }
         
         public IEnumerator JoinServerWait(string serverIp)
@@ -209,9 +225,10 @@ namespace NitroxClient.MonoBehaviours
             yield return new WaitUntil(() => LargeWorldStreamer.main != null);
             yield return new WaitUntil(() => LargeWorldStreamer.main.IsReady() || LargeWorldStreamer.main.IsWorldSettled());
             yield return new WaitUntil(() => !PAXTerrainController.main.isWorking);
-            Multiplayer.PacketSender.PlayerId = "Player" + UnityEngine.Random.Range(100f,999f);
+            Multiplayer.PacketSender.PlayerId = username;
             Multiplayer.main.StartMultiplayer(serverIp);
             Multiplayer.main.InitMonoBehaviours();
+            Destroy(gameObject);
         }
     }
 }
