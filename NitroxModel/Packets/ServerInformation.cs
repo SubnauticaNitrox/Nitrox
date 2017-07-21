9de60788bcee2628b6f36351349715b101842f79
Dave Carruth <dave@balancedcms.com>
2017-07-20 18:13:29 -0700

    * Commit missing file - will be used for future enhancements.

---
+++ NitroxModel/Packets/ServerInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class ServerInformation : Packet
+    {
+        public String Name { get; private set; }
+        public int CurrentPlayers { get; private set; }
+        public int MaxPlayers { get; private set; }
+        public bool PasswordProtected { get; private set; }
+
+        public ServerInformation() : base()
+        {
+
+        }
+    }
+}
