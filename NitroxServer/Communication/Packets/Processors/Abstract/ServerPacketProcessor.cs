83f8ede598b112d1a1dab3e96908851fe414cd77
Dave Carruth <dave@balancedcms.com>
2017-07-22 11:46:50 -0700

    * Refactored entire server to allow packet processors similar to the nitrox client   - Packets will always go to a default processor if one can not be found (it contains the old packet logging code)

---
+++ NitroxServer/Communication/Packets/Processors/Abstract/ServerPacketProcessor.cs
@@ -0,0 +1,9 @@
+﻿using NitroxModel.Packets;
+
+namespace NitroxServer.Communication.Packets.Processors.Abstract
+{
+    public abstract class ServerPacketProcessor
+    {
+        public abstract void ProcessPacket(Packet packet, Player player);
+    }
+}
