dddebe2932d92b7b0a04221a73b2211e77493b8c
Dave Carruth <dave@balancedcms.com>
2017-07-22 15:25:08 -0700

    * Fixed movement regression

--- NitroxServer/Communication/Packets/Processors/MovementPacketProcessor.cs
+++ NitroxServer/Communication/Packets/Processors/MovementPacketProcessor.cs
@@ -6,9 +6,17 @@ namespace NitroxServer.Communication.Packets.Processors
 {
     class MovementPacketProcessor : AuthenticatedPacketProcessor<Movement>
     {
+        private TcpServer tcpServer;
+
+        public MovementPacketProcessor(TcpServer tcpServer)
+        {
+            this.tcpServer = tcpServer;
+        }
+        
         public override void Process(Movement packet, Player player)
         {
-             player.Position = packet.PlayerPosition;
+            player.Position = packet.PlayerPosition;
+            tcpServer.SendPacketToOtherPlayers(packet, player);
         }
     }
 }
