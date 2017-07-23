821af381b90a7e2cea94cc033398332a0515bae6
Marijn <marijns95@gmail.com>
2017-07-24 01:06:33 +0200

    Refactor reloader to assembly-classes.
    - This allows for more fine-grained control per assembly needed for
    future ideas.

--- NitroxReloader/Reloader.cs
+++ NitroxReloader/Reloader.cs
@@ -18,24 +18,22 @@ namespace NitroxReloader
 
     public class Reloader
     {
-        private static readonly HashSet<string> assemblies = new HashSet<string>() {
+        private static readonly HashSet<string> assemblyWhitelist = new HashSet<string>() {
             "NitroxModel.dll",
             "NitroxClient.dll",
             "NitroxPatcher.dll",
         };
 
-        // TODO: Store methods per assembly (and then only reload assemblies if the initial one had at least one ReloadableMethod)
-        // TODO: Use a class, in these more properties can be defined, for instance to patch every single friggen method, see how that works out.
-        // Most notably test properties (maybe this breaks with autoproperties).
+        // TODO: Reloadable attribute for classes? (To define all methods in there to be reloadable)
         // TODO: See if method-size is an easy thing. If so, code could be copied but is slower.
         // TODO: Figure out assembly unloading.
         // TODO: Add reloader to server as well (only thing to reload so far is NitroxModel though)
 
-        private Dictionary<string, MethodInfo> reloadableMethods;
+        private Dictionary<string, ReloadableAssembly> reloadableAssemblies;
 
         public Reloader()
         {
-            reloadableMethods = AppDomain.CurrentDomain.GetAssemblies()
+            reloadableAssemblies = AppDomain.CurrentDomain.GetAssemblies()
             .Where(assembly =>
             {
                 string location;
@@ -48,23 +46,16 @@ namespace NitroxReloader
                 {
                     return false;
                 }
-                return assemblies.Contains(Path.GetFileName(location));
+                return assemblyWhitelist.Contains(Path.GetFileName(location));
             })
-            .SelectMany(assembly =>
+            .Select(assembly =>
             {
                 Console.WriteLine("Reloader: reading assembly " + assembly.FullName);
-                return assembly.GetTypes();
-            })
-            .SelectMany(type => type.GetMethods(allBindings))
-            .Where(IsMarkedReloadable)
-            .ToDictionary(method =>
-            {
-                var key = QualifiedName(method);
-                Console.WriteLine("Reloader: found reloadable method " + key);
-                return key;
-            }, null);
+                return new ReloadableAssembly(assembly);
+            }).Where(ra => ra.HasReloadableMethods)
+            .ToDictionary(ra => ra.AssemblyName, null);
 
-            // TODO: Change this.
+            // TODO: Change this. Probably by another BeforeBuild task.
             var managedFolder = @"D:\Users\marij\Documents\code\VSProjects\Nitrox\bin\Debug";
             //Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var watcher = new FileSystemWatcher()
@@ -79,8 +70,9 @@ namespace NitroxReloader
             FileSystemEventHandler handler = (s, e) =>
             {
                 var fn = Path.GetFileName(e.Name);
-                if (assemblies.Contains(fn))
-                    ReloadAssembly(e.FullPath);
+                ReloadableAssembly ra;
+                if (reloadableAssemblies.TryGetValue(fn, out ra))
+                    ra.Reload(e.FullPath);
             };
             watcher.Created += handler;
             watcher.Changed += handler;
@@ -88,66 +80,97 @@ namespace NitroxReloader
             Console.WriteLine("Reloader: set up to watch " + managedFolder);
         }
 
-        public void ReloadAssembly(string path)
+        private class ReloadableAssembly
         {
-            var assembly = Assembly.Load(File.ReadAllBytes(path));
-            foreach (var method in assembly.GetTypes()
-                .SelectMany(type => type.GetMethods(allBindings))
-                .Where(IsMarkedReloadable))
+            public Dictionary<string, MethodInfo> reloadableMethods;
+            public Assembly assembly;
+            public ReloadableAssembly(Assembly a)
             {
-                var key = QualifiedName(method);
-                Console.WriteLine("Reloader: patching " + key);
-                var originalMethod = reloadableMethods[key];
-                if (originalMethod != null)
+                assembly = a;
+                reloadableMethods = GetReloadableMethods(assembly)
+                .ToDictionary(method =>
                 {
-                    var originalCodeStart = GetMethodStart(originalMethod);
-                    var newCodeStart = GetMethodStart(method);
-                    if (originalCodeStart == newCodeStart)
-                        Console.WriteLine("Reloader: Methods are identical! (Not emitting jump, that causes an infinite loop)");
+                    var key = QualifiedName(method);
+                    Console.WriteLine("Reloader: found reloadable method " + key);
+                    return key;
+                }, null);
+            }
+
+            public void Reload(string newAssemblyLocation)
+            {
+                var newAssembly = Assembly.Load(File.ReadAllBytes(newAssemblyLocation));
+                foreach (var method in GetReloadableMethods(newAssembly))
+                {
+                    var key = QualifiedName(method);
+                    Console.WriteLine("Reloader: patching " + key);
+                    var originalMethod = reloadableMethods[key];
+                    if (originalMethod != null)
+                    {
+                        var originalCodeStart = GetMethodStart(originalMethod);
+                        var newCodeStart = GetMethodStart(method);
+                        if (originalCodeStart == newCodeStart)
+                            Console.WriteLine("Reloader: Methods are identical! (Not emitting jump, that causes an infinite loop)");
+                        else
+                            WriteJump(originalCodeStart, newCodeStart);
+                    }
                     else
-                        WriteJump(originalCodeStart, newCodeStart);
+                        Console.WriteLine("Reloader: Original method missing (did you add a method with ReloadableMethod attribute?)");
+                }
+            }
+
+            public bool HasReloadableMethods
+            {
+                get { return reloadableMethods.Count > 0; }
+            }
+
+            public string AssemblyName
+            {
+                get { return Path.GetFileName(assembly.Location); }
+            }
+
+            #region Utils
+            private static IEnumerable<MethodInfo> GetReloadableMethods(Assembly a)
+            {
+                return a.GetTypes()
+                    .SelectMany(type => type.GetMethods(allBindings))
+                    .Where(IsMarkedReloadable);
+            }
+            private static string QualifiedName(MethodBase method)
+            {
+                return method.DeclaringType.FullName + '.' + method.Name;
+            }
+
+            private static bool IsMarkedReloadable(MethodBase method)
+            {
+                return method.GetAttribute<ReloadableMethod>() != null;
+            }
+
+            private static IntPtr GetMethodStart(MethodBase method)
+            {
+                var handle = GetRuntimeMethodHandle(method);
+                RuntimeHelpers.PrepareMethod(handle);
+                return handle.GetFunctionPointer();
+            }
+
+            public static unsafe void WriteJump(IntPtr original, IntPtr destination)
+            {
+                Console.WriteLine("Reloader: Writing jump to {0} at {1}", destination, original);
+                bool x64 = IntPtr.Size == sizeof(long);
+#if true
+                if (x64)
+                {
+                    ushort* original_s = (ushort*)original;
+                    original_s[0] = 0xB848;
+                    *(ulong*)(original_s + 1) = (ulong)destination;
+                    original_s[5] = 0xE0FF;
                 }
                 else
-                    Console.WriteLine("Reloader: Original method missing (did you add a method with ReloadableMethod attribute?)");
-            }
-        }
-
-        public static string QualifiedName(MethodBase method)
-        {
-            return method.DeclaringType.FullName + '.' + method.Name;
-        }
-
-        public static bool IsMarkedReloadable(MethodBase method)
-        {
-            return method.GetAttribute<ReloadableMethod>() != null;
-        }
-
-        public static IntPtr GetMethodStart(MethodBase method)
-        {
-            var handle = GetRuntimeMethodHandle(method);
-            RuntimeHelpers.PrepareMethod(handle);
-            return handle.GetFunctionPointer();
-        }
-
-        public static unsafe void WriteJump(IntPtr original, IntPtr destination)
-        {
-            Console.WriteLine("Reloader: Writing jump to {0} at {1}", destination, original);
-            bool x64 = IntPtr.Size == sizeof(long);
-#if true
-            if (x64)
-            {
-                ushort* original_s = (ushort*)original;
-                original_s[0] = 0xB848;
-                *(ulong*)(original_s + 1) = (ulong)destination;
-                original_s[5] = 0xE0FF;
-            }
-            else
-            {
-                byte* original_b = (byte*)original;
-                original_b[0] = 0x68;
-                *(uint*)(original_b + 1) = (uint)destination;
-                original_b[5] = 0xC3;
-            }
+                {
+                    byte* original_b = (byte*)original;
+                    original_b[0] = 0x68;
+                    *(uint*)(original_b + 1) = (uint)destination;
+                    original_b[5] = 0xC3;
+                }
 #else
             // For some reason, this doesn't work...
             using (UnmanagedMemoryStream ums = new UnmanagedMemoryStream((byte*)original, x64 ? (4 + sizeof(long)) : (2 + sizeof(int))))
@@ -172,44 +195,45 @@ namespace NitroxReloader
                     bw.Write((byte)0xC3);
                 }
 #endif
-        }
-
-        private static RuntimeMethodHandle GetRuntimeMethodHandle(MethodBase method)
-        {
-            if (method is DynamicMethod)
+            }
+            private static RuntimeMethodHandle GetRuntimeMethodHandle(MethodBase method)
             {
-                Console.WriteLine($"Reloader: {method} Is a dynamic method!");
-                var nonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+                if (method is DynamicMethod)
+                {
+                    Console.WriteLine($"Reloader: {method} Is a dynamic method!");
+                    var nonPublicInstance = BindingFlags.NonPublic | BindingFlags.Instance;
 
-                // DynamicMethod actually generates its m_methodHandle on-the-fly and therefore
-                // we should call GetMethodDescriptor to force it to be created.
+                    // DynamicMethod actually generates its m_methodHandle on-the-fly and therefore
+                    // we should call GetMethodDescriptor to force it to be created.
 
-                var m_GetMethodDescriptor = typeof(DynamicMethod).GetMethod("GetMethodDescriptor", nonPublicInstance);
-                if (m_GetMethodDescriptor != null)
-                    return (RuntimeMethodHandle)m_GetMethodDescriptor.Invoke(method, new object[0]);
+                    var m_GetMethodDescriptor = typeof(DynamicMethod).GetMethod("GetMethodDescriptor", nonPublicInstance);
+                    if (m_GetMethodDescriptor != null)
+                        return (RuntimeMethodHandle)m_GetMethodDescriptor.Invoke(method, new object[0]);
 
-                // .Net Core
-                var f_m_method = typeof(DynamicMethod).GetField("m_method", nonPublicInstance);
-                if (f_m_method != null)
-                    return (RuntimeMethodHandle)f_m_method.GetValue(method);
+                    // .Net Core
+                    var f_m_method = typeof(DynamicMethod).GetField("m_method", nonPublicInstance);
+                    if (f_m_method != null)
+                        return (RuntimeMethodHandle)f_m_method.GetValue(method);
 
-                // Mono
-                var f_mhandle = typeof(DynamicMethod).GetField("mhandle", nonPublicInstance);
-                return (RuntimeMethodHandle)f_mhandle.GetValue(method);
+                    // Mono
+                    var f_mhandle = typeof(DynamicMethod).GetField("mhandle", nonPublicInstance);
+                    return (RuntimeMethodHandle)f_mhandle.GetValue(method);
+                }
+
+                return method.MethodHandle;
             }
 
-            return method.MethodHandle;
+            private const BindingFlags allBindings =
+                BindingFlags.Public
+                | BindingFlags.NonPublic
+                | BindingFlags.Instance
+                | BindingFlags.Static
+                | BindingFlags.GetField
+                | BindingFlags.SetField
+                | BindingFlags.GetProperty
+                | BindingFlags.SetProperty;
+            #endregion
         }
-
-        public const BindingFlags allBindings =
-            BindingFlags.Public
-            | BindingFlags.NonPublic
-            | BindingFlags.Instance
-            | BindingFlags.Static
-            | BindingFlags.GetField
-            | BindingFlags.SetField
-            | BindingFlags.GetProperty
-            | BindingFlags.SetProperty;
     }
 
     public static class Helper
