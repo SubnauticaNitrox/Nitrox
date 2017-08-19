2eeda3b12827feea7d992df166e1248bf9b74d8e
Dave Carruth <dave@balancedcms.com>
2017-08-18 19:54:08 -0700

    * Syncing 'Openable' open/close animations

---
+++ NitroxClient/Communication/Packets/Processors/OpenableStateChangedProcessor.cs
@@ -0,0 +1,44 @@
+﻿using NitroxClient.Communication.Packets.Processors.Abstract;
+using NitroxClient.GameLogic.Helper;
+using NitroxModel.DataStructures.Util;
+using NitroxModel.Packets;
+using System;
+using UnityEngine;
+
+namespace NitroxClient.Communication.Packets.Processors
+{
+    public class OpenableStateChangedProcessor : ClientPacketProcessor<OpenableStateChanged>
+    {
+        private PacketSender packetSender;
+
+        public OpenableStateChangedProcessor(PacketSender packetSender)
+        {
+            this.packetSender = packetSender;
+        }
+
+        public override void Process(OpenableStateChanged packet)
+        {
+            Optional<GameObject> opGameObject = GuidHelper.GetObjectFrom(packet.Guid);
+
+            if(opGameObject.IsPresent())
+            {
+                Openable openable = opGameObject.Get().GetComponent<Openable>();
+
+                if (openable != null)
+                {
+                    packetSender.AddSuppressedPacketType(typeof(OpenableStateChanged));
+                    openable.PlayOpenAnimation(packet.IsOpen, packet.Duration);
+                    packetSender.RemoveSuppressedPacketType(typeof(OpenableStateChanged));
+                } 
+                else
+                {
+                    Console.WriteLine("Gameobject did not have a corresponding openable to change state!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Could not find openable game object with guid: " + packet.Guid);
+            }
+        }
+    }
+}
---
+++ NitroxModel/Packets/OpenableStateChanged.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class OpenableStateChanged : AuthenticatedPacket
+    {
+        public String Guid { get; private set; }
+        public bool IsOpen { get; private set; }
+        public float Duration { get; private set; }
+
+        public OpenableStateChanged(String playerId, String guid, bool isOpen, float duration) : base(playerId)
+        {
+            this.Guid = guid;
+            this.IsOpen = isOpen;
+            this.Duration = duration;
+        }
+
+        public override string ToString()
+        {
+            return "[OpenableStateChanged - PlayerId: " + PlayerId + " Guid: " + Guid + " IsOpen: " + IsOpen + " Duration: " + Duration + "]";
+        }
+    }
+}
