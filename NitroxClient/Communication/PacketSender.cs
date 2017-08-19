af59ba991eb3efc7ba26f6d7539722ed71351c20
Dave Carruth <dave@balancedcms.com>
2017-08-18 20:06:24 -0700

    * Decoupled constructor business logic from packet sender

--- NitroxClient/Communication/PacketSender.cs
+++ NitroxClient/Communication/PacketSender.cs
@@ -47,28 +47,7 @@ namespace NitroxClient.Communication
 
             Send(movement);
         }
-
-        public void ConstructorBeginCrafting(GameObject constructor, TechType techType, float duration)
-        {
-            String constructorGuid = GuidHelper.GetGuid(constructor);
-
-            Console.WriteLine("Building item from constructor with uuid: " + constructorGuid);
-
-            Optional<object> opConstructedObject = TransientLocalObjectManager.Get(TransientObjectType.CONSTRUCTOR_INPUT_CRAFTED_GAMEOBJECT);
-
-            if (opConstructedObject.IsPresent())
-            {
-                GameObject constructedObject = (GameObject)opConstructedObject.Get();
-                String constructedObjectGuid = GuidHelper.GetGuid(constructedObject);
-                ConstructorBeginCrafting beginCrafting = new ConstructorBeginCrafting(PlayerId, constructorGuid, constructedObjectGuid, ApiHelper.TechType(techType), duration);
-                Send(beginCrafting);
-            }
-            else
-            {
-                Console.WriteLine("Could not send packet because there wasn't a corresponding constructed object!");
-            }
-        }
-
+        
         public void AnimationChange(AnimChangeType type, AnimChangeState state)
         {
             AnimationChangeEvent animEvent = new AnimationChangeEvent(PlayerId, (int)type, (int)state);
---
+++ NitroxClient/GameLogic/Constructor.cs
@@ -0,0 +1,41 @@
+﻿using NitroxClient.Communication;
+using NitroxClient.GameLogic.Helper;
+using NitroxModel.DataStructures.Util;
+using NitroxModel.Packets;
+using System;
+using UnityEngine;
+using static NitroxClient.GameLogic.Helper.TransientLocalObjectManager;
+
+namespace NitroxClient.GameLogic
+{
+    public class Constructor
+    {
+        private PacketSender packetSender;
+
+        public Constructor(PacketSender packetSender)
+        {
+            this.packetSender = packetSender;
+        }
+        
+        public void BeginCrafting(GameObject constructor, TechType techType, float duration)
+        {
+            String constructorGuid = GuidHelper.GetGuid(constructor);
+
+            Console.WriteLine("Building item from constructor with uuid: " + constructorGuid);
+
+            Optional<object> opConstructedObject = TransientLocalObjectManager.Get(TransientObjectType.CONSTRUCTOR_INPUT_CRAFTED_GAMEOBJECT);
+
+            if (opConstructedObject.IsPresent())
+            {
+                GameObject constructedObject = (GameObject)opConstructedObject.Get();
+                String constructedObjectGuid = GuidHelper.GetGuid(constructedObject);
+                ConstructorBeginCrafting beginCrafting = new ConstructorBeginCrafting(packetSender.PlayerId, constructorGuid, constructedObjectGuid, ApiHelper.TechType(techType), duration);
+                packetSender.Send(beginCrafting);
+            }
+            else
+            {
+                Console.WriteLine("Could not send packet because there wasn't a corresponding constructed object!");
+            }
+        }
+    }
+}
