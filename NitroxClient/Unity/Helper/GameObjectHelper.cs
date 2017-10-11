2afd5ad10b24edb51026ee2cd504d603c9587615
MarijnS95 <marijns95@gmail.com>
2017-10-11 23:05:48 +0200

    Cleaned GameObjectHelper (redundant function and generic type specifiers).

--- NitroxClient/Unity/Helper/GameObjectHelper.cs
+++ NitroxClient/Unity/Helper/GameObjectHelper.cs
@@ -8,23 +8,15 @@ namespace NitroxClient.Unity.Helper
         public static T RequireComponent<T>(this GameObject o) where T : class
         {
             T component = o.GetComponent<T>();
-            Validate.NotNull<T>(component, o.name + " did not have a component of type " + typeof(T));
+            Validate.NotNull(component, o.name + " did not have a component of type " + typeof(T));
 
             return component;
         }
 
-        public static T RequireComponentInChildren<T>(this GameObject o) where T : class
-        {
-            T component = o.GetComponentInChildren<T>();
-            Validate.NotNull<T>(component, o.name + " did not have a component of type " + typeof(T) + " in its children");
-
-            return component;
-        }
-
-        public static T RequireComponentInChildren<T>(this GameObject o, bool includeInactive) where T : class
+        public static T RequireComponentInChildren<T>(this GameObject o, bool includeInactive = false) where T : class
         {
             T component = o.GetComponentInChildren<T>(includeInactive);
-            Validate.NotNull<T>(component, o.name + " did not have a component of type " + typeof(T) + " in its children");
+            Validate.NotNull(component, o.name + " did not have a component of type " + typeof(T) + " in its children");
 
             return component;
         }
@@ -32,7 +24,7 @@ namespace NitroxClient.Unity.Helper
         public static T RequireComponentInParent<T>(this GameObject o) where T : class
         {
             T component = o.GetComponentInParent<T>();
-            Validate.NotNull<T>(component, o.name + " did not have a component of type " + typeof(T) + " in its parent");
+            Validate.NotNull(component, o.name + " did not have a component of type " + typeof(T) + " in its parent");
 
             return component;
         }
