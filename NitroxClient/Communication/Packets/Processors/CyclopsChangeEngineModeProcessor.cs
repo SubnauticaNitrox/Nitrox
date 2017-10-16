eebcfcad73d6c8badd357fdd4542887818f4ef65
Jannify <Kannst-Du-mir-folgen@gmx.de>
2017-10-17 00:45:30 +0200

    Merge Cyclops-MotorMode/State and -HUD to one branch

---
+++ NitroxClient/Communication/Packets/Processors/CyclopsChangeEngineModeProcessor.cs
@@ -0,0 +1,38 @@
+﻿using NitroxClient.Communication.Packets.Processors.Abstract;
+using NitroxClient.GameLogic.Helper;
+using NitroxClient.Unity.Helper;
+using NitroxModel.Helper;
+using NitroxModel.Packets;
+using UnityEngine;
+
+namespace NitroxClient.Communication.Packets.Processors
+{
+    public class CyclopsChangeEngineModeProcessor : ClientPacketProcessor<CyclopsChangeEngineMode>
+    {
+        private PacketSender packetSender;
+
+        public CyclopsChangeEngineModeProcessor(PacketSender packetSender)
+        {
+            this.packetSender = packetSender;
+        }
+
+        public override void Process(CyclopsChangeEngineMode motorPacket)
+        {
+            GameObject cyclops = GuidHelper.RequireObjectFrom(motorPacket.Guid);
+            CyclopsHelmHUDManager hud = cyclops.RequireComponentInChildren<CyclopsHelmHUDManager>();
+            CyclopsMotorMode motorMode = cyclops.RequireComponentInChildren<CyclopsMotorMode>();
+            CyclopsMotorModeButton[] motorModeButtons = cyclops.GetComponentsInChildren<CyclopsMotorModeButton>();
+
+            Validate.IsTrue(motorModeButtons.Length > 0, "Cyclops does not have any motormode buttons!");
+
+            if (motorPacket.Mode != motorMode.cyclopsMotorMode)
+            {
+                foreach (CyclopsMotorModeButton motorModeButton in motorModeButtons)
+                {
+                    motorModeButton.SetCyclopsMotorMode((CyclopsMotorMode.CyclopsMotorModes)motorPacket.Mode);
+                }
+
+            }
+        }
+    }
+}
---
+++ NitroxModel/Packets/CyclopsToggleEngineState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class CyclopsToggleEngineState : AuthenticatedPacket
+    {
+        public String Guid { get; }
+        public bool IsOn { get; }
+        public bool IsStarting { get; }
+
+        public CyclopsToggleEngineState(String playerId, String guid, bool isOn, bool isStarting) : base(playerId)
+        {
+            this.Guid = guid;
+            this.IsOn = isOn;
+            this.IsStarting = isStarting;
+        }
+
+        public override string ToString()
+        {
+            return "[CyclopsToggleEngineState PlayerId: " + PlayerId + " Guid: " + Guid + " IsOn: " + IsOn + " IsStarting: " + IsStarting + "]";
+        }
+    }
+}
