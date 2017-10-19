40c0898e04b2b704ad6011665eb4a42a288bff61
Sunrunner37 <sunrunner@nitroxmod.com>
2017-10-18 22:35:29 -0700

    * Spawning server side creatures on the client side. * Disabling all server side spawning * Some refactoring of batch cell parsing

---
+++ NitroxClient/Communication/Packets/Processors/SpawnEntitiesProcessor.cs
@@ -0,0 +1,35 @@
+﻿using NitroxClient.Communication.Packets.Processors.Abstract;
+using NitroxClient.GameLogic;
+using NitroxClient.GameLogic.Helper;
+using NitroxClient.GameLogic.HUD;
+using NitroxModel.GameLogic;
+using NitroxModel.Logger;
+using NitroxModel.Packets;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NitroxClient.Communication.Packets.Processors
+{
+    class SpawnEntitiesProcessor : ClientPacketProcessor<SpawnEntities>
+    {
+        HashSet<String> alreadySpawnedGuids = new HashSet<String>();
+        
+        public override void Process(SpawnEntities packet)
+        {
+            foreach(SpawnedEntity entity in packet.Entities)
+            {
+                if(!alreadySpawnedGuids.Contains(entity.Guid))
+                {
+                    GameObject techPrefab = CraftData.GetPrefabForTechType(entity.TechType);
+                    GameObject gameObject = (GameObject)UnityEngine.Object.Instantiate(techPrefab, entity.Position, Quaternion.FromToRotation(Vector3.up, Vector3.up));
+                    GuidHelper.SetNewGuid(gameObject, entity.Guid);
+                    gameObject.SetActive(true);
+
+                    alreadySpawnedGuids.Add(entity.Guid);
+                    Log.Info("Received spawned entity: " + entity.Guid + " at " + entity.Position);
+                }
+            }
+        }
+    }
+}
---
+++ NitroxPatcher/Patches/Persistent/CellManager_GetPrefabForSlot_Patch.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+
+namespace NitroxPatcher.Patches.Persistent
+{
+    class CellManager_GetPrefabForSlot_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(CellManager);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("GetPrefabForSlot", BindingFlags.Public | BindingFlags.Instance);
+
+        public static bool Prefix(IEntitySlot slot, out EntitySlot.Filler __result)
+        {
+            __result = default(EntitySlot.Filler);
+
+            // NOTE: We currently only disable spawning of creature but we can later extend this to all slot types to make the server the authority.
+
+            return (!slot.IsCreatureSlot());
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            this.PatchPrefix(harmony, TARGET_METHOD);
+        }
+    }
+}
