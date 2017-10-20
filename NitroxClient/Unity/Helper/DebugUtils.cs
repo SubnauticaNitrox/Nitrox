e6f41b554430bdd6175235c4160528852590e1ee
Jannify <Kannst-Du-mir-folgen@gmx.de>
2017-10-20 15:50:12 +0200

    Add new hierarchy analyzer to DebugUtils

--- NitroxClient/Unity/Helper/DebugUtils.cs
+++ NitroxClient/Unity/Helper/DebugUtils.cs
@@ -7,6 +7,46 @@ namespace NitroxClient.Unity.Helper
 {
     public class DebugUtils
     {
+        private static string prefix = "";
+
+        public static void DumpHierarchy(GameObject obj)
+        {
+            Log.Debug("Analyze gameobject: ");
+            Log.Debug("------------------------------------");
+            StartHierachy(obj);
+            Log.Debug("------------------------------------");
+        }
+
+        private static void StartHierachy(GameObject obj)
+        {
+            prefix = prefix + "    ";
+            Log.Debug(GetInfoOfGamobject(obj));
+            for (int i = 0; i < obj.transform.childCount; i++)
+            {
+                StartHierachy(obj.transform.GetChild(i).gameObject);
+                prefix = prefix.Substring(4);
+            }
+        }
+
+        private static string GetInfoOfGamobject(GameObject obj)
+        {
+            return prefix + obj.name + "   --<>--   " + GetComponentsOfGamobject(obj);
+        }
+
+        private static string GetComponentsOfGamobject(GameObject obj)
+        {
+            string list = "";
+            foreach (Component item in obj.GetComponents<Component>())
+            {
+                list += item.GetType() + " | ";
+            }
+            if (list.Length >= 3)
+            {
+                list = list.Substring(0, list.Length-3);
+            }
+            return list;
+        }
+
         public static void DumpGameObject(GameObject gameObject, string indent = "")
         {
             Log.Info("{0}+{1}", indent, gameObject.name);
