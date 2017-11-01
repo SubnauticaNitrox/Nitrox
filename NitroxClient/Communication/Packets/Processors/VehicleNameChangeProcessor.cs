058e27366a773bc454b1203aa466f747622a9ce6
MarijnS95 <marijns95@gmail.com>
2017-11-01 17:59:36 +0100

    Workaround for removed Proxy, color/name sync now works on vehicles.

---
+++ NitroxClient/Communication/Packets/Processors/VehicleNameChangeProcessor.cs
@@ -0,0 +1,29 @@
+﻿using NitroxClient.Communication.Packets.Processors.Abstract;
+using NitroxClient.GameLogic.Helper;
+using NitroxClient.Unity.Helper;
+using NitroxModel.Packets;
+using UnityEngine;
+
+namespace NitroxClient.Communication.Packets.Processors
+{
+    public class VehicleNameChangeProcessor : ClientPacketProcessor<VehicleNameChange>
+    {
+        private readonly PacketSender packetSender;
+
+        public VehicleNameChangeProcessor(PacketSender packetSender)
+        {
+            this.packetSender = packetSender;
+        }
+
+        public override void Process(VehicleNameChange namePacket)
+        {
+            GameObject target = GuidHelper.RequireObjectFrom(namePacket.Guid);
+            SubNameInput subNameInput = target.RequireComponentInChildren<SubNameInput>();
+
+            using (packetSender.Suppress<VehicleNameChange>())
+            {
+                subNameInput.OnNameChange(namePacket.Name);
+            }
+        }
+    }
+}
