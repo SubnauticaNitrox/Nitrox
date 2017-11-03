6fd9cf0b70e257d8152c751e3a8e3240a8c96cc2
Sunrunner37 <sunrunner@nitroxmod.com>
2017-11-02 22:17:37 -0700

    * Adding server code to reassign entity simulation when a player moves out of a cell and code that updates entitiy positions when re-entering a previous cell

--- NitroxServer/Communication/Packets/Processors/CellVisibilityChangedProcessor.cs
+++ NitroxServer/Communication/Packets/Processors/CellVisibilityChangedProcessor.cs
@@ -1,4 +1,5 @@
-﻿using NitroxModel.GameLogic;
+﻿using NitroxModel.DataStructures;
+using NitroxModel.GameLogic;
 using NitroxModel.Packets;
 using NitroxServer.Communication.Packets.Processors.Abstract;
 using NitroxServer.GameLogic;
@@ -10,10 +11,12 @@ namespace NitroxServer.Communication.Packets.Processors
     class CellVisibilityChangedProcessor : AuthenticatedPacketProcessor<CellVisibilityChanged>
     {
         private readonly EntityManager entityManager;
+        private readonly PlayerManager playerManager;
 
-        public CellVisibilityChangedProcessor(EntityManager entityManager)
-        {
+        public CellVisibilityChangedProcessor(EntityManager entityManager, PlayerManager playerManager)
+        {            
             this.entityManager = entityManager;
+            this.playerManager = playerManager;
         }
         
         public override void Process(CellVisibilityChanged packet, Player player)
@@ -21,16 +24,46 @@ namespace NitroxServer.Communication.Packets.Processors
             player.AddCells(packet.Added);
             player.RemoveCells(packet.Removed);
 
-            entityManager.AllowEntitySimulationFor(player.Id, packet.Added);
-            entityManager.RevokeEntitySimulationFor(player.Id, packet.Removed);
+            entityManager.AssignEntitySimulation(player.Id, packet.Added);
 
-            List<Entity> entities = entityManager.GetVisibleEntities(packet.Added);
-            
-            if (entities.Count > 0)
+            ReassignEntitySimulation(player, packet.Removed);
+            SendNewlyVisibleEntities(player, packet.Added);
+        }
+
+        private void SendNewlyVisibleEntities(Player player, VisibleCell[] visibleCells)
+        {
+            List<Entity> newlyVisibleEntities = entityManager.GetVisibleEntities(visibleCells);
+
+            if (newlyVisibleEntities.Count > 0)
             {
-                CellEntities cellEntities = new CellEntities(entities);
+                CellEntities cellEntities = new CellEntities(newlyVisibleEntities);
                 player.SendPacket(cellEntities);
-                Console.WriteLine(cellEntities);
+            }
+        }
+
+        private void ReassignEntitySimulation(Player sendingPlayer, VisibleCell[] removedCells)
+        {
+            List<Entity> revokedEntities = entityManager.RevokeEntitySimulationFor(sendingPlayer.Id, removedCells);
+            List<OwnedGuid> newOwnerships = new List<OwnedGuid>();
+
+            foreach (Entity entity in revokedEntities)
+            {
+                VisibleCell entityCell = new VisibleCell(entity.Position, entity.Level);
+
+                foreach (Player player in playerManager.GetPlayers())
+                {
+                    if (player != sendingPlayer && player.HasCellLoaded(entityCell))
+                    {
+                        Console.Write("player " + player.Id + " can take over " + entity.Guid);
+                        newOwnerships.Add(new OwnedGuid(entity.Guid, player.Id, true));
+                    }
+                }
+            }
+
+            if(newOwnerships.Count > 0)
+            {
+                SimulationOwnershipChange ownershipChange = new SimulationOwnershipChange(newOwnerships);
+                playerManager.SendPacketToAllPlayers(ownershipChange);
             }
         }
     }
