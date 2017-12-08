e93e193bd65a0a7c90fedd85805b672fa4f2f570
Karsen Beck <karsen.beck@gmail.com>
2017-12-07 22:58:14 -0600

    Basic story event syncing

---
+++ NitroxServer/Serialization/GenericNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NitroxServer.Serialization
+{
+    public class GenericNode
+    {
+        public List<GenericNode> children;
+        public string name;
+    }
+}
