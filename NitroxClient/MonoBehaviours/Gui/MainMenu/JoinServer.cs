487f9b713f6e18bfe6e737148bd62eb0a7a4c5a2
Anton van Dijk <measuring.infinity@gmail.com>
2018-01-21 00:42:14 +0100

    Improved join server menu again to match the add server menu.
    Signed-off-by: Anton van Dijk <measuring.infinity@gmail.com>

--- NitroxClient/MonoBehaviours/Gui/MainMenu/JoinServer.cs
+++ NitroxClient/MonoBehaviours/Gui/MainMenu/JoinServer.cs
@@ -7,9 +7,10 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
     public class JoinServer : MonoBehaviour
     {
         public string ServerIp = "";
-        private Rect joinServerWindowRect = new Rect(Screen.width / 2 - 250, 200, 500, 150);
+        Rect joinServerWindowRect = new Rect(Screen.width / 2 - 250, 200, 500, 150);
         string username = "username";
         bool showingUsername = false;
+        bool shouldFocus;
 
         public void Awake()
         {
@@ -19,6 +20,7 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
         public void Start()
         {
             showingUsername = true;
+            shouldFocus = true;
         }
 
         public void OnGUI()
@@ -28,6 +30,36 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
                 return;
             }
             joinServerWindowRect = GUILayout.Window(0, joinServerWindowRect, DoJoinServerWindow, "Join server");
+        }
+
+        public IEnumerator JoinServerWait(string serverIp)
+        {
+            IEnumerator startNewGame = (IEnumerator)uGUI_MainMenu.main.ReflectionCall("StartNewGame", false, false, GameMode.Survival);
+            StartCoroutine(startNewGame);
+            //Wait until game starts
+            yield return new WaitUntil(() => LargeWorldStreamer.main != null);
+            yield return new WaitUntil(() => LargeWorldStreamer.main.IsReady() || LargeWorldStreamer.main.IsWorldSettled());
+            yield return new WaitUntil(() => !PAXTerrainController.main.isWorking);
+            Multiplayer.Main.StartMultiplayer(serverIp, username);
+            Destroy(gameObject);
+        }
+
+        private void SetGUIStyle()
+        {
+            GUI.skin.textField.fontSize = 14;
+            GUI.skin.textField.richText = false;
+            GUI.skin.textField.alignment = TextAnchor.MiddleLeft;
+            GUI.skin.textField.wordWrap = true;
+            GUI.skin.textField.stretchHeight = true;
+            GUI.skin.textField.padding = new RectOffset(10, 10, 5, 5);
+
+            GUI.skin.label.fontSize = 14;
+            GUI.skin.label.alignment = TextAnchor.MiddleRight;
+            GUI.skin.label.stretchHeight = true;
+            GUI.skin.label.fixedWidth = 80; //change this when adding new labels that need more space.
+
+            GUI.skin.button.fontSize = 14;
+            GUI.skin.button.stretchHeight = true;
         }
 
         private void DoJoinServerWindow(int windowId)
@@ -47,31 +79,33 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
                 }
             }
 
-            using (GUILayout.VerticalScope v = new GUILayout.VerticalScope("Box", GUILayout.ExpandHeight(true)))
+            SetGUIStyle();
+            using (GUILayout.VerticalScope v = new GUILayout.VerticalScope("Box"))
             {
-                username = GUILayout.TextField(username, GUILayout.ExpandHeight(true));
-                if (GUILayout.Button("Join", GUILayout.ExpandHeight(true)))
+                using (GUILayout.HorizontalScope h = new GUILayout.HorizontalScope())
+                {
+                    GUILayout.Label("Username:");
+                    GUI.SetNextControlName("usernameField");
+                    username = GUILayout.TextField(username);
+                }
+
+                if (GUILayout.Button("Join"))
                 {
                     StartCoroutine(JoinServerWait(ServerIp));
                     showingUsername = false;
                 }
-                if (GUILayout.Button("Cancel", GUILayout.ExpandHeight(true)))
+
+                if (GUILayout.Button("Cancel"))
                 {
                     showingUsername = false;
                 }
             }
-        }
 
-        public IEnumerator JoinServerWait(string serverIp)
-        {
-            IEnumerator startNewGame = (IEnumerator)uGUI_MainMenu.main.ReflectionCall("StartNewGame", false, false, GameMode.Survival);
-            StartCoroutine(startNewGame);
-            //Wait until game starts
-            yield return new WaitUntil(() => LargeWorldStreamer.main != null);
-            yield return new WaitUntil(() => LargeWorldStreamer.main.IsReady() || LargeWorldStreamer.main.IsWorldSettled());
-            yield return new WaitUntil(() => !PAXTerrainController.main.isWorking);
-            Multiplayer.Main.StartMultiplayer(serverIp, username);
-            Destroy(gameObject);
+            if (shouldFocus)
+            {
+                GUI.FocusControl("usernameField");
+                shouldFocus = false;
+            }
         }
     }
 }
