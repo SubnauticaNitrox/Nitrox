947043548137ca74053fa63425d553825417c0d7
Ryan Hansen <hansen.ryan.a@gmail.com>
2018-01-28 14:52:32 -0600

    added two enumertion types that help describe the current state of a client bridge

---
+++ NitroxClient/Communication/ClientBridgeState.cs
@@ -0,0 +1,11 @@
+﻿namespace NitroxClient.Communication
+{
+    public enum ClientBridgeState
+    {
+        Disconnected,
+        WaitingForRerservation,
+        Failed,
+        Connected,
+        ReservationRejected
+    }
+}
---
+++ NitroxClient/Communication/ReservationRejectionReason.cs
@@ -0,0 +1,12 @@
+﻿namespace NitroxClient.Communication
+{
+    public enum ReservationRejectionReason
+    {
+        None,
+        PlayerNameInUse,
+
+        //These are all intended for future use. Maybe YAGNI, but this is where we should look to expand upon server reservations
+        ServerFull,
+        PasswordInvalid
+    }
+}
