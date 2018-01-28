27ffc69606dda3a47ce94e83d5f7c4d23a6c4d4b
Ryan Hansen <hansen.ryan.a@gmail.com>
2018-01-28 14:54:49 -0600

    a new packet that acts as a player's request to join a server

---
+++ NitroxModel/Packets/ReservePlayerSlot.cs
@@ -0,0 +1,14 @@
+﻿namespace NitroxModel.Packets
+{
+    public class ReservePlayerSlot : Packet
+    {
+        public string CorrelationId { get; private set; }
+        public string PlayerName { get; private set; }
+
+        public ReservePlayerSlot(string correlationId, string playerName)
+        {
+            CorrelationId = correlationId;
+            PlayerName = playerName;
+        }
+    }
+}
