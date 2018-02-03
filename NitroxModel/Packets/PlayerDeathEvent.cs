9e97e4f57841505510bfe340d843f3eb34d518b7
Enflorence <enflorence@hotmail.com>
2018-02-02 23:18:18 -0800

    Added player death notification (#32)
    * Added player death notification
    
    * Some refactoring of player death

---
+++ NitroxModel/Packets/PlayerDeathEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class PlayerDeathEvent : Packet
+    {
+        public string PlayerId { get; }
+        public Vector3 DeathPosition { get; }
+
+        public PlayerDeathEvent(string playerId, Vector3 deathPosition)
+        {
+            PlayerId = playerId;
+            DeathPosition = deathPosition;
+        }
+    }
+}
