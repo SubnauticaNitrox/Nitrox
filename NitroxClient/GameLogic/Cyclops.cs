8b3493810fd1437678e12068f0bd964108aa814e
RabidCrab <jrusse42@gmail.com>
2018-02-03 08:09:10 -0800

    Initial Commit

--- NitroxClient/GameLogic/Cyclops.cs
+++ NitroxClient/GameLogic/Cyclops.cs
@@ -1,5 +1,9 @@
-﻿using NitroxClient.Communication;
+﻿using System.Collections.Generic;
+using NitroxClient.Communication;
+using NitroxClient.GameLogic.Helper;
+using NitroxModel.Helper;
 using NitroxModel.Packets;
+using UnityEngine;
 
 namespace NitroxClient.GameLogic
 {
@@ -51,6 +55,49 @@ namespace NitroxClient.GameLogic
         public void ActivateShield(string guid)
         {
             CyclopsActivateShield packet = new CyclopsActivateShield(guid);
+            packetSender.Send(packet);
+        }
+
+        /// <summary>
+        /// Spawn a new Cyclops. <paramref name="subRoot"/> will not have its position or rotation declared upon spawning. You must pull those values from
+        /// elsewhere.
+        /// </summary>
+        public void SpawnNew(GameObject subRoot, Vector3 position, Quaternion rotation)
+        {
+            string guid = GuidHelper.GetGuid(subRoot.gameObject);
+            VehicleMovement packet = new VehicleMovement(packetSender.PlayerId, position, Vector3.zero, rotation, Vector3.zero, TechType.Cyclops, guid, 0, 0, false);
+            packetSender.Send(packet);
+        }
+
+        /// <summary>
+        /// Send a notice to the server to update the damage points on a Cyclops.
+        /// 
+        /// This method should only handle added/removed damage points, but until we can sync the damage points between clients by syncing the random number generators, we'll
+        /// have to get by with this. As a bonus, this is also used as a verification call. If an update packet is lost, the next one will fully sync the damage again.
+        /// </summary>
+        public void UpdateExternalDamagePoints(SubRoot subRoot)
+        {
+            string guid = GuidHelper.GetGuid(subRoot.gameObject);
+            Validate.NotNull(subRoot.damageManager, "Cyclops Guid: " + guid + " has a null 'CyclopsExternalDamageManager'!");
+
+            // We need all of the active damage point guids to fully sync the damage between computers. SubRoot.damageManager.CreatePoint() is going to create
+            // its own random points of damage on all clients as the Cyclops is damaged. When the "winning" damage point list is chosen by the server and redistributed,
+            // we will need a list of all of the points that should be active, otherwise each client will have a damage points randomly located.
+            List<int> damagePointIndexesList = new List<int>();
+            for (int i = 0; i < subRoot.damageManager.damagePoints.Length; i++)
+            {
+                if (subRoot.damageManager.damagePoints[i].gameObject.activeSelf)
+                {
+                    damagePointIndexesList.Add(i);
+                }
+            }
+
+            // We include the health because the "host" computer that called for an update on the damage points is now the one who dictates the overall
+            // state of the Cyclops. Damage points are the only way to repair a Cyclops, so reporting the health is only meant to satisfy the logic checks
+            // the client does to make sure there are the correct number of damage points for the Cyclop's current health. If there's too many or too few, it will 
+            // remove/add damage points. This made for an incredibly hard to track and irritating bug. It has to be the LiveMixin located in the DamageManager. The
+            // LiveMixin on SubRoot will not work.
+            CyclopsExternalDamage packet = new CyclopsExternalDamage(guid, subRoot.damageManager.subLiveMixin.health, damagePointIndexesList.ToArray());
             packetSender.Send(packet);
         }
     }
---
+++ NitroxPatcher/Patches/CyclopsExternalDamageManager_CreatePoint_Patch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using NitroxClient.MonoBehaviours;
+
+namespace NitroxPatcher.Patches
+{
+    /// <summary>
+    /// <see cref="CyclopsExternalDamageManager.CreatePoint()"/> is at the bottom of the execution list during the <see cref="CyclopsDamagePoint"/> creation.
+    /// <see cref="CyclopsExternalDamageManager.OnTakeDamage"/> is where the logic starts.
+    /// </summary>
+    class CyclopsExternalDamageManager_CreatePoint_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(CyclopsExternalDamageManager);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("CreatePoint", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Postfix(CyclopsExternalDamageManager __instance)
+        {
+            Multiplayer.Logic.Cyclops.UpdateExternalDamagePoints(__instance.GetComponentInParent<SubRoot>());
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPostfix(harmony, TARGET_METHOD);
+
+            // PatchTranspiler(harmony, TARGET_METHOD);
+        }
+
+        /*
+        public static readonly OpCode INJECTION_OPCODE = OpCodes.Stloc_0;
+
+        /// <summary>
+        /// When random number seeds are synced, we'll need to inject code after the index is generated. This injects just after the first line of code.
+        /// </summary>
+        public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+                yield return instruction;
+                
+                if (instruction.opcode.Equals(INJECTION_OPCODE))
+                {
+                    //Multiplayer.Logic.Cyclops.ToggleExternalDamagePoint(gameObject, true)
+
+                    yield return new ValidatedCodeInstruction(OpCodes.Ldsfld, typeof(Multiplayer).GetField("Logic", BindingFlags.Static | BindingFlags.Public));
+                    yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(Logic).GetMethod("get_Cyclops", BindingFlags.Instance | BindingFlags.Public));
+                    yield return new ValidatedCodeInstruction(OpCodes.Ldarg_0);
+                    yield return new ValidatedCodeInstruction(OpCodes.Ldfld, TARGET_CLASS.GetField("unusedDamagePoints", BindingFlags.Instance | BindingFlags.NonPublic));
+                    yield return new ValidatedCodeInstruction(OpCodes.Ldloc_0);
+                    yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(List<CyclopsDamagePoint>).GetMethod("get_Item", BindingFlags.Instance | BindingFlags.Public));
+                    yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(CyclopsDamagePoint).GetMethod("get_gameObject", BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static));
+                    yield return new ValidatedCodeInstruction(OpCodes.Ldc_I4_1);
+                    yield return new ValidatedCodeInstruction(OpCodes.Callvirt, typeof(Cyclops).GetMethod("ToggleExternalDamagePoint", BindingFlags.Public | BindingFlags.Instance));
+                }
+            }
+        }*/
+    }
+}
