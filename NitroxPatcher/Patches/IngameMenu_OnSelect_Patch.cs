2a856fdf523468cf8014709042df072b55176b9b
Duckuk <Duckuk@github.com>
2018-02-11 01:03:47 +1030

    Disabled pausing in multiplayer

---
+++ NitroxPatcher/Patches/IngameMenu_OnSelect_Patch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UWE;
+
+namespace NitroxPatcher.Patches
+{
+    public class IngameMenu_OnSelect_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(IngameMenu);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("OnSelect", BindingFlags.Public | BindingFlags.Instance);
+
+        public static void Postfix(IngameMenu __instance)
+        {
+            FreezeTime.End("IngameMenu");
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPostfix(harmony, TARGET_METHOD);
+        }
+    }
+}
