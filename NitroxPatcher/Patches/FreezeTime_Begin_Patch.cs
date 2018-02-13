a3e9bda511b7af509ed5bb2b7eefd00d67dfc838
Duckuk <Duckuk@github.com>
2018-02-13 13:13:18 +1030

    Moved patch over to FreezeTime.Begin (again)
    This time it actually works.

---
+++ NitroxPatcher/Patches/FreezeTime_Begin_Patch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UWE;
+
+namespace NitroxPatcher.Patches
+{
+    public class FreezeTime_Begin_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(FreezeTime);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("Begin", BindingFlags.Public | BindingFlags.Static);
+
+        public static bool Prefix(string userId, bool dontPauseSound)
+        {
+            return false;
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPrefix(harmony, TARGET_METHOD);
+        }
+    }
+}
