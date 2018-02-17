5e5261a5e55d752e5f25b17dd25ef34ecda6f935
MarijnS95 <marijns95@gmail.com>
2018-02-17 17:28:01 +0100

    Improved logging for regression testing.

--- NitroxModel/Extensions.cs
+++ NitroxModel/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 namespace NitroxModel
@@ -21,6 +23,17 @@ namespace NitroxModel
         public static string AsHexString(this Color32 color)
         {
             return $"#{color.r:X2}{color.g:X2}{color.b:X2}";
+        }
+
+        public static string PrefixWith<T>(this IEnumerable<T> items, string prefix)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T i in items)
+            {
+                sb.Append(prefix);
+                sb.Append(i);
+            }
+            return sb.ToString();
         }
     }
 }
