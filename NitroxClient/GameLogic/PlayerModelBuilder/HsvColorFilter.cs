52fd2f3f4f0b0a7dd1d23720af19a14412d4f8cf
MadMaxOfYore <madmaxthemighty@gmail.com>
2018-02-20 00:31:11 -0600

    corrected a bug in the HsvColorFilter that I introduced during a last minute check on code-style

--- NitroxClient/GameLogic/PlayerModelBuilder/HsvColorFilter.cs
+++ NitroxClient/GameLogic/PlayerModelBuilder/HsvColorFilter.cs
@@ -5,17 +5,22 @@ namespace NitroxClient.GameLogic.PlayerModelBuilder
 {
     public class HsvColorFilter
     {
-        private float targetHue;
-        private float targetSaturation;
-        private float targetVibrance;
-        public List<ColorValueRange> HueValueRanges => new List<ColorValueRange>();
-        public List<ColorValueRange> SaturationValueRanges => new List<ColorValueRange>();
-        public List<ColorValueRange> VibranceValueRanges => new List<ColorValueRange>();
+        private readonly float targetHue;
+        private readonly float targetSaturation;
+        private readonly float targetVibrance;
 
         public HsvColorFilter(Color targetColor)
         {
             Color.RGBToHSV(targetColor, out targetHue, out targetSaturation, out targetVibrance);
+
+            HueValueRanges = new List<ColorValueRange>();
+            SaturationValueRanges = new List<ColorValueRange>();
+            VibranceValueRanges = new List<ColorValueRange>();
         }
+
+        public List<ColorValueRange> HueValueRanges { get; }
+        public List<ColorValueRange> SaturationValueRanges { get; }
+        public List<ColorValueRange> VibranceValueRanges { get; }
 
         public Color FilterColor(Color color)
         {
