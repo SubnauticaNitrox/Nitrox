549a35d63d3425951d72029d47378079946ad427
ahk1221 <ahmed16.ahk@gmail.com>
2018-06-22 20:59:45 +0500

    Fixed movememt not working properly

---
+++ NitroxClient/MonoBehaviours/MovementSyncer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace NitroxClient.MonoBehaviours
+{
+    public class MovementSyncer : MonoBehaviour
+    {
+        public Vector3 NextPosition { get; private set; }
+        public Quaternion NextRotation { get; private set; }
+
+        public void SetNewLocation(Vector3 newPos, Quaternion newRot)
+        {
+            NextPosition = newPos;
+            NextRotation = newRot;
+        }
+
+        public void Update()
+        {
+            transform.position = Vector3.Lerp(transform.position, NextPosition, PlayerMovement.BROADCAST_INTERVAL);
+            transform.rotation = Quaternion.Lerp(transform.rotation, NextRotation, PlayerMovement.BROADCAST_INTERVAL);
+        }
+    }
+}
