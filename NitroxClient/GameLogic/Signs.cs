b547cb86bf231a7c8f735425158c80e54584a001
ahk1221 <ahmed16.ahk@gmail.com>
2018-06-23 01:48:24 +0500

    Add basic classes

---
+++ NitroxClient/GameLogic/Signs.cs
@@ -0,0 +1,22 @@
+﻿using NitroxClient.Communication.Abstract;
+using NitroxClient.GameLogic.Helper;
+using NitroxModel.Packets;
+using UnityEngine;
+
+namespace NitroxClient.GameLogic
+{
+    public class Signs
+    {
+        private readonly IPacketSender packetSender;
+
+        public Signs(IPacketSender packetSender)
+        {
+            this.packetSender = packetSender;
+        }
+
+        public void SignChanged(Sign sign)
+        {
+            string guid = GuidHelper.GetGuid(sign.gameObject);
+        }
+    }
+}
---
+++ NitroxModel/Packets/SignChanged.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class SignChanged : Packet
+    {
+        public string Guid { get; }
+        public string NewText { get; }
+        public int ColorIndex { get; }
+        public int ScaleIndex { get; }
+        public bool[] Elements { get; }
+
+        public SignChanged(string guid, string newText, int colorIndex, int scaleIndex, bool[] elements)
+        {
+            Guid = guid;
+            NewText = newText;
+            ColorIndex = colorIndex;
+            ScaleIndex = scaleIndex;
+            Elements = elements;
+        }
+    }
+}
