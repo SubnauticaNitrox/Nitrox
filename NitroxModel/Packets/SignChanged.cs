00cf7761777361518a58bc7b9e60a4a0774a5be5
ahk1221 <ahmed16.ahk@gmail.com>
2018-06-25 01:53:46 +0500

    Sign syncing (#226)
    * Add basic classes
    
    * Add sign syncing (non-functional with locker signs)
    
    * Locker signs now sync correctly.

---
+++ NitroxModel/Packets/SignChanged.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class SignChanged : Packet
+    {
+        public string Guid { get; }
+        public string NewText { get; }
+        public int ColorIndex { get; }
+        public int ScaleIndex { get; }
+        public bool[] Elements { get; }
+        public bool Background { get; }
+
+        public SignChanged(string guid, string newText, int colorIndex, int scaleIndex, bool[] elements, bool backgroundToggle)
+        {
+            Guid = guid;
+            NewText = newText;
+            ColorIndex = colorIndex;
+            ScaleIndex = scaleIndex;
+            Elements = elements;
+            Background = backgroundToggle;
+        }
+    }
+}
