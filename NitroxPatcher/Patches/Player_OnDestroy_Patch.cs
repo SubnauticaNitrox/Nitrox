913eab579c5ece43fb7613aa252b146943f4b3d9
Garsia0 <jesusefg@gmail.com>
2018-06-28 23:36:52 -0500

    Fix Player container wipe during the normal shutdown routine

---
+++ NitroxPatcher/Patches/Player_OnDestroy_Patch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using Harmony;
+using NitroxClient.Communication.Abstract;
+using NitroxClient.GameLogic;
+using NitroxModel.Core;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches
+{
+    public class Player_OnDestroy_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(Player);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("OnDestroy", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Prefix()
+        {
+            IMultiplayerSession multiplayerSession = NitroxServiceLocator.LocateService<IMultiplayerSession>();
+            multiplayerSession.Disconnect();
+            Log.Info("Player OnDestroyEvent");
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPrefix(harmony, TARGET_METHOD);
+        }
+    }
