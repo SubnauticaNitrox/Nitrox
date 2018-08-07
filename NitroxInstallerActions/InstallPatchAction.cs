fd3866c641c17d98b2882ab21a5ab43e094d803f
Devin Wall <devinmwall@gmail.com>
2018-08-07 01:11:52 -0600

    Converting the installer to wix

---
+++ NitroxInstallerActions/InstallPatchAction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+
+namespace NitroxInstallerActions
+{
+    public class InstallPatchAction
+    {
+        [CustomAction]
+        public static ActionResult InstallPatch(Session session)
+        {
+            session.Log("Begin CustomAction1");
+
+            return ActionResult.Success;
+        }
+    }
+}
