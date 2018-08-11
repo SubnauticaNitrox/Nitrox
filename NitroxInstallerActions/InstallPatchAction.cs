c934f2de366e3033fb392cc74cb6af04fbfe9de5
Devin Wall <devinmwall@gmail.com>
2018-08-10 22:18:18 -0600

    Nitrox Installer (#277)
    * Adding base for nitrox installer. This code still needs to be
    refactored.
    
    * Cleaned up code.
    
    * Fully working... Should switch this to WIX...
    
    * Fixing outputs
    
    * Converting the installer to wix
    
    * Stub custom actions added.
    
    * Adding patching and unpatching.
    
    * Installer fully ported to WIX.
    
    * Added license, and renamed folder to match namespace.

---
+++ NitroxInstallerActions/InstallPatchAction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using InstallerActions.Patches;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace NitroxInstallerActions
+{
+    public class InstallPatchAction
+    {
+        [CustomAction]
+        public static ActionResult InstallPatch(Session session)
+        {
+            session.Log("Begin install");
+
+            try
+            {
+                string managedDirectory = session.CustomActionData["MANAGEDDIR"];
+
+                if (!RequiredAssembliesExist(managedDirectory))
+                {
+                    MessageBox.Show("Error instaliing Nitrox to the specified directory. Please ensure the installer is pointing to your subnautica directory and try again.");
+                    return ActionResult.Failure;
+                }
+
+                NitroxEntryPatch nitroxPatch = new NitroxEntryPatch(managedDirectory);
+
+                if (!nitroxPatch.IsApplied)
+                {
+                    nitroxPatch.Apply();
+                }
+            }
+            catch (Exception ex)
+            {
+                session.Log(ex.Message);
+                return ActionResult.Failure;
+            }
+
+            return ActionResult.Success;
+        }
+
+        private static bool RequiredAssembliesExist(string managedDirectory)
+        {
+            return File.Exists(managedDirectory + NitroxEntryPatch.GAME_ASSEMBLY_NAME);
+        }
+    }
+}
