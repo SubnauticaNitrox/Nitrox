26f5e5248fe6e631e631ae83de932f920678d1f4
Measurity <measuring.infinity@gmail.com>
2018-12-21 11:54:47 +0100

    Moved NitroxServer WPF to its own project. (#438)
    * Moved NitroxServer WPF to its own project.
    Added input field to WPF log for sending commands to server.
    
    * Accidentally excluded Systen.Configuration from NitroxServer.csproj during merge.
    
    * Added NitroxServer WPF to installer which installs it in 'SubServer' directory.
    
    * Added the Nitrox repo license to the installer.

--- NitroxServer.Wpf/App.xaml.cs
+++ NitroxServer.Wpf/App.xaml.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
-namespace NitroxServer
+namespace NitroxServer.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
@@ -13,5 +14,4 @@ namespace NitroxServer
     public partial class App : Application
     {
     }
-
 }
