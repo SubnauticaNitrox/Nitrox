bee8a47ee1481f7778fd9569b91846d2b6822c32
DKing <dkingchn@gmail.com>
2019-01-08 11:24:46 +0800

    Online Server Menu - NitroxClient

--- NitroxClient/MonoBehaviours/Gui/MainMenu/MainMenuMultiplayerPanel.cs
+++ NitroxClient/MonoBehaviours/Gui/MainMenu/MainMenuMultiplayerPanel.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System;
 using System.Net;
-using System.Net.Sockets;
-using System.Text.RegularExpressions;
 using NitroxClient.Unity.Helper;
 using NitroxModel.Core;
-using NitroxModel.Logger;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using System.Threading;
 
 namespace NitroxClient.MonoBehaviours.Gui.MainMenu
 {
@@ -26,8 +24,19 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
         private string serverHostInput;
         private string serverNameInput;
 
-        private bool shouldFocus;
-        private bool showingAddServer;
+        private bool addServerOnFocus = false;
+        private bool showingAddServer = false;
+
+        private bool onlineServersOnFocus = false;
+        private bool showingOnlineServers = false;
+        private Rect onlineServerWindowRect = new Rect(50, 50, 1000, 600);
+        private string serverListText = "";
+        private List<ServerInfo> serverInfoList = new List<ServerInfo>();
+        private string serverFilter = "";
+        private bool serverLoadFinished = false;
+        private Vector2 scrollPos = new Vector2();
+        private int serverQueryThreadedNum = 0;
+        private readonly string serverInfoLink = "http://nitrox.qaq.link/server";
 
         public void Awake()
         {
@@ -43,6 +52,7 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
                 AddServer("local", "127.0.0.1");
             }
 
+            CreateButton("<color=red>Online Servers</color>", ToggleOnlineServers);
             CreateButton("Add server IP", ShowAddServerWindow);
             LoadSavedServers();
         }
@@ -59,7 +69,7 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
             multiplayerButtonInst.transform.SetParent(savedGameAreaContent, false);
         }
 
-        public void CreateServerButton(string text, string joinIp)
+        public void CreateServerButton(string text, ServerInfo sinfo)
         {
             GameObject multiplayerButtonInst = Instantiate(multiplayerButton);
             multiplayerButtonInst.name = (savedGameAreaContent.childCount - 1).ToString();
@@ -68,7 +78,7 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
             DestroyObject(txt.GetComponent<TranslationLiveUpdate>());
             Button multiplayerButtonButton = multiplayerButtonInst.RequireTransform("NewGameButton").GetComponent<Button>();
             multiplayerButtonButton.onClick = new Button.ButtonClickedEvent();
-            multiplayerButtonButton.onClick.AddListener(() => OpenJoinServerMenu(joinIp));
+            multiplayerButtonButton.onClick.AddListener(() => OpenJoinServerMenu(sinfo));
             multiplayerButtonInst.transform.SetParent(savedGameAreaContent, false);
 
             GameObject delete = Instantiate(SavedGamesRef.GetComponent<MainMenuLoadPanel>().saveInstance.GetComponent<MainMenuLoadButton>().deleteButton);
@@ -76,7 +86,7 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
             deleteButtonButton.onClick = new Button.ButtonClickedEvent();
             deleteButtonButton.onClick.AddListener(() =>
             {
-                RemoveServer(multiplayerButtonInst.transform.GetSiblingIndex() - 1);
+                RemoveServer(multiplayerButtonInst.transform.GetSiblingIndex() - 2);
                 Destroy(multiplayerButtonInst);
             });
             delete.transform.SetParent(multiplayerButtonInst.transform, false);
@@ -97,7 +107,7 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
             File.WriteAllLines(SERVER_LIST_PATH, serverLines.ToArray());
         }
 
-        public void OpenJoinServerMenu(string serverIp)
+        public void OpenJoinServerMenu(ServerInfo serverInfo)
         {
             NitroxServiceLocator.BeginNewLifetimeScope();
 
@@ -108,15 +118,9 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
 
             joinServerGameObject = new GameObject();
             JoinServer joinServerComponent = joinServerGameObject.AddComponent<JoinServer>();
+            joinServerComponent.ServerIp = serverInfo.Ip;
+            joinServerComponent.serverPort = serverInfo.Port;
 
-            if (Regex.IsMatch(serverIp, "^[0-9.:]+$"))
-            {
-                ResolveIpv4(joinServerComponent, serverIp);
-            }
-            else
-            {
-                ResolveHostName(joinServerComponent, serverIp);
-            }
         }
 
         public void ShowAddServerWindow()
@@ -124,23 +128,32 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
             serverNameInput = "local";
             serverHostInput = "127.0.0.1";
             showingAddServer = true;
-            shouldFocus = true;
+            addServerOnFocus = true;
         }
 
         public void HideAddServerWindow()
         {
             showingAddServer = false;
-            shouldFocus = true;
+            addServerOnFocus = true;
+        }
+
+        public void ToggleOnlineServers()
+        {
+            showingOnlineServers = !showingOnlineServers;
+            onlineServersOnFocus = !onlineServersOnFocus;
         }
 
         public void OnGUI()
         {
-            if (!showingAddServer)
+            if (showingAddServer)
             {
-                return;
+                addServerWindowRect = GUILayout.Window(GUIUtility.GetControlID(FocusType.Keyboard), addServerWindowRect, DoAddServerWindow, "Add server");
+            }
+            if (showingOnlineServers)
+            {
+                onlineServerWindowRect = GUILayout.Window(GUIUtility.GetControlID(FocusType.Keyboard), onlineServerWindowRect, DoOnlineServersWindow, "Online Servers");
             }
 
-            addServerWindowRect = GUILayout.Window(GUIUtility.GetControlID(FocusType.Keyboard), addServerWindowRect, DoAddServerWindow, "Add server");
         }
 
         private void LoadSavedServers()
@@ -150,49 +163,17 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    string[] lineData = line.Split('|');
-                    string serverName = lineData[0];
-                    string serverIp = lineData[1];
-                    CreateServerButton($"Connect to <b>{serverName}</b>\n{serverIp}", serverIp);
+                    ServerInfo sinfo = new ServerInfo(line);
+                    CreateServerButton($"Connect to <b>{sinfo.Name}</b>\n{sinfo.Address}", sinfo);
                 }
             }
         }
 
-        private void ResolveHostName(JoinServer joinServerComponent, string serverIp)
-        {
-            try
-            {
-                IPHostEntry hostEntry = Dns.GetHostEntry(serverIp);
-                joinServerComponent.ServerIp = hostEntry.AddressList[0].ToString();
-                joinServerComponent.serverPort = 11000;
-            }
-            catch (SocketException e)
-            {
-                Log.Error($"Unable to resolve the address: {serverIp}");
-                Log.Error(e.ToString());
-            }
-        }
-
-        private void ResolveIpv4(JoinServer joinServerComponent, string serverIp)
-        {
-            char seperator = ':';
-            if (serverIp.Contains(seperator))
-            {
-                string[] splitIP = serverIp.Split(seperator);
-                joinServerComponent.ServerIp = splitIP[0];
-                joinServerComponent.serverPort = int.Parse(splitIP[1]);
-            }
-            else
-            {
-                joinServerComponent.ServerIp = serverIp;
-                joinServerComponent.serverPort = 11000;
-            }
-        }
 
         private void OnAddServerButtonClicked()
         {
             AddServer(serverNameInput, serverHostInput);
-            CreateServerButton($"Connect to <b>{serverNameInput}</b>\n{serverHostInput}", serverHostInput);
+            CreateServerButton($"Connect to <b>{serverNameInput}</b>\n{serverHostInput}", new ServerInfo($"{serverNameInput}|{serverHostInput}"));
             HideAddServerWindow();
         }
 
@@ -219,8 +200,32 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
 
                 s.button.fontSize = 14;
                 s.button.stretchHeight = true;
+                
             });
         }
+
+        private GUISkin GetServerListGUISkin()
+        {
+            return GUISkinUtils.RegisterDerivedOnce("menus.serverlist", s =>
+            {
+                s.textField.fontSize = 14;
+                s.textField.richText = false;
+                s.textField.alignment = TextAnchor.MiddleLeft;
+                s.textField.wordWrap = false;
+                s.textField.stretchHeight = false;
+                s.textField.padding = new RectOffset(10, 10, 5, 5);
+
+                s.label.fontSize = 14;
+                s.label.alignment = TextAnchor.MiddleLeft;
+                s.label.stretchHeight = false;
+                s.label.wordWrap = false;
+
+                s.button.fontSize = 14;
+                s.button.stretchHeight = false;
+
+            });
+        }
+
 
         private void DoAddServerWindow(int windowId)
         {
@@ -237,6 +242,8 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
                         break;
                 }
             }
+
+            GUI.DragWindow(new Rect(0, 0, 500, 20));
 
             GUISkinUtils.RenderWithSkin(GetGUISkin(), () =>
             {
@@ -270,11 +277,184 @@ namespace NitroxClient.MonoBehaviours.Gui.MainMenu
                 }
             });
 
-            if (shouldFocus)
+            if (addServerOnFocus)
             {
                 GUI.FocusControl("serverNameField");
-                shouldFocus = false;
+                addServerOnFocus = false;
             }
         }
+        
+
+        private void CheckServerInfoThreaded(string serverStr)
+        {
+            if(serverStr == "" || !serverStr.Contains("|"))
+            {
+                serverQueryThreadedNum -= 1;
+                return;
+            }
+
+            new Thread(new ThreadStart(() =>
+            {
+                while (serverQueryThreadedNum>=20)
+                {
+                    Thread.Sleep(50);
+                }
+                ServerInfo info = new ServerInfo(serverStr.TrimEnd('\r'));
+                info.UpdateLatency();
+                serverQueryThreadedNum -= 1;
+                if (info.Ping == -1)
+                {
+                    return;
+                }
+                lock (serverInfoList)
+                {
+                    serverInfoList.Add(info);
+                    serverInfoList.Sort();
+                }
+                
+            })).Start();
+
+        }
+
+
+        private void OnServerListFetched(object sender, DownloadDataCompletedEventArgs e)
+        {
+            if (!e.Cancelled && e.Error == null)
+            {
+                serverListText = System.Text.Encoding.UTF8.GetString(e.Result);
+                lock (serverInfoList)
+                {
+                    serverInfoList.Clear();
+                }
+                foreach (string i in serverListText.Split('\n'))
+                {
+                    serverQueryThreadedNum += 1;
+                    CheckServerInfoThreaded(i);
+                }
+            }
+            else
+            {
+                serverListText = "";
+            }
+        }
+
+
+        private void DoOnlineServersWindow(int windowId)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                switch (e.keyCode)
+                {
+                    case KeyCode.Return:
+                        serverLoadFinished = false;
+                        break;
+
+                    case KeyCode.Escape:
+                        OnCancelButtonClicked();
+                        break;
+                }
+            }
+
+            GUI.DragWindow(new Rect(0, 0, 1000, 20));
+
+            GUISkinUtils.RenderWithSkin(GetServerListGUISkin(), () =>
+            {
+                using (new GUILayout.VerticalScope("Box"))
+                {
+                    using (new GUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label(" Search:", GUILayout.Width(200));
+                        GUI.SetNextControlName("serverFilter");
+                        serverFilter = GUILayout.TextField(serverFilter, 120, GUILayout.Width(400));
+
+                        if (GUILayout.Button("Refresh", GUILayout.Width(100)))
+                        {
+                            serverLoadFinished = false;
+                        }
+                        if (GUILayout.Button("Close", GUILayout.Width(100)))
+                        {
+                            showingOnlineServers = false;
+                        }
+                    }
+
+                    using (new GUILayout.HorizontalScope())
+                    {
+                        GUILayout.Label(" Server Name", GUILayout.Width(400));
+                        GUILayout.Label(" Server Address", GUILayout.Width(200));
+                        GUILayout.Label(" # Players", GUILayout.Width(100));
+                        GUILayout.Label(" Latency", GUILayout.Width(100));
+                        GUILayout.Label(" Connect");
+                    }
+
+                    using (var sv = new GUILayout.ScrollViewScope(scrollPos))
+                    {
+                        scrollPos = sv.scrollPosition;
+
+                        if (!serverLoadFinished)
+                        {
+                            using (WebClient client = new WebClient())
+                            {
+                                try
+                                {
+                                    // this might freeze game for hundreds milliseconds
+                                    client.DownloadDataCompleted += new DownloadDataCompletedEventHandler(OnServerListFetched);
+                                    client.DownloadDataAsync(new Uri(serverInfoLink));
+                                }
+                                catch (Exception)
+                                {
+                                    serverListText = "";
+                                }
+                            }
+                            serverLoadFinished = true;
+                        }
+
+                        lock (serverInfoList)
+                        { 
+                            foreach (ServerInfo sinfo in serverInfoList)
+                            {
+                                if (serverFilter != "" &&
+                                    sinfo.Name.IndexOf(serverFilter, StringComparison.OrdinalIgnoreCase) < 0 &&
+                                    sinfo.Address.IndexOf(serverFilter, StringComparison.OrdinalIgnoreCase) < 0)
+                                {
+                                    continue;
+                                }
+                                using (new GUILayout.HorizontalScope())
+                                {
+                                    GUILayout.Label(sinfo.Name, GUILayout.Width(400));
+                                    GUILayout.Label(sinfo.Address, GUILayout.Width(200));
+                                    string playernum_text = sinfo.PlayerNum == -1 ? "Unknown" : sinfo.PlayerNum.ToString();
+                                    GUILayout.Label($"<color=blue><b>{playernum_text}</b></color>", GUILayout.Width(100));
+
+                                    string color = "green";
+                                    if (sinfo.Ping < 100)
+                                        color = "green";
+                                    else if (sinfo.Ping < 200)
+                                        color = "yellow";
+                                    else
+                                        color = "red";
+                                    
+                                    if(sinfo.Ping == 9999)
+                                        GUILayout.Label($"<color=red>Unknown</color>", GUILayout.Width(100));
+                                    else
+                                        GUILayout.Label($"<color={color}>{sinfo.Ping}ms</color>", GUILayout.Width(100));
+
+                                    if (GUILayout.Button("Connect"))
+                                    {
+                                        showingOnlineServers = false;
+                                        OpenJoinServerMenu(sinfo);
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+
+                }
+            });
+        }
+
+        
+
     }
 }
---
+++ NitroxClient/MonoBehaviours/Gui/MainMenu/ServerInfo.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using NitroxModel.Logger;
+using UnityEngine;
+using System.Threading;
+using System.Linq;
+
+namespace NitroxClient.MonoBehaviours.Gui.MainMenu
+{
+    public class ServerInfo : IComparable<ServerInfo>
+    {
+        private bool _isValid = true;     // lets stay positive unless we know its not
+        public bool IsValid { get { return _isValid; } set { } }
+
+        // simple parse
+        public readonly string Name;      // title
+        public readonly string Address;   // domain or ip. directly from serverStr
+        public readonly int PlayerNum;
+
+        // LAZY LOAD considering network latency
+        public string Host { get { if(_host == null) LazyLoad(); return _host; } }
+        public string Ip { get { if (_ip == null) LazyLoad(); return _ip; } }
+        public int Port { get { if (_port == 0) LazyLoad(); return _port; } }
+        public int Ping { get { if (_ping == -1) UpdateLatency(); return _ping; }  set { _ping = value; } }
+
+        // Internals for getter/setter
+        private string _host = null;   // only domain name. no port
+        private string _ip = null;     // parsed ip address
+        private int _port = 0;
+        private int _ping = -1;        // do not use 0 here. because localhost = 0 and it is corrent
+
+
+        public ServerInfo(string serverStr)
+        {
+            string[] lineData = serverStr.Split('|');
+            if (lineData.Length == 1) // suppose this is pure address
+            {
+                Address = lineData[0].Trim();
+                Name = Address;
+                PlayerNum = -1;
+            }
+            else if (lineData.Length == 2)
+            {
+                Name = lineData[0];
+                Address = lineData[1].Trim();
+                PlayerNum = -1;
+            }
+            else
+            {
+                Name = lineData[0];
+                Address = lineData[1].Trim();
+                if (!int.TryParse(lineData[2], out PlayerNum))
+                    PlayerNum = -1;
+            }
+        }
+
+        public int CompareTo(ServerInfo other)
+        {
+            if(Ping.Equals(other.Ping))
+            {
+                return Name.CompareTo(other.Name);
+            }
+            else
+            {
+                return Ping.CompareTo(other.Ping);
+            }
+        }
+        
+        private void LazyLoad()
+        {
+            /* Possible Valid Address Formats:
+             *      12.34.56.78
+             *      12.34.56.78:12
+             *      fe80::12:34
+             *      [fe80::12:34]
+             *      [fe80::12:34]:12
+             *      sub.非英文字符.domain.com
+             *      sub.非英文字符.domain.com:123
+             * Solution:
+             *      1. split to host and port
+             *      2. check result of IPAddress.TryParse(host, out ipaddr)
+             */
+            if (!Address.Contains('.') && !Address.Contains(':'))
+            {
+                _isValid = false;
+                _host = Address;    // fill them to avoid null ref if used elsewhere
+                _ip = Address;
+                return;
+            }
+
+            string tmpAddressStr = Address;
+
+            // UrlBuilder throws exception on naked ipv6 ( address not qouted with [ ] )
+            // so we qoute it before throwing it in UrlBuilder
+            if (Address.Count(f => f == ':') > 1 && Regex.IsMatch(Address, @"^[0-9a-zA-Z:\.]+$")) // seems like naked ipv6
+            {
+                tmpAddressStr = $"[{tmpAddressStr}]";
+            }
+
+            // Now we use UrlBuilder to have everything parsed well
+            try
+            {
+                UriBuilder uri = new UriBuilder("http://" + tmpAddressStr);
+
+                _port = uri.Port == 80 ? 11000 : uri.Port;    // lets hope nobody really bind game server on 80
+
+                IPAddress tmpIPAddr = null;
+                if(IPAddress.TryParse(uri.Host, out tmpIPAddr))
+                {
+                    _ip = tmpIPAddr.ToString(); // shorten ip string
+                    _host = _ip;
+                }
+                else
+                {
+                    _host = uri.Host;
+                    _ip = Dns.GetHostEntry(_host).AddressList[0].ToString();
+                }
+                return;
+            }
+            catch (Exception e)
+            {
+                _isValid = false;
+                if (_host == null) _host = Address;
+                if (_ip   == null) _ip   = Address;
+                Log.Error($"Failed to parse server {Address}. Error Message: {e.Message}");
+            }
+            
+        }
+
+
+        public void UpdateLatency()
+        {
+            Ping ping = null;
+            try
+            {
+                ping = new Ping(Ip);
+                while (!ping.isDone)
+                {
+                    Thread.Sleep(50);
+                }
+                _ping = ping.time;
+
+            }
+            catch (Exception)
+            {
+                _ping = 9999;
+            }
+        }
+    }
+    
+}
