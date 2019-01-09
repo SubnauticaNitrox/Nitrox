efedb6775d7ec1e3fef73663d34987e4ea6708a5
MadMaxOfYore <madmaxthemighty@gmail.com>
2019-01-09 01:32:38 -0600

    no functional yet - but I can't risk losing this work at this point

--- NitroxClient/GameLogic/RemotePlayer.cs
+++ NitroxClient/GameLogic/RemotePlayer.cs
@@ -1,4 +1,7 @@
-﻿using NitroxClient.GameLogic.Helper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NitroxClient.GameLogic.Helper;
 using NitroxClient.GameLogic.PlayerModelBuilder;
 using NitroxClient.MonoBehaviours;
 using NitroxClient.Unity.Helper;
@@ -6,6 +9,7 @@ using NitroxModel.DataStructures.Util;
 using NitroxModel.Helper;
 using NitroxModel.MultiplayerSession;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace NitroxClient.GameLogic
 {
@@ -39,6 +43,8 @@ namespace NitroxClient.GameLogic
             // Get player
             PlayerModel = Body.RequireGameObject("player_view");
 
+            EquipRadiationSuit();
+
             // Move variables to keep player animations from mirroring and for identification
             ArmsController = PlayerModel.GetComponent<ArmsController>();
             ArmsController.smoothSpeedUnderWater = 0;
@@ -48,7 +54,235 @@ namespace NitroxClient.GameLogic
 
             ErrorMessage.AddMessage($"{PlayerName} joined the game.");
         }
-        
+
+        private void EquipRadiationSuit()
+        {
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_body_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_body_geo").gameObject.SetActive(false);
+
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_head_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_head_geo").gameObject.SetActive(false);
+            
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_gloves_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_hands_geo").gameObject.SetActive(false);
+
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo/radiationSuit_helmet_geo 1").gameObject.SetActive(true);
+
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_tank_geo 1").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_tank_geo 1/radiationSuit_tubes_geo 1").gameObject.SetActive(true);
+
+            PlayerModel.transform.Find("male_geo/generalSuit").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/generalSuit/flippers_basic_geo").gameObject.SetActive(true);
+
+            float playerHue;
+            float playerSaturation;
+            float playerVibrancy;
+
+            Color.RGBToHSV(PlayerSettings.PlayerColor, out playerHue, out playerSaturation, out playerVibrancy);
+
+            HsvColorFilter finFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            finFilter.AddHueRange(0f, 35f);
+
+            HsvColorFilter tankFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            tankFilter.AddHueRange(0f, 85f);
+
+            HsvColorFilter helmetFilter = new HsvColorFilter(playerHue, playerSaturation, playerVibrancy, -1f);
+            helmetFilter.AddSaturationRange(0f, 35f);
+            helmetFilter.AddVibranceRange(30f, 100f);
+
+            HsvColorFilter vestFilter = new HsvColorFilter(playerHue, playerSaturation, playerVibrancy, -1f);
+            vestFilter.AddSaturationRange(0f, 35f);
+            vestFilter.AddVibranceRange(12f, 100f);
+
+            SkinnedMeshRenderer finRenderer = PlayerModel.transform.Find("male_geo/generalSuit/flippers_basic_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            finRenderer.material.ApplyFiltersToMainTexture(finFilter);
+            finRenderer.material.SetTexture("_MainTex", finRenderer.material.mainTexture);
+            finRenderer.material.SetTexture("_SpecTex", finRenderer.material.mainTexture);
+
+            SkinnedMeshRenderer tankRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_tank_geo 1").gameObject.GetComponent<SkinnedMeshRenderer>();
+            tankRenderer.material.ApplyFiltersToMainTexture(tankFilter);
+            tankRenderer.material.SetTexture("_MainTex", tankRenderer.material.mainTexture);
+            tankRenderer.material.SetTexture("_SpecTex", tankRenderer.material.mainTexture);
+
+            SkinnedMeshRenderer helmetRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo/radiationSuit_helmet_geo 1").gameObject.GetComponent<SkinnedMeshRenderer>();
+            helmetRenderer.material.ApplyFiltersToMainTexture(helmetFilter);
+
+            SkinnedMeshRenderer neckRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            neckRenderer.material.ApplyFiltersToMainTexture(helmetFilter);
+
+            SkinnedMeshRenderer vestRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            vestRenderer.material.ApplyFiltersToMainTexture(vestFilter);
+
+            SkinnedMeshRenderer bodyRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_body_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+
+            HsvColorFilter legFilter = new HsvColorFilter(playerHue, 100f, playerVibrancy, -1f);
+            legFilter.AddSaturationRange(0f, 35f);
+            legFilter.AddVibranceRange(40f, 100f);
+
+            HsvColorFilter feetFilter = new HsvColorFilter(playerHue, -1f, playerVibrancy, -1f);
+            feetFilter.AddHueRange(0f, 100f);
+            feetFilter.AddVibranceRange(30f, 100f);
+
+            HsvColorFilter beltFilter = new HsvColorFilter(playerHue, 100f, playerVibrancy, -1f);
+            beltFilter.AddVibranceRange(3f, 100f);
+            beltFilter.AddHueRange(0f, 90f);
+
+            Texture2D bodyTexture = ((Texture2D)bodyRenderer.materials[0].mainTexture).Clone();
+            bodyTexture.ApplyFiltersToBlock(700, 484, 130, 155, legFilter);
+            bodyTexture.ApplyFiltersToBlock(525, 324, 250, 325, feetFilter);
+            bodyTexture.ApplyFiltersToBlock(570, 0, 454, 1024, beltFilter);
+
+            bodyRenderer.materials[0].mainTexture = bodyTexture;
+            bodyRenderer.materials[0].SetTexture("_SpecTex", bodyTexture);
+            //bodyRenderer.materials[0].ApplyFiltersToMainTexture(hueFilter);
+            bodyRenderer.materials[1].ApplyFiltersToMainTexture(feetFilter);
+        }
+
+        private void EquipReinforcedSuit()
+        {
+            PlayerModel.transform.Find("male_geo/reinforcedSuit/reinforced_suit_01_body_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_body_geo").gameObject.SetActive(false);
+            
+            PlayerModel.transform.Find("male_geo/reinforcedSuit/reinforced_suit_01_glove_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_hands_geo").gameObject.SetActive(false);
+
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_head_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_head_geo").gameObject.SetActive(false);
+
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo/radiationSuit_helmet_geo 1").gameObject.SetActive(true);
+
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_tank_geo 1").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_tank_geo 1/radiationSuit_tubes_geo 1").gameObject.SetActive(true);
+
+            PlayerModel.transform.Find("male_geo/SwimChargeFins").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/SwimChargeFins/flippers_basic_geo").gameObject.SetActive(true);
+
+            float playerHue;
+            float playerSaturation;
+            float playerVibrancy;
+
+            Color.RGBToHSV(PlayerSettings.PlayerColor, out playerHue, out playerSaturation, out playerVibrancy);
+
+            HsvColorFilter finFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            finFilter.AddHueRange(0f, 35f);
+
+            HsvColorFilter tankFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            tankFilter.AddHueRange(0f, 85f);
+
+            HsvColorFilter helmetFilter = new HsvColorFilter(playerHue, 100f, playerVibrancy, -1f);
+            helmetFilter.AddSaturationRange(0f, 35f);
+            helmetFilter.AddVibranceRange(30f, 100f);
+
+            HsvColorFilter vestFilter = new HsvColorFilter(playerHue, 100f, playerVibrancy, -1f);
+            vestFilter.AddSaturationRange(0f, 35f);
+            vestFilter.AddVibranceRange(12f, 100f);
+
+            HsvColorFilter bodyFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            bodyFilter.AddHueRange(0f, 20f);
+            bodyFilter.AddSaturationRange(45f, 100f);
+
+            SkinnedMeshRenderer finRenderer = PlayerModel.transform.Find("male_geo/SwimChargeFins/flippers_basic_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            finRenderer.material.ApplyFiltersToMainTexture(finFilter);
+            finRenderer.material.SetTexture("_MainTex", finRenderer.material.mainTexture);
+            finRenderer.material.SetTexture("_SpecTex", finRenderer.material.mainTexture);
+
+            SkinnedMeshRenderer tankRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_tank_geo 1").gameObject.GetComponent<SkinnedMeshRenderer>();
+            tankRenderer.material.ApplyFiltersToMainTexture(tankFilter);
+            tankRenderer.material.SetTexture("_MainTex", tankRenderer.material.mainTexture);
+            tankRenderer.material.SetTexture("_SpecTex", tankRenderer.material.mainTexture);
+
+            SkinnedMeshRenderer helmetRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo/radiationSuit_helmet_geo 1").gameObject.GetComponent<SkinnedMeshRenderer>();
+            helmetRenderer.material.ApplyFiltersToMainTexture(helmetFilter);
+
+            SkinnedMeshRenderer neckRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo/radiationSuit_vest_neckExtension_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            neckRenderer.material.ApplyFiltersToMainTexture(helmetFilter);
+
+            SkinnedMeshRenderer vestRenderer = PlayerModel.transform.Find("male_geo/radiationSuit/radiationSuit_vest_reducedNeck_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            vestRenderer.material.ApplyFiltersToMainTexture(vestFilter);
+
+            SkinnedMeshRenderer bodyRenderer = PlayerModel.transform.Find("male_geo/reinforcedSuit/reinforced_suit_01_body_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            bodyRenderer.material.ApplyFiltersToMainTexture(bodyFilter);
+            bodyRenderer.materials[1].ApplyFiltersToMainTexture(bodyFilter);
+
+            SkinnedMeshRenderer gloveRenderer = PlayerModel.transform.Find("male_geo/reinforcedSuit/reinforced_suit_01_glove_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            gloveRenderer.material.ApplyFiltersToMainTexture(bodyFilter);
+        }
+
+        private void EquipStillSuit()
+        {
+            PlayerModel.transform.Find("male_geo/stillSuit/still_suit_01_body_geo").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_body_geo").gameObject.SetActive(false);
+
+            PlayerModel.transform.Find("male_geo/scubaSuit").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/scubaSuit/scuba_head").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_head_geo").gameObject.SetActive(false);
+            
+            PlayerModel.transform.Find("male_geo/scubaSuit/scuba_vest").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/scubaSuit/scuba_vest/scuba_breatherTubes").gameObject.SetActive(true);
+
+            PlayerModel.transform.Find("male_geo/UltraGlideFins").gameObject.SetActive(true);
+            PlayerModel.transform.Find("male_geo/UltraGlideFins/flippers_basic_geo").gameObject.SetActive(true);
+
+            float playerHue;
+            float playerSaturation;
+            float playerVibrancy;
+
+            Color.RGBToHSV(PlayerSettings.PlayerColor, out playerHue, out playerSaturation, out playerVibrancy);
+
+            HsvColorFilter finFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            finFilter.AddHueRange(0f, 35f);
+
+            HsvColorFilter tankFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            tankFilter.AddHueRange(0f, 19f);
+            
+            HsvColorFilter bodyFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            bodyFilter.AddHueRange(0f, 75f);
+
+            HsvColorFilter helmetFilter = new HsvColorFilter(playerHue, -1f, -1f, -1f);
+            helmetFilter.AddHueRange(0f, 25f);
+
+            SkinnedMeshRenderer finRenderer = PlayerModel.transform.Find("male_geo/UltraGlideFins/flippers_basic_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            finRenderer.material.ApplyFiltersToMainTexture(finFilter);
+            finRenderer.material.SetTexture("_MainTex", finRenderer.material.mainTexture);
+            finRenderer.material.SetTexture("_SpecTex", finRenderer.material.mainTexture);
+
+            SkinnedMeshRenderer tankRenderer = PlayerModel.transform.Find("male_geo/scubaSuit/scuba_vest").gameObject.GetComponent<SkinnedMeshRenderer>();
+            tankRenderer.material.ApplyFiltersToMainTexture(tankFilter);
+            tankRenderer.material.SetTexture("_MainTex", tankRenderer.material.mainTexture);
+            tankRenderer.material.SetTexture("_SpecTex", tankRenderer.material.mainTexture);
+
+            SkinnedMeshRenderer bodyRenderer = PlayerModel.transform.Find("male_geo/stillSuit/still_suit_01_body_geo").gameObject.GetComponent<SkinnedMeshRenderer>();
+            bodyRenderer.material.ApplyFiltersToMainTexture(bodyFilter);
+            bodyRenderer.material.SetTexture("_MainTex", bodyRenderer.material.mainTexture);
+            bodyRenderer.material.SetTexture("_SpecTex", bodyRenderer.material.mainTexture);
+
+            bodyRenderer.materials[1].ApplyFiltersToMainTexture(bodyFilter);
+            bodyRenderer.materials[1].SetTexture("_MainTex", bodyRenderer.materials[1].mainTexture);
+            bodyRenderer.materials[1].SetTexture("_SpecTex", bodyRenderer.materials[1].mainTexture);
+
+            SkinnedMeshRenderer helmetRenderer = PlayerModel.transform.Find("male_geo/scubaSuit/scuba_head").gameObject.GetComponent<SkinnedMeshRenderer>();
+            helmetRenderer.material.ApplyFiltersToMainTexture(helmetFilter);
+
+            Shader marmosetShader = PlayerModel.transform.Find("male_geo/diveSuit/diveSuit_head_geo").gameObject.GetComponent<SkinnedMeshRenderer>().material.shader;
+            helmetRenderer.material.shader = marmosetShader;
+            helmetRenderer.material.shaderKeywords = new List<string>() { "UWE_DITHERALPHA", "MARMO_SIMPLE_GLASS", "MARMO_SPECMAP", "WBOIT", "_NORMALMAP", "_ZWRITE_ON" }.ToArray();
+            helmetRenderer.material.SetOverrideTag("RenderType", "Transparent");
+            
+            helmetRenderer.material.SetTexture("_MainTex", helmetRenderer.material.mainTexture);
+            helmetRenderer.material.SetTexture("_SpecTex", helmetRenderer.material.mainTexture);
+            helmetRenderer.material.SetTexture("_BumpMap", helmetRenderer.material.GetTexture("player_mask_01_normal"));
+
+            helmetRenderer.materials[2].shader = marmosetShader;
+            helmetRenderer.materials[2].shaderKeywords = new List<string>() { "MARMO_SPECMAP", "_ZWRITE_ON" }.ToArray();
+            helmetRenderer.materials[2].SetTexture("_MainTex", helmetRenderer.materials[2].mainTexture);
+            helmetRenderer.materials[2].SetTexture("_SpecTex", helmetRenderer.materials[2].mainTexture);
+        }
+
         public void ResetModel(ILocalNitroxPlayer localPlayer)
         {
             Body = Object.Instantiate(localPlayer.BodyPrototype);
@@ -73,7 +307,7 @@ namespace NitroxClient.GameLogic
         public void UpdatePosition(Vector3 position, Vector3 velocity, Quaternion bodyRotation, Quaternion aimingRotation)
         {
             Body.SetActive(true);
-            
+
             // When receiving movement packets, a player can not be controlling a vehicle (they can walk through subroots though).
             SetVehicle(null);
             SetPilotingChair(null);
