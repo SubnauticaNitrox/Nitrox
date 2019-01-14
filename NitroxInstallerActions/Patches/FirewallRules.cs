b140bce3a6648e758a63eb096d47ec31b0dd20f0
Fahad Suhail Ahmad <fahadahmad2001@outlook.com>
2019-01-14 19:54:17 +0300

    Added the requested changes

--- NitroxInstallerActions/Patches/FirewallRules.cs
+++ NitroxInstallerActions/Patches/FirewallRules.cs
@@ -5,9 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.IO;
+using System.Windows.Forms;
 
 namespace InstallerActions.Patches
 {
+    public enum FirewallRuleType
+    {
+        PortsUDP=0,
+        ProgramServer=1,
+        ProgramClient=2
+    }
     public struct FirewallRule
     {
         string RunNetshCommand(string Argument)
@@ -27,42 +34,42 @@ namespace InstallerActions.Patches
             netshProcess.Close();
             return output;
         }
+        public FirewallRuleType ruleType;
         public string addCommand;
         public string removeCommand;
-        public string name;
         public bool isInstalled;
-        string[] GetAddRemoveCommand(string RuleName, string GamePath, int Port = 11000)
+        string[] GetAddRemoveCommand(FirewallRuleType RuleType, string GamePath, int Port = 11000)
         {
             string[] commands = new string[2] { "", "" };
-            if (RuleName == "PortsUDP")
+            if (RuleType == FirewallRuleType.PortsUDP)
             {
                 commands[0] = "advfirewall firewall add rule name=\x22Nitrox Multiplayer Mod UDP\x22 dir=in action=allow protocol=UDP localport=" + Port;
                 commands[1] = "advfirewall firewall delete rule name=\x22Nitrox Multiplayer Mod UDP\x22 protocol=UDP localport=" + Port;
             }
-            if (RuleName == "ProgramServer")
+            else if (RuleType == FirewallRuleType.ProgramServer)
             {
                 commands[0] = "advfirewall firewall add rule name=\x22Nitrox Multiplayer Mod Server\x22 dir=in action=allow program=\x22" + Path.Combine(GamePath, "SubServer", "NitroxServer.exe") + "\x22 enable=yes";
                 commands[1] = "advfirewall firewall delete rule name=\x22Nitrox Multiplayer Mod Server\x22";
             }
-            if (RuleName == "ProgramClient")
+            else if (RuleType == FirewallRuleType.ProgramClient)
             {
                 commands[0] = "advfirewall firewall add rule name=\x22Nitrox Multiplayer Mod Client\x22 dir=in action=allow program=\x22" + Path.Combine(GamePath, "Subnautica.exe") + "\x22 enable=yes";
                 commands[1] = "advfirewall firewall delete rule name=\x22Nitrox Multiplayer Mod Client\x22";
             }
             return commands;
         }
-        bool GetRuleInstalled(string RuleName)
+        bool GetRuleInstalled(FirewallRuleType RuleType)
         {
             string command = "", output = "";
-            if (RuleName == "PortsUDP")
+            if (RuleType == FirewallRuleType.PortsUDP)
             {
                 command = "advfirewall firewall show rule name=\x22Nitrox Multiplayer Mod UDP\x22";
             }
-            if (RuleName == "ProgramServer")
+            else if (RuleType == FirewallRuleType.ProgramServer)
             {
                 command= "advfirewall firewall show rule name=\x22Nitrox Multiplayer Mod Server\x22";
             }
-            if (RuleName == "ProgramClient")
+            else if (RuleType == FirewallRuleType.ProgramClient)
             {
                 command= "advfirewall firewall show rule name=\x22Nitrox Multiplayer Mod Client\x22";
             }
@@ -76,19 +83,20 @@ namespace InstallerActions.Patches
                 return true;
             }
         }
-        public void CreateRule(string RuleName, string GamePath, int Port = 11000)
+        public bool CreateRule(FirewallRuleType NewRuleType, string GamePath, int Port = 11000)
         {
-            if (RuleName == "PortsUDP" || RuleName == "ProgramServer" || RuleName == "ProgramClient")
+            try
             {
-                isInstalled = GetRuleInstalled(RuleName);
-                string[] commands = GetAddRemoveCommand(RuleName, GamePath, Port);
+                isInstalled = GetRuleInstalled(NewRuleType);
+                string[] commands = GetAddRemoveCommand(NewRuleType, GamePath, Port);
                 addCommand = commands[0];
                 removeCommand = commands[1];
-                name = RuleName;
+                ruleType=NewRuleType;
+                return true;
             }
-            else
+            catch(Exception ex)
             {
-                throw new ArgumentException("Invalid rule type given. Rule type given was " + RuleName + ". It must be PortsUDP, ProgramServer, or ProgramClient");
+                return false;
             }
         }
         public bool Apply(bool IsInstalling)
@@ -135,11 +143,14 @@ namespace InstallerActions.Patches
         List<FirewallRule> Init()
         {
             List<FirewallRule> rules = new List<FirewallRule>();
-            string[] ruleNames = new string[3] { "PortsUDP", "ProgramServer", "ProgramClient" };
-            foreach(string eachName in ruleNames)
+            FirewallRuleType[] ruleTypes = new FirewallRuleType[3] { FirewallRuleType.PortsUDP, FirewallRuleType.ProgramServer, FirewallRuleType.ProgramClient};
+            foreach(FirewallRuleType eachType in ruleTypes)
             {
                 FirewallRule rule = new FirewallRule();
-                rule.CreateRule(eachName, subPath, portNumber);
+                if(!rule.CreateRule(eachType, subPath, portNumber))
+                {
+                    MessageBox.Show("Error in modifying Windows Firewall. You may have to add some exceptions manually");
+                }
                 rules.Add(rule);
             }
             return rules;
@@ -170,4 +181,5 @@ namespace InstallerActions.Patches
         }
     }
 }
+
 
