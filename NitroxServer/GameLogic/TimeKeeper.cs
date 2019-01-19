cdbf221820557c07763d72fbb06535e34830f2b8
Tyan <tyan@mail.de>
2019-01-19 11:20:31 +0100

    Added day/night server commands

--- NitroxServer/GameLogic/TimeKeeper.cs
+++ NitroxServer/GameLogic/TimeKeeper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using NitroxModel.Core;
+using NitroxModel.Packets;
 
 namespace NitroxServer.GameLogic
 {
@@ -6,15 +8,47 @@ namespace NitroxServer.GameLogic
     {
         // Values taken directly from hardcoded subnautica values
         private static DateTime SUBNAUTICA_FUTURE_START_DATE = new DateTime(2287, 5, 7, 9, 36, 0);
-        private static float SUBNAUTICA_BEGIN_TIME_OFFSET = 1200f * (3600f * (float)SUBNAUTICA_FUTURE_START_DATE.Hour + 60f *
-                                                                             (float)SUBNAUTICA_FUTURE_START_DATE.Minute +
-                                                                             (float)SUBNAUTICA_FUTURE_START_DATE.Second) / 86400f;
+
+        private static float SUBNAUTICA_BEGIN_TIME_OFFSET = 1200f / 86400f *
+                                                            (3600f * (float)SUBNAUTICA_FUTURE_START_DATE.Hour +
+                                                             60f * (float)SUBNAUTICA_FUTURE_START_DATE.Minute +
+                                                             (float)SUBNAUTICA_FUTURE_START_DATE.Second);
+
         public DateTime ServerStartTime { get; set; } = DateTime.Now;
 
-        public float GetCurrentTime()
+        // Discrepancy value for player based time modifications
+        private float correctionValue;
+
+        public void SetDay()
+        {
+            correctionValue += 1200.0f - GetCurrentTime() % 1200.0f + 600.0f;
+            SendCurrentTimePacket();
+        }
+
+        public void SetNight()
+        {
+            correctionValue += 1200.0f - GetCurrentTime() % 1200.0f;
+            SendCurrentTimePacket();
+        }
+
+        // Convenience for sending the TimeChange packet to a player or to all online players
+        public void SendCurrentTimePacket(Player player = null)
+        {
+            if (player != null)
+            {
+                player.SendPacket(new TimeChange(GetCurrentTime()));
+            }
+            else
+            {
+                PlayerManager playerManager = NitroxServiceLocator.LocateService<PlayerManager>();
+                playerManager.SendPacketToAllPlayers(new TimeChange(GetCurrentTime()));
+            }
+        }
+
+        private float GetCurrentTime()
         {
             TimeSpan interval = DateTime.Now - ServerStartTime;
-            return Convert.ToSingle(interval.TotalSeconds) + SUBNAUTICA_BEGIN_TIME_OFFSET;
+            return Convert.ToSingle(interval.TotalSeconds) + SUBNAUTICA_BEGIN_TIME_OFFSET + correctionValue;
         }
     }
 }
