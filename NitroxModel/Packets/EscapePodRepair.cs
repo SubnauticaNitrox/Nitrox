7e5d023b13fa126fbef6c4129204cb83f515234a
Kyle Windsor <kyle@kylewindsor.io>
2019-01-26 06:06:41 -0500

    Effects/syncing/serialization for initial broken pod state (#659)
    * custom save file name
    
    * custom save file name
    
    * save name in place of save file
    
    * initial pod damage/repairs persistence and effects

---
+++ NitroxModel/Packets/EscapePodRepair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class EscapePodRepair : Packet
+    {
+        public string Guid { get; }
+
+        public EscapePodRepair(string guid)
+        {
+            Guid = guid;
+        }
+
+        public override string ToString()
+        {
+            return "[EscapePodRepair guid: " + Guid + "]";
+        }
+    }
+}
---
+++ NitroxServer/Communication/Packets/Processors/EscapePodRadioRepairProcessor.cs
@@ -0,0 +1,26 @@
+﻿using NitroxModel.Packets;
+using NitroxServer.Communication.Packets.Processors.Abstract;
+using NitroxServer.GameLogic;
+using NitroxServer.Serialization.World;
+
+namespace NitroxServer.Communication.Packets.Processors
+{
+    public class EscapePodRadioRepairProcessor : AuthenticatedPacketProcessor<EscapePodRadioRepair>
+    {
+        private readonly World world;
+        private readonly PlayerManager playerManager;
+
+        public EscapePodRadioRepairProcessor(World world, PlayerManager playerManager)
+        {
+            this.world = world;
+            this.playerManager = playerManager;
+        }
+
+        public override void Process(EscapePodRadioRepair packet, Player player)
+        {
+            world.EscapePodManager.RepairEscapePodRadio(packet.Guid);
+            playerManager.SendPacketToOtherPlayers(packet, player);            
+        }
+    }
+
+}
