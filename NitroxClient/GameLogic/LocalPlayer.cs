50b7fd2c6fd9e6ce310dfda3f4eb194ccfe7328d
Jannify <Kannst-Du-mir-folgen@gmx.de>
2019-01-28 23:25:21 +0100

    Fix death message username

--- NitroxClient/GameLogic/LocalPlayer.cs
+++ NitroxClient/GameLogic/LocalPlayer.cs
@@ -66,7 +66,7 @@ namespace NitroxClient.GameLogic
 
         public void BroadcastDeath(Vector3 deathPosition)
         {
-            PlayerDeathEvent playerDeath = new PlayerDeathEvent(multiplayerSession.Reservation.PlayerId, deathPosition);
+            PlayerDeathEvent playerDeath = new PlayerDeathEvent(multiplayerSession.AuthenticationContext.Username, deathPosition);
             packetSender.Send(playerDeath);
         }
 
