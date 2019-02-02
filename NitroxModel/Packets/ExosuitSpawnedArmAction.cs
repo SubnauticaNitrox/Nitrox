591db02cc33334248b83cc274f059957c51b3921
Allen SImpson <Allen.SImpson@bhpbilliton.com>
2019-02-02 22:06:49 +0800

    Added spawned arm packet

---
+++ NitroxModel/Packets/ExosuitSpawnedArmAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class ExosuitSpawnedArmAction : Packet
+    {
+        public string ExoGuid { get; }
+        public string LeftArmGuid { get; }
+        public string RightArmGuid { get; }
+
+        public ExosuitSpawnedArmAction(string exoGuid, string leftArmGuid, string rightArmGuid)
+        {
+            ExoGuid = exoGuid;
+            LeftArmGuid = leftArmGuid;
+            RightArmGuid = rightArmGuid;
+        }
+
+        public override string ToString()
+        {
+            return "[ExosuitModulesAction - Guid: " + ExoGuid + " LeftArmGuid: " + LeftArmGuid + " RightArmGuid: " + RightArmGuid + "]";
+        }
+    }
+}
