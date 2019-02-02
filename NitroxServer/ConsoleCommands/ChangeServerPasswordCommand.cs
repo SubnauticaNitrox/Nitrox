b6c6072951714f11dfb20ae434372e1964bea255
dkingcn <dkingchn@gmail.com>
2019-02-02 21:09:01 +0800

    Add support for command ChangeServerPassword
    change password in AuthContext to Option.OfNullable
    rename ServerAdminPassword to AdminPassword to avoid confusion with ServerPassword

--- NitroxServer/ConsoleCommands/ChangeServerPasswordCommand.cs
+++ NitroxServer/ConsoleCommands/ChangeServerPasswordCommand.cs
@@ -8,12 +8,12 @@ using NitroxServer.ConfigParser;
 
 namespace NitroxServer.ConsoleCommands
 {
-    internal class ChangeAdminPasswordCommand : Command
+    internal class ChangeServerPasswordCommand : Command
     {
         private readonly PlayerManager playerManager;
         private readonly ServerConfig serverConfig;
 
-        public ChangeAdminPasswordCommand(PlayerManager playerManager, ServerConfig serverConfig) : base("changeadminpassword", Perms.ADMIN, "<password>", "Change the admin password")
+        public ChangeServerPasswordCommand(PlayerManager playerManager, ServerConfig serverConfig) : base("changeserverpassword", Perms.ADMIN, "[<password>]", "Change the server password. Clear password without argument")
         {
             this.playerManager = playerManager;
             this.serverConfig = serverConfig;
@@ -24,23 +24,23 @@ namespace NitroxServer.ConsoleCommands
             try
             {
                 string playerName = player.IsPresent() ? player.Get().Name : "SERVER";
-                ChangeAdminPassword(args[0], playerName);   
+                ChangeServerPassword(args.Length==0?"":args[0], playerName);
             }
             catch (Exception ex)
             {
-                Log.Error("Error attempting to change admin password: " + args[0], ex);
+                Log.Error("Error attempting to change server password: " + args[0], ex);
             }
         }
 
         public override bool VerifyArgs(string[] args)
         {
-            return args.Length >= 1;
+            return args.Length >= 0;
         }
 
-        private void ChangeAdminPassword(string password, string name)
+        private void ChangeServerPassword(string password, string name)
         {
-            serverConfig.ChangeAdminPassword(password);
-            Log.Info($"Admin password changed to \"{password}\" by {name}");
+            serverConfig.ChangeServerPassword(password);
+            Log.Info($"Server password changed to \"{password}\" by {name}");
         }
     }
 }
