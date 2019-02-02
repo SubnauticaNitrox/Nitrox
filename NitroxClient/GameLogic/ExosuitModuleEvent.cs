4e13c92d1fbd21557892cd3f5972ae01ade1d665
Allen SImpson <Allen.SImpson@bhpbilliton.com>
2019-02-02 22:02:40 +0800

    Added exosuit module events

--- NitroxClient/GameLogic/ExosuitModuleEvent.cs
+++ NitroxClient/GameLogic/ExosuitModuleEvent.cs
@@ -1,5 +1,6 @@
 ﻿using NitroxClient.Communication.Abstract;
 using NitroxClient.GameLogic.Helper;
+using NitroxModel.DataStructures.Util;
 using NitroxModel.Helper;
 using NitroxModel.Logger;
 using NitroxModel.Packets;
@@ -10,43 +11,12 @@ namespace NitroxClient.GameLogic
     public class ExosuitModuleEvent
     {
         private readonly IPacketSender packetSender;
+        private readonly IMultiplayerSession multiplayerSession;
 
-        public ExosuitModuleEvent(IPacketSender packetSender)
+        public ExosuitModuleEvent(IPacketSender packetSender, IMultiplayerSession multiplayerSession)
         {
             this.packetSender = packetSender;
-        }
-
-        public void BroadcastGrapplingHookStart(ExosuitGrapplingArm grapplingArm)
-        {
-            if (!string.IsNullOrEmpty(grapplingArm.gameObject.GetGuid()))
-            {
-                Log.Info("GRAPPLING: " + grapplingArm);
-                //string Guid = GuidHelper.GetGuid(grapplingArm.gameObject);
-                //ExosuitGrapplingAction Changed = new ExosuitGrapplingAction(Guid);
-               // packetSender.Send(Changed);
-            }
-        }
-
-        public void BroadcastTorpedoLaunch(ExosuitTorpedoArm torpedoArm, TorpedoType torpedoType, Transform siloTransform, bool verbosed)
-        {
-            if (!string.IsNullOrEmpty(torpedoArm.gameObject.GetGuid()))
-            {
-                
-                string Guid = GuidHelper.GetGuid(torpedoArm.gameObject);
-                ExosuitTorpedoAction Changed = new ExosuitTorpedoAction(torpedoType.techType, Guid, verbosed, Player.main.camRoot.GetAimingTransform().forward, Player.main.camRoot.GetAimingTransform().rotation);
-                packetSender.Send(Changed);
-            }
-        }
-
-        public void BroadcastClawArmUse(ExosuitClawArm clawArm)
-        {
-            if (!string.IsNullOrEmpty(clawArm.gameObject.GetGuid()))
-            {
-                string Guid = GuidHelper.GetGuid(clawArm.gameObject);
-                Log.Info("CLAW PUNCH: " + Guid);
-                ExosuitClawArmAction Changed = new ExosuitClawArmAction(Guid);
-                packetSender.Send(Changed);
-            }
+            this.multiplayerSession = multiplayerSession;
         }
 
         public void BroadcastSpawnedArm(Exosuit exo)
@@ -54,16 +24,16 @@ namespace NitroxClient.GameLogic
             string Guid = GuidHelper.GetGuid(exo.gameObject);
             if (!string.IsNullOrEmpty(Guid))
             {
-                string leftArmGuid = "8EBFDCE5-B4D6-4F16-85B8-58BA71ECED77";
-                string rightArmGuid = "D9624C09-ABA1-4B9E-AE77-A63F83ACD59A";
-
+                // This needs to be refactored to be done by the serverr, Since Every client will run this and the last one that does will sync all the guids...
                 IExosuitArm spawnedRArm = (IExosuitArm)exo.ReflectionGet("rightArm");
                 IExosuitArm spawnedLArm = (IExosuitArm)exo.ReflectionGet("leftArm");
 
                 GameObject spawnedRArmOb = spawnedRArm.GetGameObject();
+                string rightArmGuid = GuidHelper.GetGuid(spawnedRArmOb);
                 spawnedRArmOb.SetNewGuid(rightArmGuid);
 
                 GameObject spawnedLArmOb = spawnedLArm.GetGameObject();
+                string leftArmGuid = GuidHelper.GetGuid(spawnedLArmOb);
                 spawnedLArmOb.SetNewGuid(leftArmGuid);
 
                 ExosuitSpawnedArmAction Changed = new ExosuitSpawnedArmAction(Guid, leftArmGuid, rightArmGuid);
