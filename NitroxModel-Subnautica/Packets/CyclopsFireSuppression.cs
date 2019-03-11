6a0ab86e401d458a5073ab9c75fb81242d8828a1
Gharg <ghaarg@gmail.com>
2019-03-11 20:51:15 +0100

    add new files for fire suppression

---
+++ NitroxModel-Subnautica/Packets/CyclopsFireSuppression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NitroxModel.Packets;
+
+namespace NitroxModel_Subnautica.Packets
+{
+    [Serializable]
+    public class CyclopsFireSuppression : Packet
+    {
+        public string Guid { get; }
+
+        public CyclopsFireSuppression(string guid)
+        {
+            Guid = guid;
+        }
+
+        public override string ToString()
+        {
+            return "[CyclopsFireSuppressionSystem Guid: " + Guid + "]";
+        }
+    }
+}
+
