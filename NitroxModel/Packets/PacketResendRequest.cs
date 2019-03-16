981e2a102f147f935aac68e48d3459a224bed515
acidscorch <48223659+acidscorch@users.noreply.github.com>
2019-03-17 00:02:20 +0100

    Add files via upload

---
+++ NitroxModel/Packets/PacketResendRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace NitroxModel.Packets
+{
+    [Serializable]
+    public class PacketResendRequest : Packet
+    {
+        //public int Sequence { get; }
+
+        public PacketResendRequest(long sequence)
+        {
+            Sequence = sequence;
+        }
+
+        public override string ToString()
+        {
+            return "[PacketResendRequest Sequence:" + Sequence + "]";
+        }
+    }
+}
