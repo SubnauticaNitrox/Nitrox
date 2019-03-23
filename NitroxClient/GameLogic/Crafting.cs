8c5db0df1a95e2b8b179c86decf49b24aa62871d
acidscorch <acidscorch@gmail.com>
2019-03-23 07:54:42 +0100

    supress deconstruction by remote player if is in use

--- NitroxClient/GameLogic/Crafting.cs
+++ NitroxClient/GameLogic/Crafting.cs
@@ -16,6 +16,7 @@ namespace NitroxClient.GameLogic
         private System.Collections.Generic.Dictionary<string, GameLogic.CraftingUsageInfo> fabricatorsInUseByRemotePlayers = new System.Collections.Generic.Dictionary<string, GameLogic.CraftingUsageInfo>();
         private readonly System.Timers.Timer timer = new System.Timers.Timer(5000);
         private readonly int remoteSignalTimeOut = 120; //in seconds. 2 Minutes, because if someone crafts multiple items, no new starttime is sent and we don't want to interrupt remote player.
+        private Constructable _lastHoveredConstructable = null;
         private readonly IPacketSender packetSender;
         
         public Crafting(IPacketSender packetSender)
@@ -26,12 +27,12 @@ namespace NitroxClient.GameLogic
             timer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimerEvent);
             timer.Start();
         }
-             
+
         //Applies to Fabricator and Workbench
         public void GhostCrafter_Post_OnHandHover(GameObject gameObject, GUIHand hand)
         {
 #if TRACE && GAMEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("GhostCrafter_Pre_OnHandHover");
+            NitroxModel.Logger.Log.Debug("GhostCrafter_Post_OnHandHover");
 #endif
             //if Crafter is in use by remote Player > Display
             string _crafterGuid = GuidHelper.GetGuid(gameObject);
@@ -56,8 +57,6 @@ namespace NitroxClient.GameLogic
                 HandReticle.main.SetIcon(HandReticle.IconType.HandDeny, 1f);
             }
         }
-
-        
 
         //Applies to Fabricator and Workbench
         public bool GhostCrafter_Pre_OnHandClick(GameObject gameObject, GUIHand hand)
@@ -156,6 +155,234 @@ namespace NitroxClient.GameLogic
             }
         }
 
+        public bool CrafterLogic_Pre_OnTryPickupSingle(GameObject gameObject, TechType techTyp)
+        {
+#if TRACE && GAMEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("CrafterLogic_Pre_OnTryPickupSingle");
+#endif
+
+            bool _result = true;
+            string _crafterGuid = GuidHelper.GetGuid(gameObject);
+            lock (fabricatorsInUseByRemotePlayers)
+            {
+                if (fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid))
+                {
+                    _result = false;
+                }
+            }
+            return _result;
+        }
+
+        public void CrafterLogic_Post_OnTryPickupSingle(GameObject gameObject, bool success, TechType techType)
+        {
+#if TRACE && GAMEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("CrafterLogic_Post_OnTryPickupSingle");
+#endif
+
+            if (success)
+            {
+                string _crafterGuid = GuidHelper.GetGuid(gameObject);
+                FabricatorItemPickup _fabricatorItemPickup = new FabricatorItemPickup(_crafterGuid, techType.Model());
+                packetSender.Send(_fabricatorItemPickup);             
+            }
+        }
+
+        public bool Fabricator_Pre_OnCraftingEnd(GameObject gameObject)
+        {
+#if TRACE && GAMEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("Fabricator_Pre_OnCraftingEnd");
+#endif
+
+            bool _result = true;
+            string _crafterGuid = GuidHelper.GetGuid(gameObject);
+            lock (fabricatorsInUseByRemotePlayers)
+            {
+                if (fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid))
+                {
+                    _result = false;
+                }
+            }
+            if (_result)
+            {
+                FabricatorEndCrafting _fabricatorEnd = new FabricatorEndCrafting(_crafterGuid);
+                packetSender.Send(_fabricatorEnd);
+            }
+            return _result;
+        }
+
+        internal void Fabricator_Remote_OnCraftingEnd(string fabricatorGuid)
+        {
+#if TRACE && REMOTEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("Fabricator_Remote_OnCraftingEnd");
+#endif
+
+            bool _inUseByOther = false;
+            lock (fabricatorsInUseByRemotePlayers)
+            {
+                _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(fabricatorGuid);
+            }
+            if (_inUseByOther)
+            {
+                GameObject gameObject = GuidHelper.RequireObjectFrom(fabricatorGuid);
+                CrafterLogic crafterLogic = gameObject.RequireComponentInChildren<CrafterLogic>(true);
+                crafterLogic.Reset();
+            }
+        }
+
+        public void Fabricator_Post_OnCraftingBegin(GameObject crafter, TechType techType, float duration)
+        {
+#if TRACE && GAMEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("Fabricator_Post_OnCraftingBegin");
+#endif
+
+            string _crafterGuid = GuidHelper.GetGuid(crafter);
+            FabricatorBeginCrafting _fabricatorBeginCrafting = new FabricatorBeginCrafting(_crafterGuid, techType.Model(), duration);
+            packetSender.Send(_fabricatorBeginCrafting);
+        }
+
+        internal void Fabricator_Remote_OnCraftingBegin(string fabricatorGuid, NitroxModel.DataStructures.TechType techType, float duration)
+        {
+#if TRACE && REMOTEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("Fabricator_Remote_OnCraftingBegin");
+#endif
+            GameObject gameObject = GuidHelper.RequireObjectFrom(fabricatorGuid);
+            Fabricator fabricator = gameObject.RequireComponentInChildren<Fabricator>(true);
+            FieldInfo logic = typeof(Crafter).GetField("_logic", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (logic != null)
+            {
+                CrafterLogic crafterLogic = (CrafterLogic)logic.GetValue(fabricator);
+                crafterLogic.Craft(techType.Enum(), duration);
+            }
+        }
+
+        internal void Fabricator_Remote_ItemPickup(string fabricatorGuid, NitroxModel.DataStructures.TechType techType)
+        {
+#if TRACE && REMOTEEVENTCRAFTING
+            NitroxModel.Logger.Log.Debug("Fabricator_Remote_ItemPickup");
+#endif
+
+            bool _inUseByOther = false;
+            lock (fabricatorsInUseByRemotePlayers)
+            {
+                _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(fabricatorGuid);
+            }
+            if(_inUseByOther)
+            {
+                GameObject gameObject = GuidHelper.RequireObjectFrom(fabricatorGuid);
+                CrafterLogic crafterLogic = gameObject.RequireComponentInChildren<CrafterLogic>(true);
+
+                if (crafterLogic.numCrafted > 0)
+                {
+                    crafterLogic.numCrafted--;
+
+                    if (crafterLogic.numCrafted == 0)
+                    {
+                        crafterLogic.Reset();
+                    }
+                }
+            }
+        }
+
+        //Applies to Fabricator and Workbench
+        public bool BuilderTool_Pre_OnHover(GameObject gameObject, Constructable constructable)
+        {
+#if TRACE && GAMEEVENTBUILDING
+            NitroxModel.Logger.Log.Debug("BuilderTool_Pre_OnHover");
+#endif
+            if (constructable.techType == TechType.Fabricator || constructable.techType == TechType.Workbench)
+            {
+                string _crafterGuid = GuidHelper.GetGuid(constructable.gameObject);
+                bool _inUseByOther = false;
+                string _playerName = string.Empty;
+                lock (fabricatorsInUseByRemotePlayers)
+                {
+                    _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid);
+                    if (_inUseByOther)
+                    {
+                        _playerName = fabricatorsInUseByRemotePlayers[_crafterGuid].PlayerName;
+                    }
+                }
+                if (_inUseByOther && _crafterGuid != string.Empty)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //Applies to Fabricator and Workbench
+        public void BuilderTool_Post_OnHover(GameObject gameObject, Constructable constructable)
+        {
+#if TRACE && GAMEEVENTBUILDING
+            NitroxModel.Logger.Log.Debug("BuilderTool_Post_OnHover");
+#endif
+            if (constructable.techType == TechType.Fabricator || constructable.techType == TechType.Workbench)
+            {
+                //if Crafter is in use by remote Player > Display
+                _lastHoveredConstructable = constructable;
+                string _crafterGuid = GuidHelper.GetGuid(constructable.gameObject);
+                bool _inUseByOther = false;
+                string _playerName = string.Empty;
+
+                lock (fabricatorsInUseByRemotePlayers)
+                {
+                    _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid);
+                    if (_inUseByOther)
+                    {
+                        _playerName = fabricatorsInUseByRemotePlayers[_crafterGuid].PlayerName;
+                    }
+                }
+
+                if (_inUseByOther && _crafterGuid != string.Empty)
+                {
+                    string _baseString = "In use by";
+                    //TODO: Translate _baseString
+
+                    //
+                    string _displayText = _baseString + " " + _playerName;
+                    HandReticle.main.SetInteractText(_displayText, string.Empty);
+                    HandReticle.main.SetInteractInfo(_displayText, string.Empty); //must be used, otherwise it is not displayed
+                    HandReticle.main.SetIcon(HandReticle.IconType.HandDeny, 1f);
+                }
+            }
+            else
+            {
+                _lastHoveredConstructable = null;
+            }
+        }
+
+        public bool BuilderTool_Pre_HandleInput(GameObject gameObject)
+        {
+#if TRACE && GAMEEVENTBUILDING
+            NitroxModel.Logger.Log.Debug("BuilderTool_Pre_HandleInput");
+#endif
+            if (_lastHoveredConstructable != null)
+            {
+                //if Crafter is in use by remote Player, supress deconstruction
+                string _crafterGuid = GuidHelper.GetGuid(_lastHoveredConstructable.gameObject);
+                bool _inUseByOther = false;
+                string _playerName = string.Empty;
+
+                lock (fabricatorsInUseByRemotePlayers)
+                {
+                    _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid);
+                    if (_inUseByOther)
+                    {
+                        _playerName = fabricatorsInUseByRemotePlayers[_crafterGuid].PlayerName;
+                    }
+                }
+
+                if (_inUseByOther && _crafterGuid != string.Empty)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            else
+            { return true; }
+        }
+        
         //If Crafters are locked and something unplanned happend (wrong packet-order, etc.) > check if remoteSignalTimeOut is elapsed and reset Crafter
         private void OnTimerEvent(object sender, System.Timers.ElapsedEventArgs args)
         {
@@ -181,7 +408,7 @@ namespace NitroxClient.GameLogic
                 CrafterLogic _crafterLogic = _gameObject.GetComponentInChildren<CrafterLogic>(true);
                 _crafterLogic.Reset();
             }
-            
+
             //remove the guids after the .Reset so the Crafter.OnOpenedChange doesn't send ClosedByMe Notifys to other Players
             lock (fabricatorsInUseByRemotePlayers)
             {
@@ -227,134 +454,6 @@ namespace NitroxClient.GameLogic
             }
         }
 
-        public void Fabricator_Post_OnCraftingBegin(GameObject crafter, TechType techType, float duration)
-        {
-#if TRACE && GAMEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("Fabricator_Post_OnCraftingBegin");
-#endif
-
-            string _crafterGuid = GuidHelper.GetGuid(crafter);
-            FabricatorBeginCrafting _fabricatorBeginCrafting = new FabricatorBeginCrafting(_crafterGuid, techType.Model(), duration);
-            packetSender.Send(_fabricatorBeginCrafting);
-        }
-
-        internal void Fabricator_Remote_OnCraftingBegin(string fabricatorGuid, NitroxModel.DataStructures.TechType techType, float duration)
-        {
-#if TRACE && REMOTEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("Fabricator_Remote_OnCraftingBegin");
-#endif
-            GameObject gameObject = GuidHelper.RequireObjectFrom(fabricatorGuid);
-            Fabricator fabricator = gameObject.RequireComponentInChildren<Fabricator>(true);
-            FieldInfo logic = typeof(Crafter).GetField("_logic", BindingFlags.Instance | BindingFlags.NonPublic);
-            if (logic != null)
-            {
-                CrafterLogic crafterLogic = (CrafterLogic)logic.GetValue(fabricator);
-                crafterLogic.Craft(techType.Enum(), duration);
-            }
-        }
-
-        public bool Fabricator_Pre_OnCraftingEnd(GameObject gameObject)
-        {
-#if TRACE && GAMEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("Fabricator_Pre_OnCraftingEnd");
-#endif
-
-            bool _result = true;
-            string _crafterGuid = GuidHelper.GetGuid(gameObject);
-            lock(fabricatorsInUseByRemotePlayers)
-            {
-                if(fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid))
-                {
-                    _result = false;
-                }
-            }
-            if (_result)
-            {
-                FabricatorEndCrafting _fabricatorEnd = new FabricatorEndCrafting(_crafterGuid);
-                packetSender.Send(_fabricatorEnd);
-            }
-            return _result;
-        }
-
-        internal void Fabricator_Remote_OnCraftingEnd(string fabricatorGuid)
-        {
-#if TRACE && REMOTEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("Fabricator_Remote_OnCraftingEnd");
-#endif
-
-            bool _inUseByOther = false;
-            lock (fabricatorsInUseByRemotePlayers)
-            {
-                _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(fabricatorGuid);
-            }
-            if (_inUseByOther)
-            {
-                GameObject gameObject = GuidHelper.RequireObjectFrom(fabricatorGuid);
-                CrafterLogic crafterLogic = gameObject.RequireComponentInChildren<CrafterLogic>(true);
-                crafterLogic.Reset();
-            }
-        }
-
-        public bool CrafterLogic_Pre_OnTryPickupSingle(GameObject gameObject, TechType techTyp)
-        {
-#if TRACE && GAMEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("CrafterLogic_Pre_OnTryPickupSingle");
-#endif
-
-            bool _result = true;
-            string _crafterGuid = GuidHelper.GetGuid(gameObject);
-            lock (fabricatorsInUseByRemotePlayers)
-            {
-                if (fabricatorsInUseByRemotePlayers.ContainsKey(_crafterGuid))
-                {
-                    _result = false;
-                }
-            }
-            return _result;
-        }
-
-        public void CrafterLogic_Post_OnTryPickupSingle(GameObject gameObject, bool success, TechType techType)
-        {
-#if TRACE && GAMEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("CrafterLogic_Post_OnTryPickupSingle");
-#endif
-
-            if (success)
-            {
-                string _crafterGuid = GuidHelper.GetGuid(gameObject);
-                FabricatorItemPickup _fabricatorItemPickup = new FabricatorItemPickup(_crafterGuid, techType.Model());
-                packetSender.Send(_fabricatorItemPickup);                
-            }
-        }
-
-        internal void Fabricator_Remote_ItemPickup(string fabricatorGuid, NitroxModel.DataStructures.TechType techType)
-        {
-#if TRACE && REMOTEEVENTCRAFTING
-            NitroxModel.Logger.Log.Debug("Fabricator_Remote_ItemPickup");
-#endif
-
-            bool _inUseByOther = false;
-            lock (fabricatorsInUseByRemotePlayers)
-            {
-                _inUseByOther = fabricatorsInUseByRemotePlayers.ContainsKey(fabricatorGuid);
-            }
-            if(_inUseByOther)
-            {
-                GameObject gameObject = GuidHelper.RequireObjectFrom(fabricatorGuid);
-                CrafterLogic crafterLogic = gameObject.RequireComponentInChildren<CrafterLogic>(true);
-
-                if (crafterLogic.numCrafted > 0)
-                {
-                    crafterLogic.numCrafted--;
-
-                    if (crafterLogic.numCrafted == 0)
-                    {
-                        crafterLogic.Reset();
-                    }
-                }
-            }
-        }
-                      
     }
 
     internal class CraftingUsageInfo
---
+++ NitroxPatcher/Patches/BuilderTool_Patch.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using NitroxClient.GameLogic;
+using NitroxModel.Core;
+using NitroxModel.Helper;
+using UnityEngine;
+
+namespace NitroxPatcher.Patches
+{
+    public class BuilderTool_OnHover_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(BuilderTool);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("OnHover", BindingFlags.NonPublic | BindingFlags.Instance,
+                                                                                 null, new[] { typeof(Constructable) }, null);
+
+        public static bool Prefix(BuilderTool __instance, bool __result, Constructable constructable)
+        {
+            bool _result = true;
+
+            _result = NitroxServiceLocator.LocateService<Crafting>().BuilderTool_Pre_OnHover(__instance.gameObject, constructable);
+            if (_result == false)
+            {
+                return _result;
+            }
+            //Add more checks here later for other Objects
+
+            return _result;
+        }
+
+        public static void Postfix(BuilderTool __instance, Constructable constructable)
+        {
+            if (constructable.techType == TechType.Fabricator || constructable.techType == TechType.Workbench)
+            {
+                NitroxServiceLocator.LocateService<Crafting>().BuilderTool_Post_OnHover(__instance.gameObject, constructable);
+            }
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPostfix(harmony, TARGET_METHOD);
+        }
+    }
+
+
+    public class BuilderTool_HandleInput_Patch : NitroxPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(BuilderTool);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("HandleInput", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static readonly OpCode INJECTION_OPCODE = OpCodes.Callvirt;
+        public static readonly object INJECTION_OPERAND = typeof(Constructable).GetMethod("SetState", BindingFlags.Public | BindingFlags.Instance);
+
+        public static bool Prefix(BuilderTool __instance)
+        {
+            bool _result = true;
+
+            _result = NitroxServiceLocator.LocateService<Crafting>().BuilderTool_Pre_HandleInput(__instance.gameObject);
+            if (_result == false)
+            {
+                return _result;
+            }
+            //Add more checks here later for other Objects
+
+            return _result;
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions)
+        {
+            Validate.NotNull(INJECTION_OPERAND);
+
+            foreach (CodeInstruction instruction in instructions)
+            {
+                yield return instruction;
+                if (instruction.opcode.Equals(INJECTION_OPCODE) && instruction.operand.Equals(INJECTION_OPERAND))
+                {
+                    /*
+                     * Multiplayer.Logic.Building.DeconstructionBegin(constructable.gameObject);
+                     */
+                    yield return TranspilerHelper.LocateService<Building>();
+                    yield return original.Ldloc<Constructable>();
+                    yield return new CodeInstruction(OpCodes.Callvirt, typeof(Component).GetMethod("get_gameObject", BindingFlags.Instance | BindingFlags.Public));
+                    yield return new CodeInstruction(OpCodes.Callvirt, typeof(Building).GetMethod("DeconstructionBegin", BindingFlags.Public | BindingFlags.Instance));
+                }
+            }
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchMultiple(harmony, TARGET_METHOD,true,false,true);
+        }
+    }
+}
