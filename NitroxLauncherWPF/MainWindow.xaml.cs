5ac2c2c387843010ac56a6b0bd843f7ab9453f08
Ghaarg <gharggames@gmail.com>
2019-04-01 20:25:09 +0200

    Main window and most of launch game page + options page start

--- NitroxLauncherWPF/MainWindow.xaml.cs
+++ NitroxLauncherWPF/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using XamlCSS.WPF;
 
 namespace NitroxLauncherWPF
 {
@@ -21,10 +20,55 @@ namespace NitroxLauncherWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        LaunchGamePage launchPage = new LaunchGamePage();
+        OptionPage optionPage = new OptionPage();
+
         public MainWindow()
         {
-            XamlCSS.WPF.Css.Initialize();
             InitializeComponent();
+            MainPage.Content = launchPage;
+        }        
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Minimze_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ToPlayGame_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainPage.Content = launchPage;
+            SetDeActive(OptionsNav);
+            SetActive(PlayGameNav);
+        }
+
+        private void ToOptions_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainPage.Content = optionPage;
+            SetActive(OptionsNav);
+            SetDeActive(PlayGameNav);
+        }
+
+        private void SetDeActive(Button button)
+        {
+            if (button.Content is TextBlock block)
+            {
+                block.FontWeight = FontWeights.Normal;
+                var bc = new BrushConverter();
+                block.Foreground = (Brush)bc.ConvertFrom("#B2FFFFFF");
+            }
+        }
+        private void SetActive(Button button)
+        {
+            if (button.Content is TextBlock block)
+            {
+                block.FontWeight = FontWeights.Bold;
+                block.Foreground = Brushes.White;
+            }
         }
     }
 }
