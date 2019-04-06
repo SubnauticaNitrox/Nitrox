a764721afd58f8ead5a77ea0586007b8f392780a
killzoms <killzoms@users.noreply.github.com>
2019-04-06 12:49:34 -0400

    Update ReefbackSpawnData.cs

--- NitroxServer-Subnautica/GameLogic/Entities/Spawning/ReefbackSpawnData.cs
+++ NitroxServer-Subnautica/GameLogic/Entities/Spawning/ReefbackSpawnData.cs
@@ -22,9 +22,9 @@ namespace NitroxServer_Subnautica.GameLogic.Entities.Spawning.EntityBootstrapper
             new ReefbackEntity() { techType = TechType.Bladderfish, probability = 1, minNumber = 1, maxNumber = 2, classId = "bf9ccd04-60af-4144-aaa1-4ac184c686c2" },
             new ReefbackEntity() { techType = TechType.Eyeye, probability = 1, minNumber = 1, maxNumber = 1, classId = "79c1aef0-e505-469c-ab36-c22c76aeae44" },
             new ReefbackEntity() { techType = TechType.HoleFish, probability = 1, minNumber = 1, maxNumber = 1, classId = "495befa0-0e6b-400d-9734-227e5a732f75" },
-            new ReefbackEntity() { techType = TechType.Hoopfish, probability = 1, minNumber = 1, maxNumber = 1, classId = "08cb3290-504b-4191-97ee-6af1588af5c0" },
+            new ReefbackEntity() { techType = TechType.Hoopfish, probability = 1, minNumber = 1, maxNumber = 1, classId = "284ceeb6-b437-4aca-a8bd-d54f336cbef8" },
             new ReefbackEntity() { techType = TechType.Reginald, probability = 1, minNumber = 1, maxNumber = 2, classId = "8e82dc63-5991-4c63-a12c-2aa39373a7cf" },
-            new ReefbackEntity() { techType = TechType.Spadefish, probability = 1, minNumber = 1, maxNumber = 2, classId = "e82d3c24-5a58-4307-a775-4741050c8a78" },
+            new ReefbackEntity() { techType = TechType.Spadefish, probability = 1, minNumber = 1, maxNumber = 2, classId = "d040bec1-0368-4f7c-aed6-93b5e1852d45" },
             new ReefbackEntity() { techType = TechType.Biter, probability = 0.5f, minNumber = 1, maxNumber = 3, classId = "4064a71a-c464-4db2-942a-56391fe69951" },
             new ReefbackEntity() { techType = TechType.HoopfishSchool, probability = 1, minNumber = 3, maxNumber = 3, classId = "08cb3290-504b-4191-97ee-6af1588af5c0" }
         };
