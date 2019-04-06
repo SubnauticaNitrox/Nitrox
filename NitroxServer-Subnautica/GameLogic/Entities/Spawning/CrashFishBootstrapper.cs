d3837fe1b228575082207e11fcf74abcf6e9b2ae
Sunrunner37 <sunrunner@nitroxmod.com>
2019-04-06 16:04:39 +0200

    Tweaked crashfish bootstrapping to spawn crash powder again.

--- NitroxServer-Subnautica/GameLogic/Entities/Spawning/CrashFishBootstrapper.cs
+++ NitroxServer-Subnautica/GameLogic/Entities/Spawning/CrashFishBootstrapper.cs
@@ -9,9 +9,6 @@ namespace NitroxServer_Subnautica.GameLogic.Entities.Spawning.EntityBootstrapper
     {
         public void Prepare(Entity parentEntity, DeterministicBatchGenerator deterministicBatchGenerator)
         {
-            Entity crashFish = SpawnChild(parentEntity, deterministicBatchGenerator, TechType.Crash, "7d307502-46b7-4f86-afb0-65fe8867f893");
-            parentEntity.ChildEntities.Add(crashFish);
-
             Entity crashPower = SpawnChild(parentEntity, deterministicBatchGenerator, TechType.CrashPowder, "1ce074ee-1a58-439b-bb5b-e5e3d9f0886f");
             parentEntity.ChildEntities.Add(crashPower);
         }
