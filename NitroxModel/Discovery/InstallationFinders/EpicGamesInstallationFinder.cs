1e966e568e4a507fc370cf50b08fa46944d53ce6
Measurity <measuring.infinity@gmail.com>
2020-01-26 03:27:54 +0100

    Added error logs (and one debug log) to new Epic Games installation finder

--- NitroxModel/Discovery/InstallationFinders/EpicGamesInstallationFinder.cs
+++ NitroxModel/Discovery/InstallationFinders/EpicGamesInstallationFinder.cs
@@ -1,36 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
-using Microsoft.Win32;
 using NitroxModel.DataStructures.Util;
+using NitroxModel.Logger;
 
 namespace NitroxModel.Discovery.InstallationFinders
 {
     public class EpicGamesInstallationFinder : IFindGameInstallation
     {
-        private Regex installLocationRegex = new Regex("\"InstallLocation\"[^\"]*\"(.*)\",");
+        private readonly Regex installLocationRegex = new Regex("\"InstallLocation\"[^\"]*\"(.*)\",?");
 
         public Optional<string> FindGame(List<string> errors = null)
         {
             string epicGamesManifestsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @"Epic\EpicGamesLauncher\Data\Manifests");
-
-            if (Directory.Exists(epicGamesManifestsDir))
+            if (!Directory.Exists(epicGamesManifestsDir))
             {
-                string[] files = Directory.GetFiles(epicGamesManifestsDir, "*.item");
-                foreach (string file in files)
+                errors?.Add("Epic games manifest directory does not exist. Verify that Epic Games Store has been installed.");
+                return Optional<string>.Empty();
+            }
+
+            string[] files = Directory.GetFiles(epicGamesManifestsDir, "*.item");
+            foreach (string file in files)
+            {
+                string fileText = File.ReadAllText(file);
+                Match match = installLocationRegex.Match(fileText);
+                if (fileText.Contains("Subnautica") && match.Success)
                 {
-                    string fileText = File.ReadAllText(file);
-                    Match match = installLocationRegex.Match(fileText);
-                    if (fileText.Contains("Subnautica") && match.Success)
-                    {
-                        Console.WriteLine(match.Groups[1].Value);
-                        return Optional<string>.Of(match.Groups[1].Value);
-                    }
+                    Log.Debug($"Found Subnautica install path in '{Path.GetFullPath(file)}'. Full pattern match: '{match.Value}'");
+                    return Optional<string>.Of(match.Groups[1].Value);
                 }
             }
 
+            errors?.Add("Could not find Subnautica installation directory from Epic Games installation records. Verify that Subnautica has been installed with Epic Games Store.");
             return Optional<string>.Empty();
         }
     }
