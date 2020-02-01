32a2a37cc3bbef0d386897857235b4be4ca214f6
grandoth <grandoth@gmail.com>
2020-02-01 02:13:26 -0500

    Add and link a common AssemblyInfo into each project. This allows a single file to control common attributes, such as Version #, Copyright, etc Align all AssemblyInfo.cs files so they are easily differentiable Added a generated guid although likely unused based on COM comment...meh, now they're all the same

--- NitroxTest/Properties/AssemblyInfo.cs
+++ NitroxTest/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+
 using System.Runtime.InteropServices;
+
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,16 +9,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NitroxTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+// COMMON: [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NitroxTest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// COMMON: [assembly: AssemblyCopyright("")]
+// COMMON: [assembly: AssemblyTrademark("")]
+// COMMON: [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// COMMON: [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7787384e-3b08-4d6f-9c0e-a9e3701ba9f1")]
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// COMMON: [assembly: AssemblyVersion("X.X.X.X")]
+// COMMON: [assembly: AssemblyFileVersion("X.X.X.X")]
