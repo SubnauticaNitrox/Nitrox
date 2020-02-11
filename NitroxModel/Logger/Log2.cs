86fed328c0e00e3591784c30721ba037d7afe087
80cmdude <s80cmdude@gmail.com>
2020-02-11 21:00:24 +0000

    #913 Added NLog and supporting instance class

---
+++ NitroxModel/Logger/Log2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace NitroxModel.Logger
+{
+    public interface ILogger
+    {
+        void log(string message);
+    }
+    public sealed class Log2 : ILogger
+    {
+        // Instance
+        private static ILogger instance;
+        public static ILogger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Log2();
+                }
+                return instance;
+            }
+        }
+
+        // Private  vars
+        private readonly NLog.Logger logger;
+
+        // Public API
+        public Log2()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+
+            ConfigureLogging();
+        }
+
+        public void log(string message)
+        {
+            logger.Info(message);
+        }
+
+        private void ConfigureLogging()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "logfile.txt" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            LogManager.Configuration = config;
+        }
+    }
+}
