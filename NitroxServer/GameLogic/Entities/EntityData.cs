69037a493743d226cd65a0b7c00efa6cd2291c0b
Sunrunner37 <sunrunner@nitroxmod.com>
2020-03-03 20:03:21 -0800

    Fix for child entities not persisting across a server restart.

--- NitroxServer/GameLogic/Entities/EntityData.cs
+++ NitroxServer/GameLogic/Entities/EntityData.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using NitroxModel.DataStructures;
 using NitroxModel.DataStructures.GameLogic;
 using ProtoBufNet;
 
@@ -9,6 +11,28 @@ namespace NitroxServer.GameLogic.Entities
     {
         [ProtoMember(1)]
         public List<Entity> Entities = new List<Entity>();
+
+        [ProtoAfterDeserialization]
+        public void ProtoAfterDeserialization()
+        {
+            // After deserialziation, we want to assign all of the 
+            // children to their respective parent entities.
+            Dictionary<NitroxId, Entity> entitiesById = Entities.ToDictionary(entity => entity.Id);
+
+            foreach(Entity entity in Entities)
+            {
+                if(entity.ParentId != null)
+                {
+                    Entity parent = entitiesById[entity.ParentId];
+
+                    if(parent != null)
+                    {
+                        parent.ChildEntities.Add(entity);
+                        entity.Transform.SetParent(parent.Transform);
+                    }
+                }
+            }
+        }
 
         public static EntityData From(List<Entity> entities)
         {
