56b035f72258b66a524fdcf0b78d01ad6cf0efd4
Jannify <Kannst-Du-mir-folgen@gmx.de>
2020-03-31 17:03:49 +0200

    Moving NitroxUnity from submodule to simple folder

---
+++ NitroxUnity/Assets/Editor/CreateAssetsBundle.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    public class CreateAssetBundles
+    {
+        [MenuItem("Assets/Build AssetBundles")]
+        private static void BuildAllAssetBundles()
+        {
+            var assetBundleDirectory = "Assets/AssetBundles";
+            if (Directory.Exists(assetBundleDirectory))
+            {
+                Directory.Delete(assetBundleDirectory, true);
+            }
+            Directory.CreateDirectory(assetBundleDirectory);
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+                BuildTarget.StandaloneWindows);
+        }
+    }
+}
