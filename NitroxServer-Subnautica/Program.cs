5e11732f9c4e3c7cfa4e543348a5f262e33e8f70
killzoms <killzoms@users.noreply.github.com>
2020-04-22 10:31:20 -0400

    Update Program.cs

--- NitroxServer-Subnautica/Program.cs
+++ NitroxServer-Subnautica/Program.cs
@@ -91,14 +91,14 @@ namespace NitroxServer_Subnautica
             // better catch using WinAPI. This will handled process kill
             if (platid == PlatformID.Win32NT)
             {
-                SetConsoleCtrlHandler(_ConsoleCtrlCheckDelegate, true);
+                SetConsoleCtrlHandler(consoleCtrlCheckDelegate, true);
             }
         }
 
         [DllImport("kernel32.dll", SetLastError = true)]
         private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
 
-        private static ConsoleEventDelegate _ConsoleCtrlCheckDelegate = ConsoleEventCallback; // local variable was getting garbage collected
+        private static ConsoleEventDelegate consoleCtrlCheckDelegate = ConsoleEventCallback; // local variable was getting garbage collected
 
         private static bool ConsoleEventCallback(int eventType)
         {
