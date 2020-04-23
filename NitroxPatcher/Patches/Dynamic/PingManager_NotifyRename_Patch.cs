093e22be1bcaff0bdddaf262255c6741986f303e
Measurity <measuring.infinity@gmail.com>
2020-04-24 01:40:31 +0200

    Fixed NRE in ping rename

--- NitroxPatcher/Patches/Dynamic/PingManager_NotifyRename_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/PingManager_NotifyRename_Patch.cs
@@ -4,7 +4,10 @@ using Harmony;
 using NitroxClient.Communication.Abstract;
 using NitroxClient.GameLogic.Helper;
 using NitroxClient.MonoBehaviours;
+using NitroxClient.Unity.Helper;
 using NitroxModel.Core;
+using NitroxModel.DataStructures;
+using NitroxModel.Logger;
 using NitroxModel.Packets;
 
 namespace NitroxPatcher.Patches.Dynamic
@@ -16,11 +19,20 @@ namespace NitroxPatcher.Patches.Dynamic
 
         public static void Postfix(PingInstance instance)
         {
-            if (!instance)
+            // Only beacons are synced here (not mission, vehicle or other signals) because spawning is handled differently for non-droppable entities
+            if (!instance || !instance.GetComponent<Beacon>())
             {
                 return;
             }
-            NitroxServiceLocator.LocateService<IPacketSender>().Send(new PingRenamed(NitroxEntity.GetId(instance.gameObject), instance.GetLabel(), SerializationHelper.GetBytes(instance.gameObject)));
+            NitroxId nitroxId = NitroxEntity.GetId(instance.gameObject);
+            if (nitroxId == null)
+            {
+                Log.Error($"Missing Nitrox id on beacon: {instance.GetHierarchyPath()}");
+                return;
+            }
+            
+            PingRenamed packet = new PingRenamed(nitroxId, instance.GetLabel(), SerializationHelper.GetBytes(instance.gameObject));
+            NitroxServiceLocator.LocateService<IPacketSender>().Send(packet);
         }
 
         public override void Patch(HarmonyInstance harmony)
