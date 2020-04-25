ee597092fdd2d6167e2cc891823b7cb313ef783a
Measurity <measuring.infinity@gmail.com>
2020-04-25 03:08:52 +0200

    Fixed CyclopsLightingPanel trying to get NitroxID from wrong GO
    It checked the instance.gameObject which is the LightsControl sub GO of the Cyclops GO.
    So it's fixed by getting the parent of the instance.gameObject, being the Cyclops main GO.

--- NitroxPatcher/Patches/Dynamic/CyclopsLightingPanel_OnSubConstructionComplete_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/CyclopsLightingPanel_OnSubConstructionComplete_Patch.cs
@@ -3,8 +3,11 @@ using System.Reflection;
 using Harmony;
 using NitroxClient.GameLogic;
 using NitroxClient.MonoBehaviours;
+using NitroxClient.Unity.Helper;
 using NitroxModel.Core;
 using NitroxModel.DataStructures;
+using NitroxModel.DataStructures.Util;
+using NitroxModel.Logger;
 using NitroxModel_Subnautica.DataStructures.GameLogic;
 using UnityEngine;
 
@@ -18,11 +21,16 @@ namespace NitroxPatcher.Patches.Dynamic
         public static bool Prefix(CyclopsLightingPanel __instance)
         {
             // Suppress powered on if a cyclops´s floodlight is set to false            
-            GameObject gameObject = __instance.gameObject;
+            GameObject gameObject = __instance.gameObject.transform.parent.gameObject; // GO = LightsControl, Parent = main cyclops game object
             NitroxId id = NitroxEntity.GetId(gameObject);
-            CyclopsModel model = NitroxServiceLocator.LocateService<Vehicles>().GetVehicles<CyclopsModel>(id);
-            
-            return model.FloodLightsOn;
+            Optional<CyclopsModel> model = NitroxServiceLocator.LocateService<Vehicles>().TryGetVehicle<CyclopsModel>(id);
+            if (!model.HasValue)
+            {
+                Log.Error($"{nameof(CyclopsLightingPanel_OnSubConstructionComplete_Patch)}: Could not find {nameof(CyclopsModel)} by Nitrox id {id}.\nGO containing wrong id: {__instance.GetHierarchyPath()}");
+                return false;
+            }
+
+            return model.Value.FloodLightsOn;
         }
 
         public override void Patch(HarmonyInstance harmony)
