32ec5e8ea1937eafc353f3d218af6d693bef85f9
_HeN_ <drhen123@googlemail.com>
2020-04-28 20:19:16 +0100

    Removed redundant files

--- NitroxPatcher/Patches/Dynamic/PrecursorDoorway_ToggleDoor_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/PrecursorDoorway_ToggleDoor_Patch.cs
@@ -5,19 +5,12 @@ using NitroxClient.MonoBehaviours;
 using NitroxModel.Core;
 using NitroxModel.DataStructures;
 using NitroxModel.DataStructures.GameLogic.Entities.Metadata;
-using NitroxModel.Logger;
 
 namespace NitroxPatcher.Patches.Dynamic
 {
     class PrecursorDoorway_ToggleDoor_Patch : NitroxPatch, IDynamicPatch
     {
         public static readonly MethodInfo TARGET_METHOD = typeof(PrecursorDoorway).GetMethod(nameof(PrecursorDoorway.ToggleDoor), BindingFlags.Public | BindingFlags.Instance);
-
-        public static void Prefix(PrecursorDoorway __instance, bool open)
-        {
-            NitroxEntity entity = __instance.GetComponent<NitroxEntity>();
-            NitroxServiceLocator.LocateService<PrecursorManager>().TogglePrecursorDoor(entity.Id, open);
-        }
 
         public static void Postfix(PrecursorDoorway __instance)
         {
@@ -26,7 +19,6 @@ namespace NitroxPatcher.Patches.Dynamic
 
             Entities entities = NitroxServiceLocator.LocateService<Entities>();
             entities.BroadcastMetadataUpdate(id, precursorDoorwayMetadata);
-            Log.Debug("Precursor toggle door metadata sent id " + id + " value " + __instance.isOpen);
         }
 
         public override void Patch(HarmonyInstance harmony)
