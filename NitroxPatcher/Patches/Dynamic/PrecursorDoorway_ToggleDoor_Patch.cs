8bf0c6a1e909b36bb06d87f5e0b1f93e4cb938e7
Coding-Hen <drhen123@googlemail.com>
2020-04-29 00:52:48 +0100

    Added doorway sync for precursor doorways (#1032)
    * Added doorway sync for precursor doorways
    
    * Removed multiplayerSession
    
    * Removed unneeded using
    
    * Made suggested changes
    
    * Removed acidental changes
    
    * Added logging as suggested
    
    * Changed to getobjectfrom to remove valid check and give valuable error message
    
    * Implement with metadata
    
    * Add metadata rework
    
    * Fixed metadata infinite call
    
    * Removed redundant files
    
    * Fixed build and removed unneeded imports

---
+++ NitroxPatcher/Patches/Dynamic/PrecursorDoorway_ToggleDoor_Patch.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Harmony;
+using NitroxClient.GameLogic;
+using NitroxClient.MonoBehaviours;
+using NitroxModel.Core;
+using NitroxModel.DataStructures;
+using NitroxModel.DataStructures.GameLogic.Entities.Metadata;
+
+namespace NitroxPatcher.Patches.Dynamic
+{
+    class PrecursorDoorway_ToggleDoor_Patch : NitroxPatch, IDynamicPatch
+    {
+        public static readonly MethodInfo TARGET_METHOD = typeof(PrecursorDoorway).GetMethod(nameof(PrecursorDoorway.ToggleDoor), BindingFlags.Public | BindingFlags.Instance);
+
+        public static void Postfix(PrecursorDoorway __instance)
+        {
+            NitroxId id = NitroxEntity.GetId(__instance.gameObject);
+            PrecursorDoorwayMetadata precursorDoorwayMetadata = new PrecursorDoorwayMetadata(__instance.isOpen);
+
+            Entities entities = NitroxServiceLocator.LocateService<Entities>();
+            entities.BroadcastMetadataUpdate(id, precursorDoorwayMetadata);
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPostfix(harmony, TARGET_METHOD);
+        }
+    }
+}
