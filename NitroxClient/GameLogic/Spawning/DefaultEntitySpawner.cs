3c8b7a601de339e365a8cce6439018e912b636f5
Sunrunner37 <sunrunner@nitroxmod.com>
2020-05-02 23:21:57 -0700

    Fixed a bug that generic loot containers never had their position set.  (These are spawned if we could not find the prefab using a classId or techType)

--- NitroxClient/GameLogic/Spawning/DefaultEntitySpawner.cs
+++ NitroxClient/GameLogic/Spawning/DefaultEntitySpawner.cs
@@ -13,18 +13,8 @@ namespace NitroxClient.GameLogic.Spawning
         public Optional<GameObject> Spawn(Entity entity, Optional<GameObject> parent, EntityCell cellRoot)
         {
             TechType techType = entity.TechType.Enum();
-            GameObject prefab;
-            IPrefabRequest prefabRequest = PrefabDatabase.GetPrefabAsync(entity.ClassId);
-            if (!prefabRequest.TryGetPrefab(out prefab)) // I realize its more code but Sorry couldnt stand all the warnings
-            {
-                prefab = CraftData.GetPrefabForTechType(techType, false);
-                if (prefab == null)
-                {
-                    return Optional.Of(Utils.CreateGenericLoot(techType));
-                }
-            }
 
-            GameObject gameObject = Utils.SpawnFromPrefab(prefab, null);
+            GameObject gameObject = CreateGameObject(techType, entity.ClassId);
             gameObject.transform.position = entity.Transform.Position;
             gameObject.transform.rotation = entity.Transform.Rotation;
             gameObject.transform.localScale = entity.Transform.LocalScale;
@@ -54,6 +44,23 @@ namespace NitroxClient.GameLogic.Spawning
             }
 
             return Optional.Of(gameObject);
+        }
+
+        private GameObject CreateGameObject(TechType techType, string classId)
+        {
+            GameObject prefab;
+            IPrefabRequest prefabRequest = PrefabDatabase.GetPrefabAsync(classId);
+            if (!prefabRequest.TryGetPrefab(out prefab))
+            {
+                prefab = CraftData.GetPrefabForTechType(techType, false);
+
+                if (prefab == null)
+                {
+                    return Utils.CreateGenericLoot(techType);
+                }
+            }
+
+            return Utils.SpawnFromPrefab(prefab, null);
         }
 
         public bool SpawnsOwnChildren()
