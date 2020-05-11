4ec63cdaeb83c164bcc0b69acfae9b709ee94988
_HeN_ <drhen123@googlemail.com>
2020-05-11 23:00:19 +0100

    Fixes reloading of keypad doors

---
+++ NitroxPatcher/Patches/Dynamic/KeypadDoorConsole_Start_Patch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+
+namespace NitroxPatcher.Patches.Dynamic
+{
+    public class KeypadDoorConsole_Start_Patch : NitroxPatch, IDynamicPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(KeypadDoorConsole);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Postfix(KeypadDoorConsole __instance)
+        {
+            if (!__instance.unlocked)
+            {
+                if (__instance.root)
+
+                {
+                    __instance.root.BroadcastMessage("UnlockDoor");
+                }
+                else
+
+                {
+                    __instance.BroadcastMessage("UnlockDoor");
+                }
+                __instance.UnlockDoor();
+            }
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPostfix(harmony, TARGET_METHOD);
+        }
+    }
+}
