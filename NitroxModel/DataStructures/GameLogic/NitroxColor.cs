1ee36bf19c4aa0f1937c1a8321584f698b1ff915
Measurity <measuring.infinity@gmail.com>
2020-05-14 16:27:18 +0200

    Added protomember attributes to NitroxColor members

--- NitroxModel/DataStructures/GameLogic/NitroxColor.cs
+++ NitroxModel/DataStructures/GameLogic/NitroxColor.cs
@@ -7,9 +7,16 @@ namespace NitroxModel.DataStructures.GameLogic
     [Serializable]
     public struct NitroxColor
     {
+        [ProtoMember(1)]
         public float R { get; private set; }
+
+        [ProtoMember(2)]
         public float G { get; private set; }
+
+        [ProtoMember(3)]
         public float B { get; private set; }
+
+        [ProtoMember(4)]
         public float A { get; private set; }
 
         public NitroxColor(float r, float g, float b) : this(r, g, b, 1)
