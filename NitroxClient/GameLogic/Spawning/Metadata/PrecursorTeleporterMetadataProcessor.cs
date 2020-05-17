d16a88dbea91839ab75bd0c326199759264f215a
_HeN_ <drhen123@googlemail.com>
2020-05-17 14:49:42 +0100

    Added precursor teleporer sync

---
+++ NitroxClient/GameLogic/Spawning/Metadata/PrecursorTeleporterMetadataProcessor.cs
@@ -0,0 +1,19 @@
+﻿using NitroxModel.DataStructures.GameLogic.Entities.Metadata;
+using NitroxModel.Logger;
+using UnityEngine;
+
+namespace NitroxClient.GameLogic.Spawning.Metadata
+{
+    public class PrecursorTeleporterMetadataProcessor : GenericEntityMetadataProcessor<PrecursorTeleporterMetadata>
+    {
+        public override void ProcessMetadata(GameObject gameObject, PrecursorTeleporterMetadata metadata)
+        {
+            Log.Info($"Received precursor teleporter metadata change for {gameObject.name} with data of {metadata}");
+
+            PrecursorTeleporter precursorTeleporter = gameObject.GetComponent<PrecursorTeleporter>();
+            precursorTeleporter.isOpen = metadata.IsOpen;
+
+            precursorTeleporter.ToggleDoor(metadata.IsOpen);
+        }
+    }
+}
--- NitroxModel/DataStructures/GameLogic/Entities/Metadata/EntityMetadata.cs
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/EntityMetadata.cs
@@ -4,7 +4,7 @@ using ProtoBufNet;
 namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
 {
     [Serializable]
-    [ProtoContract, ProtoInclude(50, typeof(KeypadMetadata)), ProtoInclude(60, typeof(SealedDoorMetadata)), ProtoInclude(70, typeof(PrecursorDoorwayMetadata))]    
+    [ProtoContract, ProtoInclude(50, typeof(KeypadMetadata)), ProtoInclude(60, typeof(SealedDoorMetadata)), ProtoInclude(70, typeof(PrecursorDoorwayMetadata)), ProtoInclude(80, typeof(PrecursorTeleporterMetadata))]    
     public abstract class EntityMetadata
     {
     }
