6d5d8c65c5c35ae6763e09647ec024d145d25f3e
dartasen <clement.vara@etu.uca.fr>
2020-05-17 16:23:12 +0200

    Fix multiple rocket

--- NitroxPatcher/Patches/Dynamic/Rocket_Start_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/Rocket_Start_Patch.cs
@@ -6,7 +6,6 @@ using NitroxClient.Unity.Helper;
 using NitroxModel.Core;
 using NitroxModel.DataStructures;
 using NitroxModel.DataStructures.Util;
-using NitroxModel.Helper;
 using NitroxModel.Logger;
 using NitroxModel_Subnautica.DataStructures.GameLogic;
 using UnityEngine;
@@ -15,9 +14,9 @@ namespace NitroxPatcher.Patches.Dynamic
 {
     public class Rocket_Start_Patch : NitroxPatch, IDynamicPatch
     {
-		public static readonly MethodInfo TARGET_METHOD = typeof(Rocket).GetMethod("SubConstructionComplete", BindingFlags.Public | BindingFlags.Instance);
+        public static readonly MethodInfo TARGET_METHOD = typeof(Rocket).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        public static void Prefix(Rocket __instance)
+        public static bool Prefix(Rocket __instance)
         {
             GameObject gameObject = __instance.gameObject;
             NitroxId id = NitroxEntity.GetId(gameObject);
@@ -26,9 +25,24 @@ namespace NitroxPatcher.Patches.Dynamic
             if (!model.HasValue)
             {
                 Log.Error($"{nameof(Rocket_Start_Patch)}: Could not find {nameof(NeptuneRocketModel)} by Nitrox id {id}.\nGO containing wrong id: {__instance.GetHierarchyPath()}");
+                return false;
             }
 
             __instance.currentRocketStage = model.Value.CurrentRocketStage;
+            __instance.elevatorState = model.Value.ElevatorUp ? Rocket.RocketElevatorStates.AtTop : Rocket.RocketElevatorStates.AtBottom;
+
+            Optional<ElevatorCallControl> opelevator = Optional.OfNullable(gameObject.GetComponentInChildren<ElevatorCallControl>());
+
+            if (opelevator.HasValue)
+            {
+                opelevator.Value.elevatorUp = model.Value.ElevatorUp;
+            }
+            else
+            {
+                Log.Error($"{nameof(Rocket_Start_Patch)}: Unable to find the ElevatorCallControl inside the RocketBase");
+            }
+
+            return true;
         }
 
         public override void Patch(HarmonyInstance harmony)
