5ffc5d0a83f74eebf75c1b2edd3a3a2ab918d4b9
Coding-Hen <drhen123@googlemail.com>
2020-05-28 08:01:21 +0100

    Added precursor teleporer sync (#1105)
    * Added precursor teleporer sync
    
    * Made changes as suggested

---
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/PrecursorTeleporterMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ProtoBufNet;
+
+namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
+{
+    [Serializable]
+    [ProtoContract]
+    public class PrecursorTeleporterMetadata : EntityMetadata
+    {
+        [ProtoMember(1)]
+        public bool IsOpen { get; }
+
+        public PrecursorTeleporterMetadata()
+        {
+            // Constructor for serialization
+        }
+
+        public PrecursorTeleporterMetadata(bool isOpen)
+        {
+            IsOpen = isOpen;
+        }
+
+        public override string ToString()
+        {
+            return "[PrecursorTeleporterMetadata isOpen: " + IsOpen + "]";
+        }
+    }
+}
