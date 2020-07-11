12b5ba110c621d78a6eb335c32fe50e7091d0835
_HeN_ <drhen123@googlemail.com>
2020-07-11 11:11:06 +0100

    Added sync for the precursor key terminal

---
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/PrecursorKeyTerminalMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ProtoBufNet;
+
+namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
+{
+    [Serializable]
+    [ProtoContract]
+    public class PrecursorKeyTerminalMetadata : EntityMetadata
+    {
+        [ProtoMember(1)]
+        public bool Slotted { get; }
+
+        public PrecursorKeyTerminalMetadata()
+        {
+            // Constructor for serialization
+        }
+
+        public PrecursorKeyTerminalMetadata(bool slotted)
+        {
+            Slotted = slotted;
+        }
+
+        public override string ToString()
+        {
+            return "[PrecursorKeyTerminalMetadata Slotted: " + Slotted + "]";
+        }
+    }
+}
