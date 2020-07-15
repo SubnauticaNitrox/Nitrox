7cd74e84e4af467d84735acb81a6440b149aad35
killzoms Death <brandonpar14@gmail.com>
2020-07-14 21:55:43 -0400

    patch uGUI.OnApplicationQuit

---
+++ NitroxPatcher/Patches/Dynamic/uGUI_OnApplicationQuit_Patch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using NitroxClient.Communication.Abstract;
+using NitroxModel.Core;
+
+namespace NitroxPatcher.Patches.Dynamic
+{
+    internal class uGUI_OnApplicationQuit_Patch : NitroxPatch, IDynamicPatch
+    {
+        private readonly Type TYPE = typeof(uGUI);
+        private const string TARGET_METHOD = "OnApplicationQuit";
+
+        public static void Prefix()
+        {
+            IMultiplayerSession multiplayerSession = NitroxServiceLocator.LocateService<IMultiplayerSession>();
+            multiplayerSession.Disconnect();
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPrefix(harmony, TYPE.GetMethod(TARGET_METHOD, BindingFlags.NonPublic | BindingFlags.Instance));
+        }
+    }
+}
