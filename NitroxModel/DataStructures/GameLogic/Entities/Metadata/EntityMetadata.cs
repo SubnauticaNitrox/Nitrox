8955006ecf23d02eb0c9dac258f33ca73a7cbb67
Coding-Hen <drhen123@googlemail.com>
2020-07-24 06:09:20 +0100

    Added sync for the precursor key terminal (#1165)
    * Added sync for the precursor key terminal
    
    * Removed audio sync to prevent false sound on connection
    
    * Changed accessor to protected for json serialisation

--- NitroxModel/DataStructures/GameLogic/Entities/Metadata/EntityMetadata.cs
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/EntityMetadata.cs
@@ -4,7 +4,7 @@ using ProtoBufNet;
 namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
 {
     [Serializable]
-    [ProtoContract, ProtoInclude(50, typeof(KeypadMetadata)), ProtoInclude(60, typeof(SealedDoorMetadata)), ProtoInclude(70, typeof(PrecursorDoorwayMetadata)), ProtoInclude(80, typeof(PrecursorTeleporterMetadata))]    
+    [ProtoContract, ProtoInclude(50, typeof(KeypadMetadata)), ProtoInclude(60, typeof(SealedDoorMetadata)), ProtoInclude(70, typeof(PrecursorDoorwayMetadata)), ProtoInclude(80, typeof(PrecursorTeleporterMetadata)), ProtoInclude(90, typeof(PrecursorKeyTerminalMetadata))]
     public abstract class EntityMetadata
     {
     }
---
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/PrecursorKeyTerminalMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ProtoBufNet;
+
+namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
+{
+    [Serializable]
+    [ProtoContract]
+    public class PrecursorKeyTerminalMetadata : EntityMetadata
+    {
+        [ProtoMember(1)]
+        public bool Slotted { get; }
+
+        protected PrecursorKeyTerminalMetadata()
+        {
+            //Constructor for serialization. Has to be "protected" for json serialization.
+        }
+
+        public PrecursorKeyTerminalMetadata(bool slotted)
+        {
+            Slotted = slotted;
+        }
+
+        public override string ToString()
+        {
+            return "[PrecursorKeyTerminalMetadata Slotted: " + Slotted + "]";
+        }
+    }
+}
