e0b2ec56153c8bac3b340fd446531612e94e41e2
Coding-Hen <drhen123@googlemail.com>
2020-07-26 06:09:45 +0100

    Fix teleporter sync (#1162)
    * Fix teleporters enabling on reload
    
    * Added activation terminal sync
    
    * Changed file name to match patch objective
    
    * Fixed issue with cinimatic jump of player to terminal
    
    * Removed audio sync to prevent false sound on connection
    
    * Changed accessors to protected for json serialisation

---
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/PrecursorTeleporterActivationTerminalMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ProtoBufNet;
+
+namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
+{
+    [Serializable]
+    [ProtoContract]
+    public class PrecursorTeleporterActivationTerminalMetadata : EntityMetadata
+    {
+        [ProtoMember(1)]
+        public bool Unlocked { get; }
+
+        protected PrecursorTeleporterActivationTerminalMetadata()
+        {
+            // Constructor for serialization. Has to be "protected" for json serialization.
+        }
+
+        public PrecursorTeleporterActivationTerminalMetadata(bool unlocked)
+        {
+            Unlocked = unlocked;
+        }
+
+        public override string ToString()
+        {
+            return "[PrecursorTeleporterActivationTerminalMetadata Unlocked: " + Unlocked + "]";
+        }
+    }
+}
