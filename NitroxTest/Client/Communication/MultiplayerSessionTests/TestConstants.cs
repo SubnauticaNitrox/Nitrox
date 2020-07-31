3d6a674fd45be92ae1f5705041b2cbd80246e279
_HeN_ <drhen123@googlemail.com>
2020-07-31 18:20:20 +0100

    Updated tests for new password parameter

--- NitroxTest/Client/Communication/MultiplayerSessionTests/TestConstants.cs
+++ NitroxTest/Client/Communication/MultiplayerSessionTests/TestConstants.cs
@@ -16,7 +16,7 @@ namespace NitroxTest.Client.Communication.MultiplayerSessionTests
         public const int TEST_MAX_PLAYER_CONNECTIONS = 100;
         public const MultiplayerSessionReservationState TEST_REJECTION_STATE = MultiplayerSessionReservationState.REJECTED | MultiplayerSessionReservationState.UNIQUE_PLAYER_NAME_CONSTRAINT_VIOLATED;
         public static readonly AuthenticationContext TEST_AUTHENTICATION_CONTEXT = new AuthenticationContext(TEST_PLAYER_NAME);
-        public static readonly MultiplayerSessionPolicy TEST_SESSION_POLICY = new MultiplayerSessionPolicy(TEST_CORRELATION_ID, false, TEST_MAX_PLAYER_CONNECTIONS);
+        public static readonly MultiplayerSessionPolicy TEST_SESSION_POLICY = new MultiplayerSessionPolicy(TEST_CORRELATION_ID, false, TEST_MAX_PLAYER_CONNECTIONS, false);
         public static readonly PlayerSettings TEST_PLAYER_SETTINGS = new PlayerSettings(RandomColorGenerator.GenerateColor());
         public static readonly IMultiplayerSessionConnectionState TEST_CONNECTION_STATE = Substitute.For<IMultiplayerSessionConnectionState>();
     }
