17e147d800bfdf95860286216beaf9f04bcf66c7
_HeN_ <drhen123@googlemail.com>
2020-08-07 18:05:38 +0100

    Added starship door patches and syncing

---
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/StarshipDoorMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ProtoBufNet;
+
+namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata
+{
+    [Serializable]
+    [ProtoContract]
+    public class StarshipDoorMetadata : EntityMetadata
+    {
+        [ProtoMember(1)]
+        public bool DoorLocked { get; }
+
+        protected StarshipDoorMetadata()
+        {
+            //Constructor for serialization. Has to be "protected" for json serialization.
+        }
+
+        public StarshipDoorMetadata(bool doorLocked)
+        {
+            DoorLocked = doorLocked;
+        }
+    }
+}
---
+++ NitroxPatcher/Patches/Dynamic/StarshipDoor_UnlockDoor_Patch.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Harmony;
+using NitroxClient.GameLogic;
+using NitroxClient.MonoBehaviours;
+using NitroxModel.Core;
+using NitroxModel.DataStructures;
+using NitroxModel.DataStructures.GameLogic.Entities.Metadata;
+
+namespace NitroxPatcher.Patches.Dynamic
+{
+    class StarshipDoor_UnlockDoor_Patch : NitroxPatch, IDynamicPatch
+    {
+        public static readonly MethodInfo TARGET_METHOD = typeof(StarshipDoor).GetMethod(nameof(StarshipDoor.UnlockDoor), BindingFlags.Instance | BindingFlags.Public);
+
+        public static void Prefix(StarshipDoor __instance)
+        {
+            if (__instance.doorLocked)
+            {
+                NitroxId id = NitroxEntity.GetId(__instance.gameObject);
+                StarshipDoorMetadata starshipDoorMetadata = new StarshipDoorMetadata(!__instance.doorLocked);
+                Entities entities = NitroxServiceLocator.LocateService<Entities>();
+
+                entities.BroadcastMetadataUpdate(id, starshipDoorMetadata);
+            }
+        }
+
+        public override void Patch(HarmonyInstance harmony)
+        {
+            PatchPrefix(harmony, TARGET_METHOD);
+        }
+    }
+}
