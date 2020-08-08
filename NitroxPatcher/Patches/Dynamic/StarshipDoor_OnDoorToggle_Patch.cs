3b0492fdb343bfcbab755a0e10d53735a747714b
_HeN_ <drhen123@googlemail.com>
2020-08-08 10:03:29 +0100

    Combinded target method lines

--- NitroxPatcher/Patches/Dynamic/StarshipDoor_OnDoorToggle_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/StarshipDoor_OnDoorToggle_Patch.cs
@@ -11,8 +11,7 @@ namespace NitroxPatcher.Patches.Dynamic
 {
     class StarshipDoor_OnDoorToggle_Patch : NitroxPatch, IDynamicPatch
     {
-        public static readonly Type TARGET_CLASS = typeof(StarshipDoor);
-        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("OnDoorToggle", BindingFlags.Instance | BindingFlags.NonPublic);
+        public static readonly MethodInfo TARGET_METHOD = typeof(StarshipDoor).GetMethod("OnDoorToggle", BindingFlags.Instance | BindingFlags.NonPublic);
 
         public static void Postfix(StarshipDoor __instance)
         {
