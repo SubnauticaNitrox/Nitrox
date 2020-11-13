535d7d73d179c9f9100ae5366f829ddc9a6de2b9
dartasen <clement.vara@etu.uca.fr>
2020-11-13 13:07:07 +0100

    Fix for rocket's stage persistance

--- NitroxClient/GameLogic/Rockets.cs
+++ NitroxClient/GameLogic/Rockets.cs
@@ -18,6 +18,14 @@ namespace NitroxClient.GameLogic
             this.vehicles = vehicles;
         }
 
+        /** Rocket states :
+         * 0 : Launch Platform
+         * 1 : Gantry
+         * 2 : Boosters
+         * 3 : Fuel Reserve
+         * 4 : Cockpit
+         * 5 : Final rocket
+         **/
         public void BroadcastRocketStateUpdate(NitroxId id, TechType currentStageTech)
         {
             Optional<NeptuneRocketModel> model = vehicles.TryGetVehicle<NeptuneRocketModel>(id);
@@ -25,6 +33,13 @@ namespace NitroxClient.GameLogic
             if (model.HasValue)
             {
                 model.Value.CurrentStage += 1;
+
+                //State 5 cannot be reached for the server based on players events, so we do it by hand
+                if (model.Value.CurrentStage > 3)
+                {
+                    model.Value.CurrentStage = 5;
+                }
+
                 packetSender.Send(new RocketStageUpdate(id, model.Value.CurrentStage, currentStageTech));
             }
             else
