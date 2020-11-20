ba2e14e61aea5698c72f9b50982919727395d7fb
dartasen <clement.vara@etu.uca.fr>
2020-11-20 23:12:16 +0100

    Added username validation upon login

--- NitroxServer/Communication/Packets/Processors/MultiplayerSessionReservationRequestProcessor.cs
+++ NitroxServer/Communication/Packets/Processors/MultiplayerSessionReservationRequestProcessor.cs
@@ -18,7 +18,7 @@ namespace NitroxServer.Communication.Packets.Processors
 
         public override void Process(MultiplayerSessionReservationRequest packet, NitroxConnection connection)
         {
-            Log.Info("Processing reservation request...");
+            Log.Info($"Processing reservation request from {packet.AuthenticationContext.Username}");
 
             string correlationId = packet.CorrelationId;
             PlayerSettings playerSettings = packet.PlayerSettings;
@@ -29,7 +29,7 @@ namespace NitroxServer.Communication.Packets.Processors
                 authenticationContext,
                 correlationId);
 
-            Log.Info($"Reservation processed successfully: {reservation}...");
+            Log.Info($"Reservation processed successfully: Username: {packet.AuthenticationContext.Username} - {reservation}");
 
             connection.SendPacket(reservation);
         }
