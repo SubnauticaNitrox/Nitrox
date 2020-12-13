1fb81c3e2ccccb8af95405b023b695c73d1b15fb
Measurity <measuring.infinity@gmail.com>
2020-12-13 21:02:31 +0100

    Moved from NLog to Serilog (#1315)
    NLog rules and filters were too confusing to use practically for sensitive information logging.

---
+++ NitroxModel/Logger/MessageSink.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+
+namespace NitroxModel.Logger
+{
+    public class MessageSink : ILogEventSink
+    {
+        private readonly ITextFormatter formatter;
+        private readonly Action<string> writer;
+
+        public MessageSink(ITextFormatter formatter, Action<string> writer)
+        {
+            this.formatter = formatter;
+            this.writer = writer;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            using StringWriter stringWriter = new StringWriter();
+            formatter.Format(logEvent, stringWriter);
+            stringWriter.Flush();
+            writer(stringWriter.GetStringBuilder().ToString());
+        }
+    }
+
+    public static class MessageSinkExtensions
+    {
+        public static LoggerConfiguration Message(
+            this LoggerSinkConfiguration loggerConfiguration,
+            Action<string> writer,
+            LogEventLevel minimumLevel = LogEventLevel.Verbose,
+            string outputTemplate = "{Message}",
+            IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new MessageSink(new MessageTemplateTextFormatter(outputTemplate, formatProvider), writer), minimumLevel);
+        }
+    }
+}
