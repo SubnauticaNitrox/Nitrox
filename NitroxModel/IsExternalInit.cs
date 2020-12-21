7c3e1cc71227924ad9689452661979a54f561c0e
Measurity <measuring.infinity@gmail.com>
2020-12-21 06:18:48 +0100

    Upgraded projects to C# 9 (#1325)
    * Updated projects to use C# 9
    
    * Added IsExternalInit.cs so that C# 9 record types can be used
    
    * Use .netstandard2.0 automatically for libraries that don't have it defined
    
    * Build fix attempt
    
    * Fixed Newtonsoft.Json dependency being misconfigured
    
    * Added serilog settings to packages again
    
    * Updated DotNetZip package

---
+++ NitroxModel/IsExternalInit.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    ///     Having this class allows us to use C# 9 record types.
+    /// </summary>
+    public class IsExternalInit
+    {
+    }
+}
