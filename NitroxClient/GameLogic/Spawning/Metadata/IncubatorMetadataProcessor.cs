7a949b7214d6df20838e8e28a60a373b36450a30
Sunrunner37 <sunrunner@nitroxmod.com>
2020-12-30 01:54:20 -0800

    Adding sync for incubator enzyme terminal and power pillar.

---
+++ NitroxClient/GameLogic/Spawning/Metadata/IncubatorMetadataProcessor.cs
@@ -0,0 +1,26 @@
+﻿using NitroxModel.DataStructures.GameLogic.Entities.Metadata;
+using UnityEngine;
+
+namespace NitroxClient.GameLogic.Spawning.Metadata
+{
+    class IncubatorMetadataProcessor : GenericEntityMetadataProcessor<IncubatorMetadata>
+    {
+        public override void ProcessMetadata(GameObject gameObject, IncubatorMetadata metadata)
+        {
+            if (metadata.Powered)
+            {
+                IncubatorActivationTerminal terminal = gameObject.GetComponentInChildren<IncubatorActivationTerminal>();
+                terminal.incubator.SetPowered(true);
+                terminal.onUseGoal?.Trigger();
+                terminal.CloseDeck();
+            }
+
+            if (metadata.Hatched)
+            {
+                Incubator incubator = gameObject.GetComponentInChildren<Incubator>();
+                incubator.hatched = true;
+                incubator.OnHatched();
+            }
+        }
+    }
+}
