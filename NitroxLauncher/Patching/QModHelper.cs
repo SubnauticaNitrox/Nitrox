c578565d7183cbe3b40756355f0a76803d059ffe
Coding-Hen <8798074+Coding-Hen@users.noreply.github.com>
2021-03-15 17:41:06 +0000

    Stop qmod initialisation on load and restore on exit

---
+++ NitroxLauncher/Patching/QModHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace NitroxLauncher.Patching
+{
+    public static class QModHelper
+    {
+        private static readonly string originalFileName = "winhttp.dll";
+        private static readonly string renamedFileName = "winhttp_nitrox_stopped.dll";
+
+        public static void RemoveQModEntryPoint(string subnauticaBasePath)
+        {
+            RenameFile(subnauticaBasePath, originalFileName, renamedFileName);
+        }
+
+        public static void RestoreQModEntryPoint(string subnauticaBasePath)
+        {
+            RenameFile(subnauticaBasePath, renamedFileName, originalFileName);
+        }
+
+        private static void RenameFile(string subnauticaBasePath, string fileToRename, string newFileName)
+        {
+            string fileToRenamePath = Path.Combine(subnauticaBasePath, fileToRename);
+            if (!File.Exists(fileToRenamePath))
+            {
+                return;
+            }
+
+            string newFilePath = Path.Combine(subnauticaBasePath, newFileName);
+            File.Move(fileToRenamePath, newFilePath);
+        }
+    }
+}
