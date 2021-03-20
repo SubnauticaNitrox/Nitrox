a2c82a0704e51c9883889d010a7e82e4aed97b9f
Coding-Hen <8798074+Coding-Hen@users.noreply.github.com>
2021-03-20 15:03:11 +0000

    Added logging and fixed cold initialisation of singleplayer

--- NitroxLauncher/Patching/QModHelper.cs
+++ NitroxLauncher/Patching/QModHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using NitroxModel.Logger;
 
 namespace NitroxLauncher.Patching
 {
@@ -6,14 +7,24 @@ namespace NitroxLauncher.Patching
     {
         private static readonly string originalFileName = "winhttp.dll";
         private static readonly string renamedFileName = "winhttp_nitrox_stopped.dll";
-
+        private static bool QModPatched;
         public static void RemoveQModEntryPoint(string subnauticaBasePath)
         {
+            if (QModPatched || !IsQModInstalled(subnauticaBasePath))
+            {
+                return;
+            }
+            Log.Info("Attempting to remove QMod initialisation");
             RenameFile(subnauticaBasePath, originalFileName, renamedFileName);
         }
 
         public static void RestoreQModEntryPoint(string subnauticaBasePath)
         {
+            if (!QModPatched || !IsQModInstalled(subnauticaBasePath))
+            {
+                return;
+            }
+            Log.Info("Attempting to restore QMod initialisation");
             RenameFile(subnauticaBasePath, renamedFileName, originalFileName);
         }
 
@@ -22,11 +33,20 @@ namespace NitroxLauncher.Patching
             string fileToRenamePath = Path.Combine(subnauticaBasePath, fileToRename);
             if (!File.Exists(fileToRenamePath))
             {
+                Log.Error($"QMod entry cannot be found, please uninstall QMod");
                 return;
             }
 
+            QModPatched = !QModPatched;
             string newFilePath = Path.Combine(subnauticaBasePath, newFileName);
             File.Move(fileToRenamePath, newFilePath);
+            Log.Info("Removing/Restoring QMod initialisation has been successful");
+        }
+
+        private static bool IsQModInstalled(string subnauticaBasePath)
+        {
+            string subnauticaQModsPath = Path.Combine(subnauticaBasePath, "QMods");
+            return Directory.Exists(subnauticaQModsPath);
         }
     }
 }
