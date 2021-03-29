ab5ed9853803e3063f31ab8cf8995483882b0161
Coding-Hen <8798074+Coding-Hen@users.noreply.github.com>
2021-03-29 20:42:06 +0100

    Fixed more than one player using a Bench/Chair

---
+++ NitroxPatcher/Patches/Dynamic/Bench_OnHandClick_Patch.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using NitroxClient.GameLogic;
+using NitroxClient.GameLogic.HUD;
+using NitroxClient.GameLogic.Simulation;
+using NitroxClient.MonoBehaviours;
+using NitroxModel.Core;
+using NitroxModel.DataStructures;
+using NitroxModel.Logger;
+
+namespace NitroxPatcher.Patches.Dynamic
+{
+    class Bench_OnHandClick_Patch : NitroxPatch, IDynamicPatch
+    {
+        public static readonly MethodInfo TARGET_METHOD = typeof(Bench).GetMethod(nameof(Bench.OnHandClick), BindingFlags.Public | BindingFlags.Instance);
+
+        private static bool skipPrefix = false;
+
+        public static bool Prefix(Bench __instance, GUIHand hand)
+        {
+            if (skipPrefix)
+            {
+                return true;
+            }
+
+            SimulationOwnership simulationOwnership = NitroxServiceLocator.LocateService<SimulationOwnership>();
+            
+            NitroxId id = NitroxEntity.GetId(__instance.gameObject);
+
+            if (simulationOwnership.HasExclusiveLock(id))
+            {
+                Log.Debug($"Already have an exclusive lock on the bench/chair: {id}");
+                return true;
+            }
+
+            HandInteraction<Bench> context = new HandInteraction<Bench>(__instance, hand);
+            LockRequest<HandInteraction<Bench>> lockRequest = new LockRequest<HandInteraction<Bench>>(id, SimulationLockType.EXCLUSIVE, ReceivedSimulationLockResponse, context);
+
+            simulationOwnership.RequestSimulationLock(lockRequest);
+
+            return false;
+        }
+
+        private static void ReceivedSimulationLockResponse(NitroxId id, bool lockAquired, HandInteraction<Bench> context)
+        {
+            Bench bench = context.Target;
+
+            if (lockAquired)
+            {
+                skipPrefix = true;
+                TARGET_METHOD.Invoke(bench, new[] { context.GuiHand });
+                skipPrefix = false;
+            }
+            else
+            {
+                bench.gameObject.AddComponent<DenyOwnershipHand>();
+                bench.isValidHandTarget = false;
+            }
+        }
+
+        public override void Patch(Harmony harmony)
+        {
+            PatchPrefix(harmony, TARGET_METHOD);
+        }
+    }
+}
---
+++ NitroxPatcher/Patches/Dynamic/Bench_OnPlayerDeath_Patch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using NitroxClient.GameLogic;
+using NitroxClient.MonoBehaviours;
+using NitroxModel.Core;
+using NitroxModel.DataStructures;
+
+namespace NitroxPatcher.Patches.Dynamic
+{
+    public class Bench_OnPlayerDeath_Patch : NitroxPatch, IDynamicPatch
+    {
+        public static readonly Type TARGET_CLASS = typeof(Bench);
+        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod("OnPlayerDeath", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Postfix(Bench __instance)
+        {
+            NitroxId id = NitroxEntity.GetId(__instance.gameObject);
+
+            SimulationOwnership simulationOwnership = NitroxServiceLocator.LocateService<SimulationOwnership>();
+
+            // Request to be downgraded to a transient lock so we can still simulate the positioning.
+            simulationOwnership.RequestSimulationLock(id, SimulationLockType.TRANSIENT);
+        }
+
+        public override void Patch(Harmony harmony)
+        {
+            PatchPostfix(harmony, TARGET_METHOD);
+        }
+    }
+}
