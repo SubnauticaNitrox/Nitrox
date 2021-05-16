0fa3a89269bf968e39249e2c9b7a92d15761aabc
Coding-Hen <8798074+Coding-Hen@users.noreply.github.com>
2021-05-16 12:37:15 +0100

    Fixed spelling and changed to new logging format

--- NitroxClient/Communication/Packets/Processors/PDAScannerEntryProgressProcessor.cs
+++ NitroxClient/Communication/Packets/Processors/PDAScannerEntryProgressProcessor.cs
@@ -31,13 +31,13 @@ namespace NitroxClient.Communication.Packets.Processors
                 {
                     if (packet.Unlocked > entry.unlocked)
                     {
-                        Log.Info("PDAEntryProgress Upldate Old:" + entry.unlocked + " New" + packet.Unlocked);
+                        Log.Info($"PDAEntryProgress Update For {techType} Old:{entry.unlocked} New:{packet.Unlocked}");
                         entry.unlocked = packet.Unlocked;
                     }
                 }
                 else
                 {
-                    Log.Info("PDAEntryProgress New TechType:" + techType + " Unlocked:" + packet.Unlocked);
+                    Log.Info($"PDAEntryProgress New TechType:{techType} Unlocked:{packet.Unlocked}");
                     MethodInfo methodAdd = typeof(PDAScanner).GetMethod("Add", BindingFlags.NonPublic | BindingFlags.Static, null, new Type[] { typeof(TechType), typeof(int) }, null);
                     entry = (PDAScanner.Entry)methodAdd.Invoke(null, new object[] { techType, packet.Unlocked });
                 }
