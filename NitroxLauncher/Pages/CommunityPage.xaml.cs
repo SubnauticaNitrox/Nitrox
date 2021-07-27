a005a701f728016b57234273322472d8900d5282
keyboardfox <hi@rux.gg>
2021-07-27 23:23:08 +0200

    Implement new launcher design UI

---
+++ NitroxLauncher/Pages/CommunityPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NitroxLauncher.Pages
+{
+    /// <summary>
+    /// Interaction logic for CommunityPage.xaml
+    /// </summary>
+    public partial class CommunityPage : Page
+    {
+        public CommunityPage()
+        {
+            InitializeComponent();
+        }
+        private void OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
+        }
+    }
+}
