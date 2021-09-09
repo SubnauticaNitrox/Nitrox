6cbaaa848cf76a2b791c051799d08322b9921191
MrPurple6411 <mrpurple6411@gmail.com>
2021-09-08 19:56:31 -0600

    Add null check to avoid Null ref exception when no vehicle docked. (#1586)
    * Add null check to avoid Null ref exception when no vehicle docked.
    
    * Update VehicleDockingBay_OnTriggerEnter.cs

--- NitroxPatcher/Patches/Dynamic/VehicleDockingBay_OnTriggerEnter.cs
+++ NitroxPatcher/Patches/Dynamic/VehicleDockingBay_OnTriggerEnter.cs
@@ -21,7 +21,7 @@ namespace NitroxPatcher.Patches.Dynamic
         {
             Vehicle vehicle = other.GetComponentInParent<Vehicle>();
             prevInterpolatingVehicle = (Vehicle)__instance.ReflectionGet("interpolatingVehicle");
-            return NitroxServiceLocator.LocateService<SimulationOwnership>().HasAnyLockType(NitroxEntity.GetId(vehicle.gameObject));
+            return vehicle == null || NitroxServiceLocator.LocateService<SimulationOwnership>().HasAnyLockType(NitroxEntity.GetId(vehicle.gameObject));
         }
 
         public static void Postfix(VehicleDockingBay __instance)
