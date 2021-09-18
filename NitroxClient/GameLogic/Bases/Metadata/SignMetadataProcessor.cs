828c070dd15546f87944274a59bf36bd0c70df0f
MrPurple6411 <mrpurple6411@gmail.com>
2021-09-18 08:13:33 -0600

    Sign metadata processor null ref fix (#1598)
    * Check Inactive Children
    
    * Log Error on Null instead of crashing

--- NitroxClient/GameLogic/Bases/Metadata/SignMetadataProcessor.cs
+++ NitroxClient/GameLogic/Bases/Metadata/SignMetadataProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using NitroxClient.MonoBehaviours;
+using NitroxClient.Unity.Helper;
 using NitroxModel.DataStructures;
 using NitroxModel.DataStructures.GameLogic.Buildings.Metadata;
+using NitroxModel.Logger;
 using UnityEngine;
 
 namespace NitroxClient.GameLogic.Bases.Metadata
@@ -10,13 +12,19 @@ namespace NitroxClient.GameLogic.Bases.Metadata
         public override void UpdateMetadata(NitroxId id, SignMetadata metadata)
         {
             GameObject gameObject = NitroxEntity.RequireObjectFrom(id);
-            uGUI_SignInput sign = gameObject.GetComponentInChildren<uGUI_SignInput>();
-
-            sign.text = metadata.Text;
-            sign.colorIndex = metadata.ColorIndex;
-            sign.elementsState = metadata.Elements;
-            sign.scaleIndex = metadata.ScaleIndex;
-            sign.SetBackground(metadata.Background);
+            uGUI_SignInput sign = gameObject.GetComponentInChildren<uGUI_SignInput>(true);
+            if (sign.AliveOrNull() != null)
+            {
+                sign.text = metadata.Text;
+                sign.colorIndex = metadata.ColorIndex;
+                sign.elementsState = metadata.Elements;
+                sign.scaleIndex = metadata.ScaleIndex;
+                sign.SetBackground(metadata.Background);
+            }
+            else
+            {
+                Log.Error($"SignMetaData Processing failed for {gameObject.name}({gameObject.transform.position}). No sign component found on object.");
+            }
         }
     }
 }
