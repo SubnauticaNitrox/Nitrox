f2c22cf5c72aa420e34ad76651ea9deeedbb84cf
Measurity <measuring.infinity@gmail.com>
2021-09-18 17:43:38 +0200

    Added gameinfo class with info about SN1 and BZ

---
+++ NitroxModel/GameInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace NitroxModel
+{
+    public sealed class GameInfo
+    {
+        public static readonly GameInfo Subnautica = new()
+        {
+            Name = "Subnautica",
+            FullName = "Subnautica",
+            ExeName = "Subnautica.exe",
+            SteamAppId = 264710
+        };
+
+        public static readonly GameInfo SubnauticaBelowZero = new()
+        {
+            Name = "SubnauticaZero",
+            FullName = "Subnautica: Below Zero",
+            ExeName = "SubnauticaZero.exe",
+            SteamAppId = 848450
+        };
+
+        public string Name { get; private set; }
+        public string FullName { get; private set; }
+        public string ExeName { get; private set; }
+        public int SteamAppId { get; private set; }
+
+        private GameInfo()
+        {
+        }
+    }
+}
