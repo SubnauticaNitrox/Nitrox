36d321d3f3b44937a81d6cdf8846b1ea4f6e298a
Measurity <measuring.infinity@gmail.com>
2021-10-05 16:06:59 +0200

    Added comment on starting below zero through ms store

--- NitroxModel/Platforms/Store/MSStore.cs
+++ NitroxModel/Platforms/Store/MSStore.cs
@@ -11,13 +11,13 @@ namespace NitroxModel.Platforms.Store
         private static MSStore instance;
         public static MSStore Instance => instance ??= new MSStore();
 
+        public string Name => "Microsoft Store";
+
         public bool OwnsGame(string gameDirectory)
         {
             bool isLocalAppData = Path.GetFullPath(gameDirectory).StartsWith(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Packages"), StringComparison.InvariantCultureIgnoreCase);
             return isLocalAppData;
         }
-
-        public string Name => "Microsoft Store";
 
         public async Task<ProcessEx> StartPlatformAsync()
         {
@@ -35,7 +35,7 @@ namespace NitroxModel.Platforms.Store
             // TODO: Support MS Store again and run command (example, should use pathToGameExe argument):
             // return await Task.FromResult(ProcessEx.Start(null,
             //                                        new[] { ("NITROX_LAUNCHER_PATH", "") },
-            //                                        commandLine: "ms-xbl-38616e6e:\\"));
+            //                                        commandLine: GameInfo.Subnautica.MsStoreStartUrl));
             await Task.CompletedTask; // Suppresses async-without-await warning - can be removed.
             throw new NotImplementedException($"{Name} support is unavailable");
         }
