b04ffd2f864a36d8c4da699ed48fbe0f71bb3b4e
Measurity <measuring.infinity@gmail.com>
2021-10-20 23:56:01 +0200

    Updated StoryGoal_Execute_Patch code as well

--- NitroxPatcher/Patches/Dynamic/StoryGoal_Execute_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/StoryGoal_Execute_Patch.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using HarmonyLib;
 using NitroxClient.Communication.Abstract;
-using NitroxModel.Core;
+using NitroxModel.Helper;
 using NitroxModel.Packets;
 using Story;
 
@@ -10,18 +9,14 @@ namespace NitroxPatcher.Patches.Dynamic
 {
     public class StoryGoal_Execute_Patch : NitroxPatch, IDynamicPatch
     {
-        public static readonly Type TARGET_CLASS = typeof(StoryGoal);
-        public static readonly MethodInfo TARGET_METHOD = TARGET_CLASS.GetMethod(nameof(StoryGoal.Execute), BindingFlags.Public | BindingFlags.Static);
+        public static readonly MethodInfo TARGET_METHOD = ReflectionHelper.GetMethodInfo(() => StoryGoal.Execute(default(string), default(Story.GoalType)));
 
-        private static IPacketSender packetSender;
-        private static IPacketSender PacketSender => packetSender ??= NitroxServiceLocator.LocateService<IPacketSender>();
-        
         public static void Prefix(string key, Story.GoalType goalType)
         {
             if (!StoryGoalManager.main.completedGoals.Contains(key))
             {
                 StoryEventSend packet = new((StoryEventSend.EventType)goalType, key);
-                PacketSender.Send(packet);
+                Resolve<IPacketSender>().Send(packet);
             }
         }
 
