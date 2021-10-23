004512e6349542cf2b20d5919b29472a433ea0d0
Coding-Hen <8798074+Coding-Hen@users.noreply.github.com>
2021-10-23 15:51:31 +0100

    Added defaults for storygoal execute patch

--- NitroxPatcher/Patches/Dynamic/StoryGoal_Execute_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/StoryGoal_Execute_Patch.cs
@@ -9,7 +9,11 @@ namespace NitroxPatcher.Patches.Dynamic
 {
     public class StoryGoal_Execute_Patch : NitroxPatch, IDynamicPatch
     {
+#if SUBNAUTICA
         public static readonly MethodInfo TARGET_METHOD = ReflectionHelper.GetMethodInfo(() => StoryGoal.Execute(default(string), default(Story.GoalType)));
+#elif BELOWZERO
+        public static readonly MethodInfo TARGET_METHOD = ReflectionHelper.GetMethodInfo(() => StoryGoal.Execute(default(string), default(Story.GoalType), default(bool), default(bool)));
+#endif
 
         public static void Prefix(string key, Story.GoalType goalType)
         {
