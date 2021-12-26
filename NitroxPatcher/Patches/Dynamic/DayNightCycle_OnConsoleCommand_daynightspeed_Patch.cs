881df0c35210bb68ed7b79a4426ff6772dfcd0a4
Jannify <23176718+Jannify@users.noreply.github.com>
2021-12-26 20:29:51 +0100

    Added speed "functionality"

---
+++ NitroxPatcher/Patches/Dynamic/DayNightCycle_OnConsoleCommand_daynightspeed_Patch.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+public class DayNightCycle_OnConsoleCommand_daynightspeed_Patch : NitroxPatch, IDynamicPatch
+{
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((DayNightCycle t) => t.OnConsoleCommand_daynightspeed(default));
+
+    // The command is skipped because simulating speed reliable on the server is out of scope
+    public static bool Prefix()
+    {
+#if DEBUG
+        ErrorMessage.AddMessage("This command is not fully supported. Expect breaking your instance/world.");
+        return true;
+#else
+        ErrorMessage.AddMessage(Language.main.Get("Nitrox_CommandNotAvailable"));
+        return false;
+#endif
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
+    }
+}
---
+++ NitroxPatcher/Patches/Dynamic/SpeedConsoleCommand_OnConsoleCommand_speed_Patch.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+public class SpeedConsoleCommand_OnConsoleCommand_speed_Patch : NitroxPatch, IDynamicPatch
+{
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((SpeedConsoleCommand t) => t.OnConsoleCommand_speed(default));
+
+    // The command is skipped because simulating speed reliable is totally out of scope
+    public static bool Prefix()
+    {
+#if DEBUG
+        ErrorMessage.AddMessage("This command is not fully supported. Expect breaking your instance/world.");
+        return true;
+#else
+        ErrorMessage.AddMessage(Language.main.Get("Nitrox_CommandNotAvailable"));
+        return false;
+#endif
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
+    }
+}
