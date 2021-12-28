9942b6a2752e7f1d6b2a877da1d2b56238d110d8
tornac1234 <tortornac@gmail.com>
2021-12-28 15:39:36 +0100

    Modified some namespace format and removed a validation in the transpiler

--- Nitrox.Test/Patcher/Patches/CyclopsSonarButton_Update_PatchTest.cs
+++ Nitrox.Test/Patcher/Patches/CyclopsSonarButton_Update_PatchTest.cs
@@ -5,47 +5,45 @@ using System.Reflection.Emit;
 using HarmonyLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NitroxModel.Helper;
-using NitroxPatcher.Patches.Dynamic;
 using NitroxTest.Patcher.Test;
 
-namespace Nitrox.Test.Patcher.Patches
+namespace NitroxPatcher.Patches.Dynamic;
+
+[TestClass]
+public class CyclopsSonarButton_Update_PatchTest
 {
-    [TestClass]
-    public class CyclopsSonarButton_Update_PatchTest
+    [TestMethod]
+    public void Sanity()
     {
-        [TestMethod]
-        public void Sanity()
+        List<CodeInstruction> instructions = PatchTestHelper.GenerateDummyInstructions(100);
+        instructions.Add(new CodeInstruction(CyclopsSonarButton_Update_Patch.INJECTION_OPCODE, CyclopsSonarButton_Update_Patch.INJECTION_OPERAND));
+        instructions.Add(new CodeInstruction(OpCodes.Callvirt, Reflect.Method((Player player) => player.GetMode())));
+        instructions.Add(new CodeInstruction(OpCodes.Brtrue));
+
+        IEnumerable<CodeInstruction> result = CyclopsSonarButton_Update_Patch.Transpiler(null, instructions);
+        Assert.AreEqual(instructions.Count + 2, result.Count());
+    }
+
+    [TestMethod]
+    public void InjectionSanity()
+    {
+        ReadOnlyCollection<CodeInstruction> beforeInstructions = PatchTestHelper.GetInstructionsFromMethod(CyclopsSonarButton_Update_Patch.TARGET_METHOD);
+        IEnumerable<CodeInstruction> result = CyclopsSonarButton_Update_Patch.Transpiler(CyclopsSonarButton_Update_Patch.TARGET_METHOD, beforeInstructions);
+
+        Assert.IsTrue(beforeInstructions.Count < result.Count());
+    }
+
+    [TestMethod]
+    public void CheckMethodValidity()
+    {
+        ReadOnlyCollection<CodeInstruction> instructions = PatchTestHelper.GetInstructionsFromMethod(CyclopsSonarButton_Update_Patch.TARGET_METHOD);
+        for (int i = 0; i < instructions.Count; i++)
         {
-            List<CodeInstruction> instructions = PatchTestHelper.GenerateDummyInstructions(100);
-            instructions.Add(new CodeInstruction(CyclopsSonarButton_Update_Patch.INJECTION_OPCODE, CyclopsSonarButton_Update_Patch.INJECTION_OPERAND));
-            instructions.Add(new CodeInstruction(OpCodes.Callvirt, Reflect.Method((Player player) => player.GetMode())));
-            instructions.Add(new CodeInstruction(OpCodes.Brtrue));
-
-            IEnumerable<CodeInstruction> result = CyclopsSonarButton_Update_Patch.Transpiler(null, instructions);
-            Assert.AreEqual(instructions.Count + 2, result.Count());
-        }
-
-        [TestMethod]
-        public void InjectionSanity()
-        {
-            ReadOnlyCollection<CodeInstruction> beforeInstructions = PatchTestHelper.GetInstructionsFromMethod(CyclopsSonarButton_Update_Patch.TARGET_METHOD);
-            IEnumerable<CodeInstruction> result = CyclopsSonarButton_Update_Patch.Transpiler(CyclopsSonarButton_Update_Patch.TARGET_METHOD, beforeInstructions);
-
-            Assert.IsTrue(beforeInstructions.Count < result.Count());
-        }
-
-        [TestMethod]
-        public void CheckMethodValidity()
-        {
-            ReadOnlyCollection<CodeInstruction> instructions = PatchTestHelper.GetInstructionsFromMethod(CyclopsSonarButton_Update_Patch.TARGET_METHOD);
-            for (int i = 0; i < instructions.Count; i++)
+            CodeInstruction instruction = instructions[i];
+            if (instruction.opcode.Equals(CyclopsSonarButton_Update_Patch.INJECTION_OPCODE) && instruction.operand.Equals(CyclopsSonarButton_Update_Patch.INJECTION_OPERAND))
             {
-                CodeInstruction instruction = instructions[i];
-                if (instruction.opcode.Equals(CyclopsSonarButton_Update_Patch.INJECTION_OPCODE) && instruction.operand.Equals(CyclopsSonarButton_Update_Patch.INJECTION_OPERAND))
-                {
-                    CodeInstruction nextBr = instructions[i + 2];
-                    Assert.IsTrue(nextBr.opcode.Equals(OpCodes.Brtrue), "Looks like subnautica code has changed. Update jump offset!");
-                }
+                CodeInstruction nextBr = instructions[i + 2];
+                Assert.IsTrue(nextBr.opcode.Equals(OpCodes.Brtrue), "Looks like subnautica code has changed. Update jump offset!");
             }
         }
     }
--- NitroxPatcher/Patches/Dynamic/CyclopsMotorMode_RestoreEngineState_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/CyclopsMotorMode_RestoreEngineState_Patch.cs
@@ -2,21 +2,20 @@
 using HarmonyLib;
 using NitroxModel.Helper;
 
-namespace NitroxPatcher.Patches.Dynamic
+namespace NitroxPatcher.Patches.Dynamic;
+
+public class CyclopsMotorMode_RestoreEngineState_Patch : NitroxPatch, IDynamicPatch
 {
-    public class CyclopsMotorMode_RestoreEngineState_Patch : NitroxPatch, IDynamicPatch
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((CyclopsMotorMode t) => t.RestoreEngineState());
+
+    public static bool Prefix()
     {
-        public static readonly MethodInfo TARGET_METHOD = Reflect.Method((CyclopsMotorMode t) => t.RestoreEngineState());
+        // We don't want this to happen because it will prevent players that were far of the cyclops when spawning to see its actual engine state
+        return false;
+    }
 
-        public static bool Prefix()
-        {
-            // We don't want this to happen because it will prevent players that were far of the cyclops when spawning to see its actual engine state
-            return false;
-        }
-
-        public override void Patch(Harmony harmony)
-        {
-            PatchPrefix(harmony, TARGET_METHOD);
-        }
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
     }
 }
