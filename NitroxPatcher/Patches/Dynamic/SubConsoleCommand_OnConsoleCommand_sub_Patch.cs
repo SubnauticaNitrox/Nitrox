a4feb39b3dad22352f6be19d9fde9661437d66fd
Jannify <23176718+Jannify@users.noreply.github.com>
2021-12-29 23:54:48 +0100

    Added sub command and more translations

--- NitroxPatcher/Patches/Dynamic/SubConsoleCommand_OnConsoleCommand_sub_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/SubConsoleCommand_OnConsoleCommand_sub_Patch.cs
@@ -3,24 +3,24 @@ using HarmonyLib;
 using NitroxClient.GameLogic;
 using NitroxModel.Helper;
 
-namespace NitroxPatcher.Patches.Dynamic
+namespace NitroxPatcher.Patches.Dynamic;
+
+/// <summary>
+/// Called whenever a Cyclops or Seamoth is spawned. Nitrox already has <see cref="Vehicles.CreateVehicle(NitroxModel.DataStructures.GameLogic.VehicleModel)"/> to
+/// spawn vehicles. This patch is only meant to block the method from executing, causing two vehicles to be spawned instead of one
+/// </summary>
+public class SubConsoleCommand_OnConsoleCommand_sub_Patch : NitroxPatch, IDynamicPatch
 {
-    /// <summary>
-    /// Called whenever a Cyclops or Seamoth is spawned. Nitrox already has <see cref="Vehicles.CreateVehicle(NitroxModel.DataStructures.GameLogic.VehicleModel)"/> to
-    /// spawn vehicles. This patch is only meant to block the method from executing, causing two vehicles to be spawned instead of one
-    /// </summary>
-    class SubConsoleCommand_OnConsoleCommand_sub_Patch : NitroxPatch, IDynamicPatch
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((SubConsoleCommand t) => t.OnConsoleCommand_sub(default));
+
+    public static bool Prefix()
     {
-        private static readonly MethodInfo TARGET_METHOD = Reflect.Method((SubConsoleCommand t) => t.OnConsoleCommand_sub(default(NotificationCenter.Notification)));
+        ErrorMessage.AddMessage(Language.main.Get("Nitrox_CommandNotAvailable"));
+        return false;
+    }
 
-        public static bool Prefix(SubConsoleCommand __instance, NotificationCenter.Notification n)
-        {
-            return false;
-        }
-
-        public override void Patch(Harmony harmony)
-        {
-            PatchPrefix(harmony, TARGET_METHOD);
-        }
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
     }
 }
