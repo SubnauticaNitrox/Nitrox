4cec0d07bec6c0bbd24d431d0fdcffceb08d3cac
rootcan <tortornac@gmail.com>
2021-12-31 01:46:34 +0100

    Fix lockers label disappearing (#1693)
    * Made sign input update metadata depending on the type of its holder
    
    * Added default case to metadata change swith and modified ToString() to use interpolation
    
    * Smol modifications (refactoring)
    
    * Removed any link between SignMetadata and EntitySignMetadata

---
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/EntitySignMetadata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ProtoBufNet;
+
+namespace NitroxModel.DataStructures.GameLogic.Entities.Metadata;
+
+[Serializable]
+[ProtoContract]
+public class EntitySignMetadata : EntityMetadata
+{
+    [ProtoMember(1)]
+    public string Text { get; set; }
+
+    [ProtoMember(2)]
+    public int ColorIndex { get; set; }
+
+    [ProtoMember(3)]
+    public int ScaleIndex { get; set; }
+
+    [ProtoMember(4)]
+    public bool[] Elements { get; set; }
+
+    [ProtoMember(5)]
+    public bool Background { get; set; }
+
+    protected EntitySignMetadata()
+    {
+        //Constructor for serialization. Has to be "protected" for json serialization.
+    }
+
+    public EntitySignMetadata(string text, int colorIndex, int scaleIndex, bool[] elements, bool background)
+    {
+        Text = text;
+        ColorIndex = colorIndex;
+        ScaleIndex = scaleIndex;
+        Elements = elements;
+        Background = background;
+    }
+
+    public override string ToString()
+    {
+        return $"[EntitySignMetadata - Text: {Text}, ColorIndex: {ColorIndex}, ScaleIndex: {ScaleIndex}, Elements: {Elements}, Background: {Background}]";
+    }
+}
