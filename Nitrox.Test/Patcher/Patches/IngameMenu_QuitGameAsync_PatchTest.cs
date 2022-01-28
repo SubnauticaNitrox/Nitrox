65cba53b5c90d5555414e53c2605e1cd21d88131
Jannify <23176718+Jannify@users.noreply.github.com>
2022-01-28 23:04:20 +0100

    Fixing NRE "routine is null" (#1712)

---
+++ Nitrox.Test/Patcher/Patches/IngameMenu_QuitGameAsync_PatchTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NitroxPatcher.Patches.Dynamic;
+using NitroxTest.Patcher.Test;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+[TestClass]
+public class IngameMenu_QuitGameAsync_PatchTest
+{
+    [TestMethod]
+    public void Sanity()
+    {
+        List<CodeInstruction> instructions = PatchTestHelper.GenerateDummyInstructions(100);
+        instructions.Add(new CodeInstruction(OpCodes.Call, IngameMenu_QuitGameAsync_Patch.triggerOperand));
+
+        IEnumerable<CodeInstruction> result = IngameMenu_QuitGameAsync_Patch.Transpiler(instructions);
+
+        Assert.AreEqual(instructions.Count, result.Count());
+    }
+
+    [TestMethod]
+    public void InjectionSanity()
+    {
+        IEnumerable<CodeInstruction> beforeInstructions = PatchTestHelper.GetInstructionsFromMethod(IngameMenu_QuitGameAsync_Patch.targetMethod);
+        IEnumerable<CodeInstruction> result = IngameMenu_QuitGameAsync_Patch.Transpiler(beforeInstructions);
+
+        Assert.IsTrue(beforeInstructions.Count() == result.Count()); // The ifs in the target method are all false in the testing environment
+                                                                     // so we are left with the same code instructions.
+    }
+}
--- NitroxPatcher/Patches/Dynamic/IngameMenu_QuitSubscreen_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/IngameMenu_QuitSubscreen_Patch.cs
@@ -2,21 +2,20 @@
 using HarmonyLib;
 using NitroxModel.Helper;
 
-namespace NitroxPatcher.Patches.Dynamic
+namespace NitroxPatcher.Patches.Dynamic;
+
+public class IngameMenu_QuitSubscreen_Patch : NitroxPatch, IDynamicPatch
 {
-    public class IngameMenu_QuitSubscreen_Patch : NitroxPatch, IDynamicPatch
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((IngameMenu t) => t.QuitSubscreen());
+
+    public static bool Prefix()
     {
-        private static readonly MethodInfo TARGET_METHOD = Reflect.Method((IngameMenu t) => t.QuitSubscreen());
+        IngameMenu.main.QuitGame(false);
+        return false;
+    }
 
-        public static bool Prefix()
-        {
-            IngameMenu.main.QuitGame(false);
-            return false;
-        }
-
-        public override void Patch(Harmony harmony)
-        {
-            PatchPrefix(harmony, TARGET_METHOD);
-        }
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
     }
 }
