1232cb95fdd113f68da1a8189b10a1cc100d6ca6
LionZXY <nikita@kulikof.ru>
2022-02-09 17:43:58 +0300

    Revert changes in InventoryManager.cs

--- NitroxServer/GameLogic/Items/InventoryManager.cs
+++ NitroxServer/GameLogic/Items/InventoryManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using NitroxModel.DataStructures;
 using NitroxModel.DataStructures.GameLogic;
@@ -16,11 +16,6 @@ namespace NitroxServer.GameLogic.Items
             inventoryItemsById = new ThreadSafeDictionary<NitroxId, ItemData>(inventoryItems.ToDictionary(item => item.ItemId), false);
             storageSlotItemsByContainerId = new ThreadSafeDictionary<NitroxId, ItemData>(storageSlotItems.ToDictionary(item => item.ContainerId), false);
             modulesById = new ThreadSafeDictionary<NitroxId, EquippedItemData>(modules.ToDictionary(module => module.ItemId), false);
-            Log.Debug($"Create InventoryManager with {modules.Count} modules");
-            foreach (EquippedItemData module in modules)
-            {
-                Log.Debug($"Module {module.ItemId} in container {module.ContainerId} with slot {module.Slot}");
-            }
         }
 
         public void InventoryItemAdded(ItemData itemData)
@@ -57,7 +52,6 @@ namespace NitroxServer.GameLogic.Items
         public void ModuleAdded(EquippedItemData itemData)
         {
             modulesById[itemData.ItemId] = itemData;
-            Log.Debug($"Received module {itemData.ItemId} to container {itemData.ContainerId} and slot {itemData.Slot}. Total modules: {modulesById.Count}");
         }
 
         public bool ModuleRemoved(NitroxId ownerId)
