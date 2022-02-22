754b52be0c55522794e21268a1206632b7983bf6
tornac1234 <tortornac@gmail.com>
2022-02-22 01:10:43 +0100

    Fix an issue where duplicated item id in modules blocked world loading

--- NitroxServer/Serialization/SaveDataUpgrades/Upgrade_V1600.cs
+++ NitroxServer/Serialization/SaveDataUpgrades/Upgrade_V1600.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using NitroxModel.DataStructures;
 using NitroxServer.Serialization.Upgrade;
 
 namespace NitroxServer.Serialization.SaveDataUpgrades
@@ -30,6 +31,19 @@ namespace NitroxServer.Serialization.SaveDataUpgrades
                 }
             }
             data["GameData"]["PDAState"]["PdaLog"] = new JArray(cleanPdaLog);
+
+            Dictionary<string, JToken> modules = new();
+            foreach (JToken moduleEntry in data["InventoryData"]["Modules"])
+            {
+                JToken itemId = moduleEntry["ItemId"];
+                if (modules.ContainsKey(itemId.ToString()))
+                {
+                    itemId = new NitroxId().ToString();
+                    moduleEntry["ItemId"] = itemId;
+                }
+                modules.Add(itemId.ToString(), moduleEntry);
+            }
+            data["InventoryData"]["Modules"] = new JArray(modules.Values);
 
             data.Property("ServerStartTime")?.Remove();
         }
