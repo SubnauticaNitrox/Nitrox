6f52a7c2c1087728944a18db972540890faea0c2
tornac1234 <tortornac@gmail.com>
2022-03-05 14:27:23 +0100

    Added the possibility to freeze game from modals

--- NitroxPatcher/Patches/Dynamic/FreezeTime_Begin_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/FreezeTime_Begin_Patch.cs
@@ -1,17 +1,24 @@
 ﻿using System.Reflection;
 using HarmonyLib;
+using NitroxClient.MonoBehaviours.Gui.InGame;
 using NitroxModel.Helper;
 using UWE;
 
+/// <summary>
+/// Because we're in multiplayer mode, we generally don't want the game to freeze
+/// </summary>
 namespace NitroxPatcher.Patches.Dynamic
 {
     public class FreezeTime_Begin_Patch : NitroxPatch, IDynamicPatch
     {
         private static readonly MethodInfo TARGET_METHOD = Reflect.Method(() => FreezeTime.Begin(default(string), default(bool)));
 
-        public static bool Prefix(string userId, bool dontPauseSound)
+        // We don't want to prevent from freezing the game if the opened modal wants to freeze the game
+        public static bool Prefix(string userId)
         {
-            return userId.Equals("FeedbackPanel");
+            // If we ask to freeze from a Nitrox modal, userId will be like this: NitroxServerStoppedModal, so we need to remove "Nitrox" to access the modal's name
+            return userId.Equals("FeedbackPanel") ||
+                   (Modal.ModalsPerSubWindowName.TryGetValue(userId.Replace("Nitrox", ""), out Modal modal) && modal.FreezeGame);
         }
 
         public override void Patch(Harmony harmony)
