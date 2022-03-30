7abb7e150d5867960c2ee652f8cf07f24d8a17c5
rootcan <tortornac@gmail.com>
2022-03-30 19:27:30 +0200

    Improved the modal system (#1720)
    * Added a "no longer connected" text that stays on the screen when disconnected by the server
    
    * Moved the text to the language files
    
    * Added a different text for when server is stopped
    
    * Made a generic Modal class
    
    * Added the possibility to freeze game from modals
    
    * Switched current modals to new modal system
    
    * Added an interval between ServerStopped packet and actual server stop
    
    * Disconnect client when receiving the server stopped packet
    
    * Removed adding component in Multiplayer class
    
    * Updated FreezeTime modal detection
    
    * Updated base modal class to only have optional parameters
    
    * Added a way to know which Modal is currently open
    
    * Modified a comment :)
    
    * Changed Modal class to no longer be a MonoBehaviour

---
+++ NitroxPatcher/Patches/Dynamic/IngameMenu_OnDeselect_Patch.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using NitroxClient.MonoBehaviours.Gui.InGame;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+/// <summary>
+/// Disable the possibility of escaping a modal by clicking outside of its box or pressing the escape for the modals that want it
+/// </summary>
+public class IngameMenu_OnDeselect_Patch : NitroxPatch, IDynamicPatch
+{
+    private static MethodInfo TARGET_METHOD = Reflect.Method((IngameMenu t) => t.OnDeselect());
+
+    // OnDeselect happens when you deselect the modal (by clicking outside or pressing escape)
+    // Therefore, if we prevent it from happening it certain cases, it will force the user to click one of the buttons of the modal
+    public static bool Prefix()
+    {
+        // Cancel if the modal is set to be non-avoidable (IsAvoidable) and if it's not the modal that wants to close itself (IsAvoidableBypass)
+        if (Modal.CurrentModal != null && !Modal.CurrentModal.IsAvoidable && !Modal.CurrentModal.IsAvoidableBypass)
+        {
+            return false;
+        }
+        Modal.CurrentModal = null;
+        return true;
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
+    }
+}
