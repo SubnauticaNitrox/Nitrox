e6e9d766b2951ba221e8712f5682ed335bdaa42d
Measurity <measuring.infinity@gmail.com>
2022-04-21 20:47:00 +0200

    Added docs for some classes in NitroxModel-Subnautica (#1788)

--- NitroxModel-Subnautica/Logger/SubnauticaInGameLogger.cs
+++ NitroxModel-Subnautica/Logger/SubnauticaInGameLogger.cs
@@ -2,6 +2,10 @@
 
 namespace NitroxModel_Subnautica.Logger
 {
+    /// <summary>
+    ///     Log handler for logging and showing information to the player.
+    ///     The log is also written to the log file.
+    /// </summary>
     public class SubnauticaInGameLogger : InGameLogger
     {
         public void Log(object message) => Log(message?.ToString());
@@ -11,6 +15,7 @@ namespace NitroxModel_Subnautica.Logger
             {
                 return;
             }
+            // Logs to the top-left of the screen using UWE code.
             ErrorMessage.AddMessage(message);
         }
     }
