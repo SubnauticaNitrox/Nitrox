25054430d9191c57f7cb9fdf88bab84a20f52a43
killzoms <killzoms@users.noreply.github.com>
2022-05-14 16:20:04 -0400

    Fixes null ref in PlayerManager (#1729)
    * Fixes null ref in PlayerManager
    
    A player could leave while in the JoinQueue and it would cause a null reference when trying to reserve that player later
    
    * Better spot
    
    * Move FinishProcessingReservation out of null check
    
    * added logging
    
    * overhauled the InitialSyncTimer
    
    * removed some extra whitespace
    
    * requested changes

---
+++ NitroxServer/Communication/ConnectionState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroxServer.Communication
+{
+    public enum NitroxConnectionState
+    {
+        Unknown,
+        Disconnected,
+        Connected,
+        Reserved,
+        InGame
+    }
+}
