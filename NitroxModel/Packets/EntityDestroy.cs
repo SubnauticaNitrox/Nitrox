019cb4759cccc5a4dd09e08c2251cf8d84d2d722
Jannify <23176718+Jannify@users.noreply.github.com>
2022-05-14 22:10:41 +0200

    Update NitroxModel/Packets/EntityDestroy.cs

--- NitroxModel/Packets/EntityDestroy.cs
+++ NitroxModel/Packets/EntityDestroy.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using NitroxModel.DataStructures;
 
-namespace NitroxModel.Packets
-{
-    [Serializable]
-    public class EntityDestroy : Packet
-    {
-        public NitroxId EntityId { get; }
+namespace NitroxModel.Packets;
 
-        public EntityDestroy(NitroxId entityId)
-        {
-            EntityId = entityId;
-        }
+[Serializable]
+public class EntityDestroy : Packet
+{
+    public NitroxId EntityId { get; }
+
+    public EntityDestroy(NitroxId entityId)
+    {
+        EntityId = entityId;
     }
 }
