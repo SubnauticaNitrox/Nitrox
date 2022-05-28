fe387a312f58a8e0a7a978eef861db28d7289706
tornac1234 <tortornac@gmail.com>
2022-05-28 15:26:15 +0200

    Added pings visibility inside PlayerListTab

---
+++ NitroxPatcher/Patches/Dynamic/uGUI_Pings_OnWillRenderCanvases_Patch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+/// <summary>
+/// Decide whether or not we want to render the pings on the screen
+/// </summary>
+public class uGUI_Pings_OnWillRenderCanvases_Patch : NitroxPatch, IDynamicPatch
+{
+    private static MethodInfo TARGET_METHOD = Reflect.Method((uGUI_Pings t) => t.OnWillRenderCanvases());
+
+    private static readonly OpCode INJECTION_OPCODE = OpCodes.Call;
+    private static readonly object INJECTION_OPERAND = Reflect.Method((uGUI_Pings t) => t.IsVisibleNow());
+
+    public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions)
+    {
+        Validate.NotNull(INJECTION_OPERAND);
+
+        List<CodeInstruction> instructionList = instructions.ToList();
+
+        for (int i = 0; i < instructionList.Count; i++)
+        {
+            CodeInstruction instruction = instructionList[i];
+            if (instruction.opcode.Equals(INJECTION_OPCODE) && instruction.operand.Equals(INJECTION_OPERAND))
+            {
+                /*
+                 * Replace
+                 * bool flag = this.IsVisibleNow();
+                 * by
+                 * bool flag = uGUI_Pings_OnWillRenderCanvases_Patch.IsVisible(this);
+                 */
+                instruction.operand = Reflect.Method(() => IsVisible(default));
+            }
+            yield return instruction;
+        }
+    }
+
+    private static bool IsVisible(uGUI_Pings t)
+    {
+        if (t.IsVisibleNow())
+        {
+            return true;
+        }
+        if (!uGUI_PDA.main)
+        {
+            return false;
+        }
+        return uGUI_PDA.main.currentTabType == uGUI_PDA_Initialize_Patch.PLAYERLIST;
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchTranspiler(harmony, TARGET_METHOD);
+    }
+}
