0948730db4304738364a9d44e3252a39c8751551
rootcan <tortornac@gmail.com>
2022-10-30 20:25:32 +0100

    Fix some occurrences of player inventory clear (#1878)
    * Prevent building on a player and destroying a building where there's a player
    
    * Prevent a player from sending inventory events to other players inventories
    
    * Moved some stuff and renamed some things
    
    * Make sure to destroy an item that has been removed from a remote player's inventory

---
+++ NitroxClient/GameLogic/PlayerLogic/RemotePlayerIdentifier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace NitroxClient.GameLogic.PlayerLogic;
+
+/// <summary>
+/// Attached to a RemotePlayer. Useful to determine that this script's GameObject is in the EntityRoot of a RemotePlayer.
+/// </summary>
+/// <remarks>
+/// The EntityRoot of an object is defined as the top most GameObject as when an object hierarchy first was spawned in. Either from a prefab, or in Nitrox' case, a cloned root game object.
+/// </remarks>
+public class RemotePlayerIdentifier : MonoBehaviour, IObstacle
+{
+    public RemotePlayer RemotePlayer;
+
+    public bool CanDeconstruct(out string reason)
+    {
+        reason = Language.main.Get("Nitrox_RemotePlayerObstacle");
+        return false;
+    }
+}
---
+++ NitroxPatcher/Patches/Dynamic/Base_OnPreDestroy_Patch.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using NitroxClient.GameLogic.PlayerLogic;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+/// <summary>
+/// Before a base gets destroyed, we eventually detach/exit any remote player's object that would be inside so that their GameObjects don't get destroyed
+/// </summary>
+public class Base_OnPreDestroy_Patch : NitroxPatch, IDynamicPatch
+{
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((Base t) => t.OnPreDestroy());
+
+    public static void Prefix(Base __instance)
+    {
+        foreach (RemotePlayerIdentifier remotePlayerIdentifier in __instance.GetComponentsInChildren<RemotePlayerIdentifier>(true))
+        {
+            remotePlayerIdentifier.RemotePlayer.Detach();
+        }
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD);
+    }
+}
