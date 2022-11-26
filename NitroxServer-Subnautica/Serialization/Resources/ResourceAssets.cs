12b656de335ccb3d0677f992fcb0b5c9e29230ae
Measurity <measuring.infinity@gmail.com>
2022-11-27 00:21:28 +0100

    Changed where Validate.IsTrue was used for null checking

--- NitroxServer-Subnautica/Serialization/Resources/ResourceAssets.cs
+++ NitroxServer-Subnautica/Serialization/Resources/ResourceAssets.cs
@@ -9,16 +9,16 @@ namespace NitroxServer_Subnautica.Serialization.Resources
 {
     public class ResourceAssets
     {
-        public Dictionary<string, WorldEntityInfo> WorldEntitiesByClassId { get; } = new Dictionary<string, WorldEntityInfo>();
-        public Dictionary<string, GameObjectAsset> PrefabsByClassId { get; } = new Dictionary<string, GameObjectAsset>();
+        public Dictionary<string, WorldEntityInfo> WorldEntitiesByClassId { get; } = new();
+        public Dictionary<string, GameObjectAsset> PrefabsByClassId { get; } = new();
         public string LootDistributionsJson { get; set; } = "";
-        public Dictionary<string, PrefabPlaceholdersGroupAsset> PrefabPlaceholderGroupsByGroupClassId = new Dictionary<string, PrefabPlaceholdersGroupAsset>();
+        public readonly Dictionary<string, PrefabPlaceholdersGroupAsset> PrefabPlaceholderGroupsByGroupClassId = new();
 
         public RandomStartGenerator NitroxRandom;
 
         public static void ValidateMembers(ResourceAssets resourceAssets)
         {
-            Validate.IsFalse(resourceAssets == null);
+            Validate.NotNull(resourceAssets);
             Validate.IsTrue(resourceAssets.WorldEntitiesByClassId.Count > 0);
             Validate.IsTrue(resourceAssets.LootDistributionsJson != "");
             Validate.IsTrue(resourceAssets.PrefabPlaceholderGroupsByGroupClassId.Count > 0);
