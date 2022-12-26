zombital/NitroxForMe

Large refactoring to explicitly separate out entity types. Immediately, this facilitates much smaller packet sizes in prefab dense zones such as the aurora.  From a code standpoint, this offers separation of concerns and opens up opportunity to unify redundant systems such as items (which are essentially entities that also need persisted metadata).  This also removes the concept of storing serialized game objects (such as on drop) because they are a lazy way of not implementing the appropriate metadata payload.
Known regressions:
- Ping no longer updating (need to convert this to entity metadata system)
- Dropped items resetting state (such as a battery - again need to use entity metadata system)

NitroxServer-Subnautica/GameLogic/Entities/Spawning/UntypedCellEntityBootstrapper.cs
@@ -1,4 +1,4 @@
-﻿using NitroxModel.DataStructures.GameLogic;
+﻿using NitroxModel.DataStructures.GameLogic.Entities;
 using NitroxServer.GameLogic.Entities.Spawning;
 
 namespace NitroxServer_Subnautica.GameLogic.Entities.Spawning.EntityBootstrappers
@@ -13,7 +13,7 @@ namespace NitroxServer_Subnautica.GameLogic.Entities.Spawning.EntityBootstrapper
 
         private readonly string cellRootClassId = "55d7ab35-de97-4d95-af6c-ac8d03bb54ca";
 
-        public void Prepare(Entity entity, Entity parentEntity, DeterministicBatchGenerator deterministicBatchGenerator)
+        public void Prepare(WorldEntity entity, WorldEntity parentEntity, DeterministicBatchGenerator deterministicBatchGenerator)
         {
             bool hasCellRootAsParent = (parentEntity != null && parentEntity.ClassId == cellRootClassId);
 

SubnauticaNitrox/Nitrox

NitroxServer-Subnautica/GameLogic/Entities/Spawning/UntypedCellEntityBootstrapper.cs
@@ -1,4 +1,4 @@
-﻿using NitroxModel.DataStructures.GameLogic;
+using NitroxModel.DataStructures.GameLogic.Entities;
 using NitroxServer.GameLogic.Entities.Spawning;
 using NitroxServer.Helper;
 
@@ -15,12 +15,12 @@ public class UntypedCellEntityBootstrapper : IEntityBootstrapper
     /// </summary>
     private readonly string cellRootClassId = "55d7ab35-de97-4d95-af6c-ac8d03bb54ca";
 
-    public void Prepare(Entity entity, Entity parentEntity, DeterministicGenerator deterministicBatchGenerator)
+    public void Prepare(WorldEntity entity, WorldEntity parentEntity, DeterministicGenerator deterministicBatchGenerator)
     {
         bool hasCellRootAsParent = parentEntity != null && parentEntity.ClassId == cellRootClassId;
 
         if (hasCellRootAsParent && parentEntity.Level < entity.Level)
-        {
+        { 
             // bump the cell root up to the child's visibility level
             parentEntity.Level = entity.Level;
         }