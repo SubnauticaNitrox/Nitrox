0b5189accdbdae7d4072b81444f6dbda739947cb
Jannify <23176718+Jannify@users.noreply.github.com>
2022-12-30 19:48:34 +0100

    Reworking serializers to work with DataContract/DataMember

--- NitroxServer/Serialization/SaveDataUpgrades/SaveDataUpgrade.cs
+++ NitroxServer/Serialization/SaveDataUpgrades/SaveDataUpgrade.cs
@@ -3,7 +3,7 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
-using NitroxModel.DataStructures.JsonConverter;
+using NitroxServer.Serialization.Json;
 
 namespace NitroxServer.Serialization.Upgrade
 {
