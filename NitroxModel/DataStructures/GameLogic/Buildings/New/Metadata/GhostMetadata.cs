4d1301d6a86424d1cfc16ceeae4df6500050e405
tornac1234 <tortornac@gmail.com>
2022-12-30 19:55:47 +0100

    Added the required ProtoBufNet attributes to the new data classes

--- NitroxModel/DataStructures/GameLogic/Buildings/New/Metadata/GhostMetadata.cs
+++ NitroxModel/DataStructures/GameLogic/Buildings/New/Metadata/GhostMetadata.cs
@@ -1,18 +1,27 @@
+using ProtoBufNet;
+
 namespace NitroxModel.DataStructures.GameLogic.Buildings.New.Metadata;
 
+[ProtoContract]
 public abstract class GhostMetadata
 {
+    [ProtoMember(1)]
     public NitroxInt3 TargetOffset;
 }
 
+[ProtoContract]
 public class BasicGhostMetadata : GhostMetadata { }
 
+[ProtoContract]
 public class BaseAnchoredFaceGhostMetadata : GhostMetadata
 {
+    [ProtoMember(1)]
     public NitroxBaseFace? AnchoredFace;
 }
 
+[ProtoContract]
 public class BaseAnchoredCellGhostMetadata : GhostMetadata
 {
+    [ProtoMember(1)]
     public NitroxInt3? AnchoredCell;
 }
