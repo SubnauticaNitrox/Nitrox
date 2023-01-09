7f4fc60dced33072cb37353e1058fb840f524aa3
tornac1234 <tortornac@gmail.com>
2023-01-09 21:25:00 +0100

    Changed the way InitialSyncProcessor processing works (dit not deprecate old way yet)

--- NitroxClient/GameLogic/InitialSync/StoryGoalInitialSyncProcessor.cs
+++ NitroxClient/GameLogic/InitialSync/StoryGoalInitialSyncProcessor.cs
@@ -1,105 +1,123 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NitroxClient.GameLogic.InitialSync.Base;
+using NitroxModel.Core;
 using NitroxModel.DataStructures.GameLogic;
 using NitroxModel.Packets;
 using Story;
+using UnityEngine;
 
-namespace NitroxClient.GameLogic.InitialSync
+namespace NitroxClient.GameLogic.InitialSync;
+
+public class StoryGoalInitialSyncProcessor : InitialSyncProcessor
 {
-    public class StoryGoalInitialSyncProcessor : InitialSyncProcessor
+    public override List<IEnumerator> GetSteps(InitialPlayerSync packet, WaitScreen.ManualWaitItem waitScreenItem)
     {
+        return new List<IEnumerator> {
+            SetTimeData(packet),
+            SetRadioQueue(packet),
+            SetCompletedStoryGoals(packet),
+            SetGoalUnlocks(packet),
+            SetBiomeGoalTrackerGoals(packet),
+            SetScheduledGoals(packet)
+        };
+    }
 
-        public override IEnumerator Process(InitialPlayerSync packet, WaitScreen.ManualWaitItem waitScreenItem)
+    private IEnumerator SetTimeData(InitialPlayerSync packet)
+    {
+        InitialTimeData timeData = packet.InitialTimeData;
+
+        NitroxServiceLocator.LocateService<TimeManager>().ProcessUpdate(timeData.TimePacket);
+
+        AuroraWarnings auroraWarnings = GameObject.FindObjectOfType<AuroraWarnings>();
+        auroraWarnings.timeSerialized = DayNightCycle.main.timePassedAsFloat;
+        auroraWarnings.OnProtoDeserialize(null);
+
+        CrashedShipExploder.main.version = 2;
+        CrashedShipExploder.main.timeToStartCountdown = timeData.CrashedShipExploderData.TimeToStartCountdown;
+        CrashedShipExploder.main.timeToStartWarning = timeData.CrashedShipExploderData.TimeToStartWarning;
+        CrashedShipExploder.main.OnProtoDeserialize(null);
+        yield break;
+    }
+
+    private IEnumerator SetRadioQueue(InitialPlayerSync packet)
+    {
+        List<string> radioQueue = packet.StoryGoalData.RadioQueue;
+
+        StoryGoalManager.main.pendingRadioMessages.AddRange(radioQueue);
+        StoryGoalManager.main.PulsePendingMessages();
+        Log.Info($"Radio queue: [{string.Join(", ", radioQueue.ToArray())}]");
+        yield break;
+    }
+
+    private IEnumerator SetCompletedStoryGoals(InitialPlayerSync packet)
+    {
+        List<string> storyGoalData = packet.StoryGoalData.CompletedGoals;
+        StoryGoalManager.main.completedGoals.Clear();
+        StoryGoalManager.main.completedGoals.AddRange(storyGoalData);
+
+        Log.Info($"Received initial sync packet with {storyGoalData.Count} completed story goals");
+        yield break;
+    }
+
+    private IEnumerator SetGoalUnlocks(InitialPlayerSync packet)
+    {
+        List<string> goalUnlocks = packet.StoryGoalData.GoalUnlocks;
+        foreach (string goalUnlock in goalUnlocks)
         {
-            SetCompletedStoryGoals(packet.StoryGoalData.CompletedGoals);
-            waitScreenItem.SetProgress(0.2f);
-            yield return null;
-
-            SetRadioQueue(packet.StoryGoalData.RadioQueue);
-            waitScreenItem.SetProgress(0.4f);
-            yield return null;
-
-            SetGoalUnlocks(packet.StoryGoalData.GoalUnlocks);
-            waitScreenItem.SetProgress(0.6f);
-            yield return null;
-
-            SetBiomeGoalTrackerGoals();
-            waitScreenItem.SetProgress(0.8f);
-            yield return null;
-
-            SetScheduledGoals(packet.StoryGoalData.ScheduledGoals);
-            waitScreenItem.SetProgress(1f);
-            yield return null;
+            StoryGoalManager.main.onGoalUnlockTracker.NotifyGoalComplete(goalUnlock);
         }
+        yield break;
+    }
 
-        private void SetRadioQueue(List<string> radioQueue)
+    private IEnumerator SetBiomeGoalTrackerGoals(InitialPlayerSync packet)
+    {
+        Dictionary<string, PDALog.Entry> entries = PDALog.entries;
+        List<BiomeGoal> goals = BiomeGoalTracker.main.goals;
+        int alreadyIn = 0;
+        for (int i = goals.Count - 1; i >= 0; i--)
         {
-            StoryGoalManager.main.pendingRadioMessages.AddRange(radioQueue);
-            StoryGoalManager.main.PulsePendingMessages();
-            Log.Info($"Radio queue: [{string.Join(", ", radioQueue.ToArray())}]");
-        }
-
-        private void SetCompletedStoryGoals(List<string> storyGoalData)
-        {
-            StoryGoalManager.main.completedGoals.Clear();
-            StoryGoalManager.main.completedGoals.AddRange(storyGoalData);
-
-            Log.Info($"Received initial sync packet with {storyGoalData.Count} completed story goals");
-        }
-
-        private void SetGoalUnlocks(List<string> goalUnlocks)
-        {
-            foreach (string goalUnlock in goalUnlocks)
+            if (entries.ContainsKey(goals[i].key))
             {
-                StoryGoalManager.main.onGoalUnlockTracker.NotifyGoalComplete(goalUnlock);
+                goals.Remove(goals[i]);
+                alreadyIn++;
+            }
+        }
+        Log.Debug($"{alreadyIn} pda log entries were removed from the goals");
+        yield break;
+    }
+
+    private IEnumerator SetScheduledGoals(InitialPlayerSync packet)
+    {
+        List<NitroxScheduledGoal> scheduledGoals = packet.StoryGoalData.ScheduledGoals;
+
+        Dictionary<string, PDALog.Entry> entries = PDALog.entries;
+        // Need to clear some duplicated goals that might have appeared during loading and before sync
+        for (int i = StoryGoalScheduler.main.schedule.Count - 1; i >= 0; i--)
+        {
+            ScheduledGoal scheduledGoal = StoryGoalScheduler.main.schedule[i];
+            if (entries.ContainsKey(scheduledGoal.goalKey))
+            {
+                StoryGoalScheduler.main.schedule.Remove(scheduledGoal);
             }
         }
 
-        private void SetBiomeGoalTrackerGoals()
+        foreach (NitroxScheduledGoal scheduledGoal in scheduledGoals)
         {
-            Dictionary<string, PDALog.Entry> entries = PDALog.entries;
-            List<BiomeGoal> goals = BiomeGoalTracker.main.goals;
-            int alreadyIn = 0;
-            for (int i = goals.Count - 1; i >= 0; i--)
+            ScheduledGoal goal = new ScheduledGoal();
+            goal.goalKey = scheduledGoal.GoalKey;
+            goal.goalType = (Story.GoalType)System.Enum.Parse(typeof(Story.GoalType), scheduledGoal.GoalType);
+            goal.timeExecute = scheduledGoal.TimeExecute;
+            if (goal.timeExecute >= DayNightCycle.main.timePassedAsDouble
+                && !StoryGoalScheduler.main.schedule.Any(alreadyInGoal => alreadyInGoal.goalKey == goal.goalKey)
+                && !entries.TryGetValue(goal.goalKey, out PDALog.Entry value)
+                && !StoryGoalManager.main.completedGoals.Contains(goal.goalKey))
             {
-                if (entries.ContainsKey(goals[i].key))
-                {
-                    goals.Remove(goals[i]);
-                    alreadyIn++;
-                }
+                StoryGoalScheduler.main.schedule.Add(goal);
             }
-            Log.Debug($"{alreadyIn} pda log entries were removed from the goals");
         }
 
-        private void SetScheduledGoals(List<NitroxScheduledGoal> scheduledGoals)
-        {
-            Dictionary<string, PDALog.Entry> entries = PDALog.entries;
-            // Need to clear some duplicated goals that might have appeared during loading and before sync
-            for (int i = StoryGoalScheduler.main.schedule.Count - 1; i >= 0; i--)
-            {
-                ScheduledGoal scheduledGoal = StoryGoalScheduler.main.schedule[i];
-                if (entries.ContainsKey(scheduledGoal.goalKey))
-                {
-                    StoryGoalScheduler.main.schedule.Remove(scheduledGoal);
-                }
-            }
-
-            foreach (NitroxScheduledGoal scheduledGoal in scheduledGoals)
-            {
-                ScheduledGoal goal = new ScheduledGoal();
-                goal.goalKey = scheduledGoal.GoalKey;
-                goal.goalType = (Story.GoalType)System.Enum.Parse(typeof(Story.GoalType), scheduledGoal.GoalType);
-                goal.timeExecute = scheduledGoal.TimeExecute;
-                if (goal.timeExecute >= DayNightCycle.main.timePassedAsDouble
-                    && !StoryGoalScheduler.main.schedule.Any(alreadyInGoal => alreadyInGoal.goalKey == goal.goalKey)
-                    && !entries.TryGetValue(goal.goalKey, out PDALog.Entry value)
-                    && !StoryGoalManager.main.completedGoals.Contains(goal.goalKey))
-                {
-                    StoryGoalScheduler.main.schedule.Add(goal);
-                }
-            }
-        }
+        yield break;
     }
 }
