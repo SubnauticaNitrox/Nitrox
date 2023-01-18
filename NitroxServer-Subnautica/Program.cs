7edd09a5c9a67a89b58fb18c3b302ef39c422c65
Measurity <measuring.infinity@gmail.com>
2023-01-18 02:12:58 +0100

    Centralized default culture info setter

--- NitroxServer-Subnautica/Program.cs
+++ NitroxServer-Subnautica/Program.cs
@@ -54,7 +54,7 @@ public class Program
         Log.Setup(true, isConsoleApp: true);
         AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
 
-        ConfigureCultureInfo();
+        CultureManager.ConfigureCultureInfo();
         Log.Info($"Starting NitroxServer {NitroxEnvironment.ReleasePhase} v{NitroxEnvironment.Version} for Subnautica");
 
         AppMutex.Hold(() => { Log.Info("Waiting on other Nitrox servers to initialize before starting.."); }, 120000);
@@ -231,29 +231,6 @@ public class Program
         // Read assemblies as bytes as to not lock the file so that Nitrox can patch assemblies while server is running.
         Assembly assembly = Assembly.Load(File.ReadAllBytes(dllPath));
         return resolvedAssemblyCache[dllFileName] = assembly;
-    }
-
-    /**
-     * Internal subnautica files are setup using US english number formats and dates.  To ensure
-     * that we parse all of these appropriately, we will set the default cultureInfo to en-US.
-     * This must best done for any thread that is spun up and needs to read from files (unless
-     * we were to migrate to 4.5.)  Failure to set the context can result in very strange behaviour
-     * throughout the entire application.  This originally manifested itself as a duplicate spawning
-     * issue for players in Europe.  This was due to incorrect parsing of probability tables.
-     */
-    private static void ConfigureCultureInfo()
-    {
-        CultureInfo cultureInfo = new("en-US");
-
-        // Although we loaded the en-US cultureInfo, let's make sure to set these incase the
-        // default was overriden by the user.
-        cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
-        cultureInfo.NumberFormat.NumberGroupSeparator = ",";
-
-        Thread.CurrentThread.CurrentCulture = cultureInfo;
-        Thread.CurrentThread.CurrentUICulture = cultureInfo;
-        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-        CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
     }
 
     private static void CatchExitEvent()
