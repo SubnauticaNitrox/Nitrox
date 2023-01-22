959dc2f21bc67a464a8deaaa14be048a45046323
Measurity <measuring.infinity@gmail.com>
2023-01-22 22:43:58 +0100

    Fixed regression of missing hyperlink in ServersView
    Regression caused by incompatibility with Avalonia Markdown nuget package. This package causes exception during rendering.

--- Nitrox.Launcher/Views/ServersView.axaml.cs
+++ Nitrox.Launcher/Views/ServersView.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Nitrox.Launcher.ViewModels;
 using Nitrox.Launcher.Views.Abstract;
@@ -14,5 +16,10 @@ public partial class ServersView : RoutableViewBase<ServersViewModel>
 
     protected override void RegisterDispose(Action<IDisposable> disposables)
     {
+    }
+
+    private void NitroxWikiTextBlock_OnPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo("https://nitrox.rux.gg/wiki/article/run-and-host-nitrox-subnautica-server") { UseShellExecute = true })?.Dispose();
     }
 }
