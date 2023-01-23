d1fad913a34c637c077dbf5061394b4d98fa28b0
Measurity <measuring.infinity@gmail.com>
2023-01-24 00:30:24 +0100

    Fixed case where ManageServerView was not navigated again after server instance was set before

--- Nitrox.Launcher/ViewModels/ManageServerViewModel.cs
+++ Nitrox.Launcher/ViewModels/ManageServerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Nitrox.Launcher.Models;
 using Nitrox.Launcher.ViewModels.Abstract;
+using Nitrox.Launcher.Views;
 using ReactiveUI;
 
 namespace Nitrox.Launcher.ViewModels;
@@ -8,10 +9,18 @@ public class ManageServerViewModel : RoutableViewModelBase
 {
     private ServerEntry server;
 
+    /// <summary>
+    ///     When set, navigates to the <see cref="ManageServerView" />.
+    /// </summary>
     public ServerEntry Server
     {
         get => server;
-        set => this.RaiseAndSetIfChanged(ref server, value);
+        set
+        {
+            this.RaisePropertyChanging();
+            server = value;
+            this.RaisePropertyChanged();
+        }
     }
 
     public ManageServerViewModel(IScreen hostScreen) : base(hostScreen)
