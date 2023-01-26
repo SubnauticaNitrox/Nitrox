8115e6fb4af77d34ac3cd7763d82c55a9c7ea1f2
Measurity <measuring.infinity@gmail.com>
2023-01-26 20:59:53 +0100

    Changed IPacketSender to use file scoped namespace

--- NitroxClient/Communication/Abstract/IPacketSender.cs
+++ NitroxClient/Communication/Abstract/IPacketSender.cs
@@ -1,18 +1,17 @@
 ﻿using NitroxModel.Packets;
 
-namespace NitroxClient.Communication.Abstract
-{
-    public interface IPacketSender
-    {
-        /// <summary>
-        ///     Sends the packet.
-        /// </summary>
-        void Send<T>(T packet) where T : Packet;
+namespace NitroxClient.Communication.Abstract;
 
-        /// <summary>
-        ///     Sends the packet if the call stack originated from UWE code.
-        ///     If the game code was called by Nitrox, triggering a packet send in injected code, no packet is send.
-        /// </summary>
-        bool SendIfGameCode<T>(T packet) where T : Packet;
-    }
+public interface IPacketSender
+{
+    /// <summary>
+    ///     Sends the packet.
+    /// </summary>
+    void Send<T>(T packet) where T : Packet;
+
+    /// <summary>
+    ///     Sends the packet if the call stack originated from UWE code.
+    ///     If the game code was called by Nitrox, triggering a packet send in injected code, no packet is send.
+    /// </summary>
+    bool SendIfGameCode<T>(T packet) where T : Packet;
 }
