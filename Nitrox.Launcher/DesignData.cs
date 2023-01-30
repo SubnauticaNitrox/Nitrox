f58312a308451b4947543ea0331a4c97b7d9c81b
Measurity <measuring.infinity@gmail.com>
2023-01-30 02:31:06 +0100

    Moved color codes into a palette.xaml

---
+++ Nitrox.Launcher/DesignData.cs
@@ -0,0 +1,14 @@
+using Nitrox.Launcher.Models;
+using Nitrox.Launcher.ViewModels;
+
+namespace Nitrox.Launcher;
+
+/// <summary>
+///     Design-time data for use with the XAML previewer plugin.
+/// </summary>
+public static class DesignData
+{
+    public static MainWindowViewModel MainWindowViewModel { get; } = new();
+    public static CreateServerViewModel CreateServerViewModel { get; } = new() { Name = "My Server Name", SelectedGameMode = GameMode.CREATIVE };
+    public static ManageServerViewModel ManageServerViewModel { get; } = new(null) { Server = new ServerEntry { Name = "My fun server" } };
+}
