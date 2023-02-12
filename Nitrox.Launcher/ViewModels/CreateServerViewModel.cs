a2e2c604794ed9a33bb9ab8f06624b08efad8256
Measurity <measuring.infinity@gmail.com>
2023-02-12 23:22:32 +0100

    Added valid file name validation rule to server name

--- Nitrox.Launcher/ViewModels/CreateServerViewModel.cs
+++ Nitrox.Launcher/ViewModels/CreateServerViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Reactive;
+﻿using System.IO;
+using System.Linq;
+using System.Reactive;
 using Avalonia.Input;
 using Nitrox.Launcher.Models;
 using Nitrox.Launcher.ViewModels.Abstract;
@@ -9,6 +11,8 @@ namespace Nitrox.Launcher.ViewModels;
 
 public class CreateServerViewModel : ModalViewModelBase
 {
+    private static readonly char[] invalidPathCharacters = Path.GetInvalidFileNameChars();
+
     private string name = "";
     private GameMode selectedGameMode = GameMode.SURVIVAL;
     public ReactiveCommand<Unit, CreateServerViewModel> CreateServerCommand { get; }
@@ -17,7 +21,7 @@ public class CreateServerViewModel : ModalViewModelBase
     public string Name
     {
         get => name;
-        set => this.RaiseAndSetIfChanged(ref name, value.Trim());
+        set => this.RaiseAndSetIfChanged(ref name, value);
     }
 
     public KeyGesture BackHotkey { get; } = new(Key.Escape);
@@ -33,7 +37,8 @@ public class CreateServerViewModel : ModalViewModelBase
     {
         BackCommand = ReactiveCommand.Create(() => (CreateServerViewModel)null!);
         CreateServerCommand = ReactiveCommand.Create(() => this, this.IsValid());
-        
-        this.ValidationRule(vm => vm.Name, value => !string.IsNullOrWhiteSpace(value), $"{nameof(Name)} shouldn't be empty.");
+
+        this.ValidationRule(vm => vm.Name, s => !string.IsNullOrWhiteSpace(s), $"{nameof(Name)} shouldn't be empty.");
+        this.ValidationRule(vm => vm.Name, s => s.All(c => !invalidPathCharacters.Contains(c)), $"{nameof(Name)} shouldn't contain invalid characters.");
     }
 }
