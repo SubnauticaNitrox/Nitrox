b9106660c3eab84cc871daa89f9f231e3a3b9c20
Measurity <measuring.infinity@gmail.com>
2023-02-17 23:07:51 +0100

    Cleanup up ThrottledPacketSender API

--- NitroxClient/Helpers/ThrottledPacketSender.cs
+++ NitroxClient/Helpers/ThrottledPacketSender.cs
@@ -3,81 +3,80 @@ using System.Collections.Generic;
 using NitroxClient.Communication.Abstract;
 using NitroxModel.Packets;
 
-namespace NitroxClient.Helpers
+namespace NitroxClient.Helpers;
+
+/// <summary>
+///     Throttles packets by deduplicating them by packet type and sending the most up-to-date packet in an <see cref="Update" /> cycle.
+/// </summary>
+public class ThrottledPacketSender
 {
-    public class ThrottledPacketSender
+    private readonly IPacketSender packetSender;
+    private readonly Dictionary<object, ThrottledPacket> throttlePerPacketType = new();
+
+    public ThrottledPacketSender(IPacketSender packetSender)
     {
-        private readonly Dictionary<object, ThrottledPacket> throttledPackets = new();
-        private readonly IPacketSender packetSender;
+        this.packetSender = packetSender;
+    }
 
-        public ThrottledPacketSender(IPacketSender packetSender)
+    public void Update()
+    {
+        foreach (ThrottledPacket throttledPacket in throttlePerPacketType.Values)
         {
-            this.packetSender = packetSender;
-        }
-
-        public void Update()
-        {
-            foreach (ThrottledPacket throttledPacket in throttledPackets.Values)
+            if (throttledPacket.WasSend || throttledPacket.SendTime > DateTime.UtcNow)
             {
-                if (throttledPacket.WasSend || throttledPacket.SendTime > DateTime.UtcNow)
-                {
-                    continue;
-                }
+                continue;
+            }
 
-                if (packetSender.SendIfGameCode(throttledPacket.Packet))
-                {
-                    throttledPacket.WasSend = true;
-                }
+            if (!packetSender.IsPacketSuppressed(throttledPacket.Packet.GetType()))
+            {
+                packetSender.Send(throttledPacket.Packet);
+                throttledPacket.WasSend = true;
             }
         }
+    }
 
-        public bool SendThrottled(Packet packet, Func<Packet, object> dedupeMethod, float throttleTime = 0.2f)
+    /// <summary>
+    ///     Queues a packet for sending in the next <see cref="Update" /> cycle. If packet is currently suppressed, it won't be queued.
+    /// </summary>
+    public void SendThrottled(Packet packet, Func<Packet, object> dedupeMethod, float throttleTime = 0.2f)
+    {
+        Type packetType = packet.GetType();
+        if (packetSender.IsPacketSuppressed(packetType))
         {
-            Type packetType = packet.GetType();
-
-            if (packetSender.IsPacketSuppressed(packetType))
-            {
-                return false;
-            }
-
-            object dedupeKey = dedupeMethod.Invoke(packet);
-
-            if (throttledPackets.TryGetValue(dedupeKey, out ThrottledPacket throttledPacket))
-            {
-                throttledPacket.ReplacePacket(packet, throttleTime);
-                return true;
-            }
-
-            if (!packetSender.SendIfGameCode(packet))
-            {
-                return false;
-            }
-
-            throttledPackets.Add(dedupeKey, new ThrottledPacket(packet, throttleTime));
-            return true;
+            return;
         }
 
-        private class ThrottledPacket
+        object dedupeKey = dedupeMethod(packet);
+        if (!throttlePerPacketType.TryGetValue(dedupeKey, out ThrottledPacket throttledPacket))
         {
-            public DateTime SendTime { get; private set; }
-            public Packet Packet { get; private set; }
-            public bool WasSend { get; set; }
+            throttlePerPacketType.Add(dedupeKey, new ThrottledPacket(packet, throttleTime));
+        }
+        else
+        {
+            throttledPacket.ReplacePacket(packet, throttleTime);
+        }
+    }
 
-            public ThrottledPacket(Packet packet, float throttleTime)
+    private class ThrottledPacket
+    {
+        public DateTime SendTime { get; private set; }
+        public Packet Packet { get; private set; }
+        public bool WasSend { get; set; }
+
+        public ThrottledPacket(Packet packet, float throttleTime)
+        {
+            SendTime = DateTime.UtcNow.AddSeconds(throttleTime);
+            Packet = packet;
+        }
+
+        public void ReplacePacket(Packet packet, float throttleTime)
+        {
+            Packet = packet;
+
+            if (WasSend)
             {
                 SendTime = DateTime.UtcNow.AddSeconds(throttleTime);
-                Packet = packet;
-            }
-
-            public void ReplacePacket(Packet packet, float throttleTime)
-            {
-                Packet = packet;
-
-                if (WasSend)
-                {
-                    SendTime = DateTime.UtcNow.AddSeconds(throttleTime);
-                    WasSend = false;
-                }
+                WasSend = false;
             }
         }
     }
