7c3c4c51154d81dc3385ec83b655dcef3a4f47c4
Sunrunner37 <sunrunner@nitroxmod.com>
2023-03-14 19:40:39 -0700

    Fixed a bug causing the player entity to never receive an update position when the player moves.  This makes some calculations incorrect later (such as isSwimming check).

--- NitroxServer/Communication/Packets/Processors/PlayerMovementProcessor.cs
+++ NitroxServer/Communication/Packets/Processors/PlayerMovementProcessor.cs
@@ -1,20 +1,34 @@
-﻿using NitroxModel.Packets;
+using NitroxModel.DataStructures.GameLogic;
+using NitroxModel.DataStructures.GameLogic.Entities;
+using NitroxModel.DataStructures.Util;
+using NitroxModel.Packets;
 using NitroxServer.Communication.Packets.Processors.Abstract;
 using NitroxServer.GameLogic;
+using NitroxServer.GameLogic.Entities;
 
 namespace NitroxServer.Communication.Packets.Processors
 {
     class PlayerMovementProcessor : AuthenticatedPacketProcessor<PlayerMovement>
     {
         private readonly PlayerManager playerManager;
+        private readonly EntityRegistry entityRegistry;
 
-        public PlayerMovementProcessor(PlayerManager playerManager)
+        public PlayerMovementProcessor(PlayerManager playerManager, EntityRegistry entityRegistry)
         {
             this.playerManager = playerManager;
+            this.entityRegistry = entityRegistry;
         }
 
         public override void Process(PlayerMovement packet, Player player)
         {
+            Optional<PlayerWorldEntity> playerEntity = entityRegistry.GetEntityById<PlayerWorldEntity>(player.PlayerContext.PlayerNitroxId);
+
+            if (playerEntity.HasValue)
+            {
+                playerEntity.Value.Transform.Position = packet.Position;
+                playerEntity.Value.Transform.Rotation = packet.BodyRotation;
+            }
+
             player.Position = packet.Position;
             player.Rotation = packet.BodyRotation;
             playerManager.SendPacketToOtherPlayers(packet, player);
