2099fd1046c7494535d48c20a97331dc1c878361
Sunrunner37 <dave@balancedcms.com>
2023-03-17 16:44:08 -0700

    Added entity deletion accounting at several locations. (#2002)

---
+++ NitroxPatcher/Patches/Dynamic/Survival_Use_Patch.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using HarmonyLib;
+using NitroxClient.Communication.Abstract;
+using NitroxClient.MonoBehaviours;
+using NitroxModel.DataStructures;
+using NitroxModel.Helper;
+using NitroxModel.Packets;
+using UnityEngine;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+/// <summary>
+/// Let the server know when the player successfully uses a consumable item (such as a first aid kit).
+/// </summary>
+public class Survival_Use_Patch : NitroxPatch, IDynamicPatch
+{
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((Survival t) => t.Use(default(GameObject)));
+
+    public static void Postfix(bool __result, GameObject useObj)
+    {
+        if (__result && useObj)
+        {
+            NitroxId id = NitroxEntity.GetId(useObj);
+            Resolve<IPacketSender>().Send(new EntityDestroyed(id));
+        }
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchPostfix(harmony, TARGET_METHOD);
+    }
+}
