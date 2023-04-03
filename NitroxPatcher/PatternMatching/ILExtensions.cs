81553d8c93aeca266497629b02a0a3f2f11c10c1
Measurity <measuring.infinity@gmail.com>
2023-04-03 22:42:03 +0200

    Added docs to ILExtensions.Transform() functions

--- NitroxPatcher/PatternMatching/ILExtensions.cs
+++ NitroxPatcher/PatternMatching/ILExtensions.cs
@@ -12,11 +12,18 @@ internal static class ILExtensions
         return string.Join(Environment.NewLine, instructions.Select(i => i.ToString()));
     }
 
+    /// <summary>
+    ///     Iterates the instructions, searching for the given pattern. When the pattern matches, the transform function is
+    ///     called. If the pattern does not match the expected match count <see cref="InstructionsPattern.expectedMatches" />,
+    ///     an exception is thrown.
+    /// </summary>
     public static IEnumerable<CodeInstruction> Transform(this IEnumerable<CodeInstruction> instructions, InstructionsPattern pattern, Func<string, CodeInstruction, IEnumerable<CodeInstruction>> transform)
     {
         return pattern.ApplyTransform(instructions, transform);
     }
-    
+
+    /// <inheritdoc
+    ///     cref="Transform(System.Collections.Generic.IEnumerable{HarmonyLib.CodeInstruction},NitroxPatcher.PatternMatching.InstructionsPattern,System.Func{string,HarmonyLib.CodeInstruction,System.Collections.Generic.IEnumerable{HarmonyLib.CodeInstruction}})" />
     public static IEnumerable<CodeInstruction> Transform(this IEnumerable<CodeInstruction> instructions, InstructionsPattern pattern, Action<string, CodeInstruction> transform)
     {
         return pattern.ApplyTransform(instructions, (label, instruction) =>
