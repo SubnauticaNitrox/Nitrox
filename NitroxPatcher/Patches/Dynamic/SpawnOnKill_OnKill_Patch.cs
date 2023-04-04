00d6c2b59b466c78c810a85d8ddcc0b81d8006ad
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-04-04 23:30:36 +0200

    Updated SpawnOnKill from a Prefix to a Transpiler. It's optimized 👌

--- NitroxPatcher/Patches/Dynamic/SpawnOnKill_OnKill_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/SpawnOnKill_OnKill_Patch.cs
@@ -5,8 +5,12 @@ using NitroxClient.MonoBehaviours;
 using NitroxModel.DataStructures;
 using NitroxModel.Helper;
 using NitroxModel.Packets;
+using NitroxPatcher.PatternMatching;
+using Oculus.Platform;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using static System.Reflection.Emit.OpCodes;
 
 namespace NitroxPatcher.Patches.Dynamic;
 
@@ -17,6 +21,45 @@ public class SpawnOnKill_OnKill_Patch : NitroxPatch, IDynamicPatch
 {
     private static readonly MethodInfo TARGET_METHOD = Reflect.Method((SpawnOnKill t) => t.OnKill());
 
+    private static readonly InstructionsPattern OnKillPattern = new()
+    {
+        { Stloc_0, "CallbackInsertion" },
+        Ldarg_0,
+    };
+
+    public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions) => instructions
+    .Transform(OnKillPattern, (label, instruction) =>
+    {
+        switch (label)
+        {
+            case "CallbackInsertion":
+                return new List<CodeInstruction>
+                {
+                    new CodeInstruction(Ldarg_0),
+                    new CodeInstruction(Ldloc_0),
+                    new CodeInstruction(Call, Reflect.Method(() => Callback(default, default))),
+                };
+        }
+        return null;
+    });
+
+    public static void Callback(SpawnOnKill __this, GameObject gameObject)
+    {
+        // get the ID of the destroyed object
+        NitroxId destroyedEntityId = NitroxEntity.GetId(__this.gameObject);
+        // Says that the entity doesn't exist anymore  --  Without this, the gameobject is not updated. It will not be pickuppable after that.
+        Resolve<IPacketSender>().Send(new EntityDestroyed(destroyedEntityId));
+        // Give an ID to the new game object
+        NitroxEntity.SetNewId(gameObject, destroyedEntityId);
+        // Drop the item in the game
+        Resolve<Items>().Dropped(gameObject, CraftData.GetTechType(gameObject));
+    }
+
+    /// <summary>
+    /// DEPRECATED
+    /// </summary>
+    /// <param name="__instance"></param>
+    /// <returns></returns>
     public static bool Prefix(SpawnOnKill __instance)
     {
         // ORIGINAL CODE
@@ -45,6 +88,6 @@ public class SpawnOnKill_OnKill_Patch : NitroxPatch, IDynamicPatch
 
     public override void Patch(Harmony harmony)
     {
-        PatchPrefix(harmony, TARGET_METHOD);
+        PatchTranspiler(harmony, TARGET_METHOD);
     }
 }
