d97ee580b28d499ff226b52e5d4cf4361d1abd33
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-04-04 23:32:53 +0200

    Removed the prefix code

--- NitroxPatcher/Patches/Dynamic/SpawnOnKill_OnKill_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/SpawnOnKill_OnKill_Patch.cs
@@ -55,37 +55,6 @@ public class SpawnOnKill_OnKill_Patch : NitroxPatch, IDynamicPatch
         Resolve<Items>().Dropped(gameObject, CraftData.GetTechType(gameObject));
     }
 
-    /// <summary>
-    /// DEPRECATED
-    /// </summary>
-    /// <param name="__instance"></param>
-    /// <returns></returns>
-    public static bool Prefix(SpawnOnKill __instance)
-    {
-        // ORIGINAL CODE
-        GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(__instance.prefabToSpawn, __instance.transform.position, __instance.transform.rotation);
-        if (__instance.randomPush)
-        {
-            Rigidbody component = gameObject.GetComponent<Rigidbody>();
-            if (component)
-            {
-                component.AddForce(UnityEngine.Random.onUnitSphere * 1.4f, ForceMode.Impulse);
-            }
-        }
-
-        // get the ID of the destroyed object
-        NitroxId destroyedEntityId = NitroxEntity.GetId(__instance.gameObject);
-        // Says that the entity doesn't exist anymore  --  Without this, the gameobject is not updated. It will not be pickuppable after that.
-        Resolve<IPacketSender>().Send(new EntityDestroyed(destroyedEntityId));
-        // Give an ID to the new game object
-        NitroxEntity.SetNewId(gameObject, destroyedEntityId);
-        // Drop the item in the game
-        Resolve<Items>().Dropped(gameObject, CraftData.GetTechType(gameObject));
-
-        // Prevent the original function to happen twice, so avoid duplication of the object on client side
-        return false;
-    }
-
     public override void Patch(Harmony harmony)
     {
         PatchTranspiler(harmony, TARGET_METHOD);
