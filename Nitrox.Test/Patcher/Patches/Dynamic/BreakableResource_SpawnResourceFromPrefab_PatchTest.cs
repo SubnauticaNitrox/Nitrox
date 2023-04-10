51ea86ace157441c414d964b82e6b87009892030
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-04-10 15:23:30 +0200

    corrected test class (offset was 3 instead of 2)

--- Nitrox.Test/Patcher/Patches/Dynamic/BreakableResource_SpawnResourceFromPrefab_PatchTest.cs
+++ Nitrox.Test/Patcher/Patches/Dynamic/BreakableResource_SpawnResourceFromPrefab_PatchTest.cs
@@ -17,6 +17,6 @@ public class BreakableResource_SpawnResourceFromPrefab_PatchTest
     {
         ReadOnlyCollection<CodeInstruction> originalIL = PatchTestHelper.GetInstructionsFromMethod(TARGET_METHOD);
         IEnumerable<CodeInstruction> transformedIL = Transpiler(TARGET_METHOD, originalIL);
-        originalIL.Count.Should().Be(transformedIL.Count() - 3);
+        originalIL.Count.Should().Be(transformedIL.Count() - 2);
     }
 }
