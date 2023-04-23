c8f1c014c1e5333b901e517d816e54fa75d94494
Jannify <23176718+Jannify@users.noreply.github.com>
2023-04-23 13:28:40 +0200

    Use GenerateNewId instead of SetNewId

--- NitroxPatcher/Patches/Dynamic/IncubatorEgg_HatchNow_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/IncubatorEgg_HatchNow_Patch.cs
@@ -46,8 +46,7 @@ namespace NitroxPatcher.Patches.Dynamic
                 baby.transform.position = egg.attachPoint.transform.position;
                 baby.transform.localRotation = Quaternion.identity;
 
-                NitroxId babyId = new NitroxId();
-                NitroxEntity.SetNewId(baby, babyId);
+                NitroxId babyId = NitroxEntity.GenerateNewId(baby);
 
                 WorldEntity entity = new WorldEntity(baby.transform.position.ToDto(), baby.transform.rotation.ToDto(), baby.transform.localScale.ToDto(), TechType.SeaEmperorBaby.ToDto(), 3, "09883a6c-9e78-4bbf-9561-9fa6e49ce766", false, babyId, null, false, null);
                 NitroxServiceLocator.LocateService<Entities>().BroadcastEntitySpawnedByClient(entity);
