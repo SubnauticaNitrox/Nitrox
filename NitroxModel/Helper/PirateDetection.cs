8fe93ac5b9f75a13f4b173bf857c9ae34c0554d8
piwdipiy <48891836+piwdipiy@users.noreply.github.com>
2023-04-24 13:51:05 +0300

    pirate support

--- NitroxModel/Helper/PirateDetection.cs
+++ NitroxModel/Helper/PirateDetection.cs
@@ -42,18 +42,6 @@ namespace NitroxModel.Helper
 
         private static bool IsPirateByDirectory(string subnauticaRoot)
         {
-            string subdirDll = Path.Combine(subnauticaRoot, "Subnautica_Data", "Plugins", "x86_64", "steam_api64.dll");
-            if (File.Exists(subdirDll) && !FileSystem.Instance.IsTrustedFile(subdirDll))
-            {
-                return true;
-            }
-            // Dlls might be in root if cracked game (to override DLLs in sub directories).
-            string rootDll = Path.Combine(subnauticaRoot, "steam_api64.dll");
-            if (File.Exists(rootDll) && !FileSystem.Instance.IsTrustedFile(rootDll))
-            {
-                return true;
-            }
-
             return false;
         }
 
