a80bb34e2d46b3f77f7efd468edc24ee18764908
Measurity <measuring.infinity@gmail.com>
2023-05-06 00:14:25 +0200

    Removed config IO in GameInput_SetupDefaultKeyboardBindings_Patch
    The key bindings are already known in memory, no need to read client config file.

--- NitroxPatcher/Patches/Persistent/GameInput_SetupDefaultKeyboardBindings_Patch.cs
+++ NitroxPatcher/Patches/Persistent/GameInput_SetupDefaultKeyboardBindings_Patch.cs
@@ -2,7 +2,6 @@
 using HarmonyLib;
 using NitroxClient.MonoBehaviours.Gui.Input;
 using NitroxClient.MonoBehaviours.Gui.Input.KeyBindings;
-using NitroxClient.Serialization;
 using NitroxModel.Helper;
 
 namespace NitroxPatcher.Patches.Persistent;
@@ -13,21 +12,15 @@ public class GameInput_SetupDefaultKeyboardBindings_Patch : NitroxPatch, IPersis
 
     public static void Postfix()
     {
-        ClientConfig cfg = ClientConfig.Init();
         KeyBindingManager keyBindingManager = new();
         foreach (KeyBinding keyBinding in keyBindingManager.KeyboardKeyBindings)
         {
             GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Primary, keyBinding.PrimaryKey);
 
             // if the options.bin gets deleted (when loading SP) and the client.cfg has secondary keybinds, repopulate them.
-            switch ((KeyBindingValues)keyBinding.Button) 
+            if (!string.IsNullOrEmpty(keyBinding.SecondaryKey))
             {
-                case KeyBindingValues.CHAT:
-                    GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, cfg.OpenChatKeybindSecondary);
-                    break;
-                case KeyBindingValues.FOCUS_DISCORD:
-                    GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, cfg.FocusDiscordKeybindSecondary);
-                    break;
+                GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, keyBinding.SecondaryKey);
             }
         }
     }
