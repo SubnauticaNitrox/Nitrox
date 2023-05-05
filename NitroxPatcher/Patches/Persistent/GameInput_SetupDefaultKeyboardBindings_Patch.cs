b6360d1e3ad71cffb4acb1a3119b0c6a25061de2
MistaOmega <jack.nash6@gmail.com>
2023-05-05 14:45:19 +0100

    Converted from block-scoped to file-scoped namespace

--- NitroxPatcher/Patches/Persistent/GameInput_SetupDefaultKeyboardBindings_Patch.cs
+++ NitroxPatcher/Patches/Persistent/GameInput_SetupDefaultKeyboardBindings_Patch.cs
@@ -5,35 +5,34 @@ using NitroxClient.MonoBehaviours.Gui.Input.KeyBindings;
 using NitroxClient.Serialization;
 using NitroxModel.Helper;
 
-namespace NitroxPatcher.Patches.Persistent
-{
-    public class GameInput_SetupDefaultKeyboardBindings_Patch : NitroxPatch, IPersistentPatch
-    {
-        private static readonly MethodInfo TARGET_METHOD = Reflect.Method(() => GameInput.SetupDefaultKeyboardBindings());
+namespace NitroxPatcher.Patches.Persistent;
 
-        public static void Postfix()
+public class GameInput_SetupDefaultKeyboardBindings_Patch : NitroxPatch, IPersistentPatch
+{
+    private static readonly MethodInfo TARGET_METHOD = Reflect.Method(() => GameInput.SetupDefaultKeyboardBindings());
+
+    public static void Postfix()
+    {
+        ClientConfig cfg = ClientConfig.InitClientConfig();
+        //ClientConfig cfg = ClientConfig.Load(ClientConfigSettings.NitroxRoamingDir);
+        KeyBindingManager keyBindingManager = new();
+        foreach (KeyBinding keyBinding in keyBindingManager.KeyboardKeyBindings)
         {
-            ClientConfig cfg = ClientConfig.InitClientConfig();
-            //ClientConfig cfg = ClientConfig.Load(ClientConfigSettings.NitroxRoamingDir);
-            KeyBindingManager keyBindingManager = new();
-            foreach (KeyBinding keyBinding in keyBindingManager.KeyboardKeyBindings)
+            GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, keyBinding.DefaultKeyBinding.BindingSet, keyBinding.DefaultKeyBinding.Binding);
+            switch (keyBinding.HasSecondary) // if for some reason the options.bin gets deleted, such when loading sp, and the client.cfg has secondary keybinds, we need to repopulate them, this handles that.
             {
-                GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, keyBinding.DefaultKeyBinding.BindingSet, keyBinding.DefaultKeyBinding.Binding);
-                switch (keyBinding.HasSecondary) // if for some reason the options.bin gets deleted, such when loading sp, and the client.cfg has secondary keybinds, we need to repopulate them, this handles that.
-                {
-                    case true when keyBinding.Button == (GameInput.Button)KeyBindingValues.CHAT:
-                        GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, cfg.OpenChatKeybindSecondary);
-                        break;
-                    case true when keyBinding.Button == (GameInput.Button)KeyBindingValues.FOCUS_DISCORD:
-                        GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, cfg.FocusDiscordKeybindSecondary);
-                        break;
-                }
+                case true when keyBinding.Button == (GameInput.Button)KeyBindingValues.CHAT:
+                    GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, cfg.OpenChatKeybindSecondary);
+                    break;
+                case true when keyBinding.Button == (GameInput.Button)KeyBindingValues.FOCUS_DISCORD:
+                    GameInput.SetBindingInternal(keyBinding.Device, keyBinding.Button, GameInput.BindingSet.Secondary, cfg.FocusDiscordKeybindSecondary);
+                    break;
             }
         }
+    }
 
-        public override void Patch(Harmony harmony)
-        {
-            PatchPostfix(harmony, TARGET_METHOD);
-        }
+    public override void Patch(Harmony harmony)
+    {
+        PatchPostfix(harmony, TARGET_METHOD);
     }
 }
