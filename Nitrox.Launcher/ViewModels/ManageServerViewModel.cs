e959f41a81cb1782f2a2763d683f01cb8677632d
NinjaPedroX <pedro.gonzalez.nasa@gmail.com>
2023-06-15 00:25:16 -0400

    Continued work on the "ManageServerView" View
    Added server password input box and server gamemode radio buttons

--- Nitrox.Launcher/ViewModels/ManageServerViewModel.cs
+++ Nitrox.Launcher/ViewModels/ManageServerViewModel.cs
@@ -9,7 +9,6 @@ namespace Nitrox.Launcher.ViewModels;
 public class ManageServerViewModel : RoutableViewModelBase
 {
     private string serverName;
-
     /// <summary>
     ///     When set, navigates to the <see cref="ManageServerView" />.
     /// </summary>
@@ -18,7 +17,23 @@ public class ManageServerViewModel : RoutableViewModelBase
         get => serverName;
         set => this.RaiseAndSetIfChanged(ref serverName, value);
     }
+    
+    private string serverPassword;
+    /// <summary>
+    ///     When set, navigates to the <see cref="ManageServerView" />.
+    /// </summary>
+    public string ServerPassword
+    {
+        get => serverPassword;
+        set => this.RaiseAndSetIfChanged(ref serverPassword, value);
+    }
 
+    private GameMode serverGameMode;
+    public GameMode ServerGameMode
+    {
+        get => serverGameMode;
+        set => this.RaiseAndSetIfChanged(ref serverGameMode, value);
+    }
     public ReactiveCommand<Unit, IRoutableViewModel> BackCommand { get; init; }
 
     public ManageServerViewModel(IScreen hostScreen) : base(hostScreen)
@@ -30,5 +45,7 @@ public class ManageServerViewModel : RoutableViewModelBase
     public void LoadFrom(ServerEntry server)
     {
         ServerName = server.Name;
+        ServerPassword = ""; // Need to add password config
+        ServerGameMode = server.GameMode;
     }
 }
