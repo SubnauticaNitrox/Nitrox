afe566d2e897f16e42c8597af4d240d1213ecb11
NinjaPedroX <pedro.gonzalez.nasa@gmail.com>
2023-07-09 02:11:59 -0400

    Fixed textbox stretching and implemented "View Password" button for ServerPassword Textbox
    Fixed the width stretching of the textbox in the CreateServerView modal and the ManageServerView, in addition to implementing a scrollviewer for the server name for easier readability.
    
    Co-Authored-By: Meas <measuring.infinity@gmail.com>

--- Nitrox.Launcher/Views/ManageServerView.axaml.cs
+++ Nitrox.Launcher/Views/ManageServerView.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Input;
+﻿using Avalonia.Controls;
+using Avalonia.Input;
 using Nitrox.Launcher.ViewModels;
 using Nitrox.Launcher.Views.Abstract;
 
@@ -14,5 +15,20 @@ public partial class ManageServerView : RoutableViewBase<ManageServerViewModel>
     private void InputElement_OnKeyDown(object sender, KeyEventArgs e)
     {
         e.Handled = e.Key is < Key.D0 or > Key.D9;
+    }
+
+    private void ScrollViewer_OnPointerWheelChanged(object sender, PointerWheelEventArgs e)
+    {
+        // Change scroll wheel input to move scroll viewer left and right
+        ScrollViewer scrollViewer = (ScrollViewer)sender;
+        if (e.Delta.Y < 0)
+        {
+            scrollViewer.LineRight();
+        }
+        else
+        {
+            scrollViewer.LineLeft();
+        }
+        e.Handled = true;
     }
 }
