f07ce0895e9f217fd491c4f6c807f1d83b92f030
Measurity <measuring.infinity@gmail.com>
2023-12-31 07:59:59 +0100

    Added local FS fallback to BitmapAssetValueConverter

--- Nitrox.Launcher/Models/Converters/BitmapAssetValueConverter.cs
+++ Nitrox.Launcher/Models/Converters/BitmapAssetValueConverter.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using Avalonia.Data.Converters;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using Path = System.IO.Path;
 
 namespace Nitrox.Launcher.Models.Converters;
 
@@ -28,15 +31,38 @@ public class BitmapAssetValueConverter : Converter<BitmapAssetValueConverter>, I
             return bitmap;
         }
         Uri uri = rawUri.StartsWith("avares://") ? new Uri(rawUri) : new Uri($"avares://{assemblyName}{rawUri}");
-        if (!AssetLoader.Exists(uri))
+        if (!AssetLoader.Exists(uri) && !Avalonia.Controls.Design.IsDesignMode)
         {
             return null;
         }
+        // In design mode, resource aren't yet embedded.
+        if (Avalonia.Controls.Design.IsDesignMode)
+        {
+            bitmap = TryLoadFromLocalFileSystem(rawUri);
+        }
 
-        bitmap = new Bitmap(AssetLoader.Open(uri));
+        bitmap ??= new Bitmap(AssetLoader.Open(uri));
         assetCache.Add(rawUri, bitmap);
         return bitmap;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+
+    private Bitmap TryLoadFromLocalFileSystem(string fileUri)
+    {
+        string targetedProject = Path.GetDirectoryName(Environment.GetCommandLineArgs().FirstOrDefault(part => !part.Contains("Designer", StringComparison.Ordinal) && part.EndsWith("dll", StringComparison.OrdinalIgnoreCase) && File.Exists(part)));
+        while (targetedProject != null && !Directory.EnumerateFileSystemEntries(targetedProject, "*.csproj", SearchOption.TopDirectoryOnly).Any())
+        {
+            targetedProject = Path.GetDirectoryName(targetedProject);
+        }
+        if (targetedProject == null)
+        {
+            return null;
+        }
+        while (fileUri.StartsWith('/') | fileUri.StartsWith('\\'))
+        {
+            fileUri = fileUri.Substring(1);
+        }
+        return new(Path.Combine(targetedProject, fileUri));
+    }
 }
