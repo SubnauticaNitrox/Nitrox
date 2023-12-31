a842ae4b3f93475bd1316b9aac5e675fba05cc71
NinjaPedroX <pedro.gonzalez.nasa@gmail.com>
2023-12-31 02:03:36 -0600

    Started work on "OptionsView"

--- Nitrox.Launcher/ViewModels/OptionsViewModel.cs
+++ Nitrox.Launcher/ViewModels/OptionsViewModel.cs
@@ -1,11 +1,42 @@
-﻿using Nitrox.Launcher.ViewModels.Abstract;
+﻿using CommunityToolkit.Mvvm.Input;
+using Nitrox.Launcher.ViewModels.Abstract;
+using NitroxModel.Discovery;
+using NitroxModel.Helper;
 using ReactiveUI;
 
 namespace Nitrox.Launcher.ViewModels;
 
 public partial class OptionsViewModel : RoutableViewModelBase
 {
+    public Platform GamePlatform => NitroxUser.GamePlatform?.Platform ?? Platform.NONE;
+    public string PathToSubnautica => NitroxUser.GamePath;
+    public string SubnauticaLaunchArguments => "-vrmode none";//LauncherLogic.Config.SubnauticaLaunchArguments;
+    
     public OptionsViewModel(IScreen hostScreen) : base(hostScreen)
     {
+    }
+
+    [RelayCommand]
+    private void ChangePath()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void ResetArguments()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void ChangeArguments()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void ViewFolder()
+    {
+        
     }
 }
