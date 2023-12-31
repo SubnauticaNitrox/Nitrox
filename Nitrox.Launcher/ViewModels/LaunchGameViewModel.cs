f197a963983c67e25c98688d59f28de33a0f0499
NinjaPedroX <pedro.gonzalez.nasa@gmail.com>
2023-12-30 19:46:04 -0600

    Added Dynamic Gallery image loading and scrolling (WIP)
    Also cleaned up the code a bit and added two more images to the image gallery.
    
    There are issues with loading the image dynamically, as shown with comments in the code.

--- Nitrox.Launcher/ViewModels/LaunchGameViewModel.cs
+++ Nitrox.Launcher/ViewModels/LaunchGameViewModel.cs
@@ -1,13 +1,40 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using Avalonia.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Nitrox.Launcher.ViewModels.Abstract;
+using NitroxModel;
+using NitroxModel.Discovery;
+using NitroxModel.Helper;
 using ReactiveUI;
 
 namespace Nitrox.Launcher.ViewModels;
 
 public partial class LaunchGameViewModel : RoutableViewModelBase
 {
+    public string PlatformToolTip => GamePlatform.GetAttribute<DescriptionAttribute>()?.Description ?? "Unknown";
+    public Platform GamePlatform => NitroxUser.GamePlatform?.Platform ?? Platform.NONE;
+    public string Version => $"INDEV 1.8.0.0"; //$"{LauncherLogic.ReleasePhase} {LauncherLogic.Version}";
+    
+    [ObservableProperty]
+    private AvaloniaList<string> galleryImageSources = new();
+    
     public LaunchGameViewModel(IScreen hostScreen) : base(hostScreen)
     {
+        // Dynamically get the image names contained within the project's "gallery-images" folder
+        foreach (string imageName in Directory.GetFiles($@"{Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))}\Assets\Images\gallery-images"))
+        {
+            GalleryImageSources.Add(imageName.Replace(@"D:\Code\Nitrox\Nitrox.Launcher", "")); // HARDCODED VALUE BASED ON MY LOCAL PATH - Needs to be fixed
+        }
+
+        // TEMP
+        foreach (string value in GalleryImageSources)
+        {
+            Console.WriteLine($"Adding image source \"{value}\"");
+        }
     }
 
     [RelayCommand]
--- Nitrox.Launcher/Views/LaunchGameView.axaml.cs
+++ Nitrox.Launcher/Views/LaunchGameView.axaml.cs
@@ -1,32 +1,12 @@
-﻿using System;
-using System.ComponentModel;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
-using HanumanInstitute.MvvmDialogs.FrameworkDialogs;
-using Nitrox.Launcher.ViewModels;
+﻿using Nitrox.Launcher.ViewModels;
 using Nitrox.Launcher.Views.Abstract;
-using NitroxModel;
-using NitroxModel.Discovery;
-using NitroxModel.Helper;
 
 namespace Nitrox.Launcher.Views;
 
 public partial class LaunchGameView : RoutableViewBase<LaunchGameViewModel>
 {
-    public string PlatformToolTip => GamePlatform.GetAttribute<DescriptionAttribute>()?.Description ?? "Unknown";
-    public Platform GamePlatform => NitroxUser.GamePlatform?.Platform ?? Platform.NONE;
-    //public string Version => $"{LauncherLogic.ReleasePhase} {LauncherLogic.Version}";
-    
     public LaunchGameView()
     {
         InitializeComponent();
-    }
-
-    private void LogicPropertyChanged(object sender, PropertyChangedEventArgs args)
-    {
-        //OnPropertyChanged(nameof(GamePlatform));
-        //OnPropertyChanged(nameof(PlatformToolTip));
     }
 }
