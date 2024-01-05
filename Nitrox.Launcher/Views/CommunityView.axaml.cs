81b652e2586377475dd8ac147fd6c78936733abd
NinjaPedroX <pedro.gonzalez.nasa@gmail.com>
2024-01-04 19:04:12 -0600

    Finished "CommunityVew"
    Also made small improvements to the other views.

--- Nitrox.Launcher/Views/CommunityView.axaml.cs
+++ Nitrox.Launcher/Views/CommunityView.axaml.cs
@@ -1,7 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-using Nitrox.Launcher.ViewModels;
+﻿using Nitrox.Launcher.ViewModels;
 using Nitrox.Launcher.Views.Abstract;
 
 namespace Nitrox.Launcher.Views;
