219ef4e255db66053de0627ec6690c5048c8d511
OhmVIR <bourdeaux.adrien@gmail.com>
2024-02-20 19:00:36 -0500

    Added text wrapping so that the exception message does not exceed the messageBox's width, expanded messageBox vertically

--- Nitrox.Test/Model/StatusCodeTests.cs
+++ Nitrox.Test/Model/StatusCodeTests.cs
@@ -23,6 +23,13 @@ public class StatusCodeTests
         {
             DisplayStatusCode((StatusCode)i, true, "Testing exception");
         }
-        throw new Exception("Test failed: Program continued after a fatal StatusCode call");
+        throw new Exception("Test failed: Program continued to run after a fatal StatusCode call");
     }
+    [TestMethod]
+    public void ThrowStatusCodesLongException()
+    {
+        // Test if the text will wrap or if the messageBox will expand to accomodate for a long exception message
+        DisplayStatusCode(StatusCode.onedriveFolderDetected, false, "This is a testing exception that is super long to test if text will wrap to fit the message box space, or potentially expand the message box as needed. Did you know that you are wasting your time reading this? Like actually there is nothing here you can stop reading, just run the test and you will see this text there. Alright this is probably long enough hopefully it works :) and btw Crabsnake is the best mod.");
+    }
+
 }
