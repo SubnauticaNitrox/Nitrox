775cf862b42f8afe37ed9bd6a2254fdbe61aef0e
tornac1234 <24827220+tornac1234@users.noreply.github.com>
2024-05-10 11:18:44 +0200

    Add sync for plant exact slot in the planter

--- NitroxModel/DataStructures/GameLogic/Entities/Metadata/PlantableMetadata.cs
+++ NitroxModel/DataStructures/GameLogic/Entities/Metadata/PlantableMetadata.cs
@@ -11,19 +11,23 @@ public class PlantableMetadata : EntityMetadata
     [DataMember(Order = 1)]
     public float TimeStartGrowth { get; }
 
+    [DataMember(Order = 2)]
+    public int SlotID { get; }
+
     [IgnoreConstructor]
     protected PlantableMetadata()
     {
         // Constructor for serialization. Has to be "protected" for json serialization.
     }
 
-    public PlantableMetadata(float timeStartGrowth)
+    public PlantableMetadata(float timeStartGrowth, int slotID)
     {
         TimeStartGrowth = timeStartGrowth;
+        SlotID = slotID;
     }
 
     public override string ToString()
     {
-        return $"[{nameof(PlantableMetadata)} TimeStartGrowth: {TimeStartGrowth}]";
+        return $"[{nameof(PlantableMetadata)} TimeStartGrowth: {TimeStartGrowth}, SlotID: {SlotID}]";
     }
 }
--- NitroxPatcher/Patches/Dynamic/Planter_AddItem_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/Planter_AddItem_Patch.cs
@@ -19,7 +19,7 @@ public sealed partial class Planter_AddItem_Patch : NitroxPatch, IDynamicPatch
         }
 
         // When the planter accepts the new incoming seed, we want to send out metadata about what time the seed was planted.
-        if (plantable.TryGetIdOrWarn(out NitroxId id))
+        if (plantable.TryGetNitroxId(out NitroxId id))
         {
             Resolve<Entities>().EntityMetadataChanged(plantable, id);
         }
