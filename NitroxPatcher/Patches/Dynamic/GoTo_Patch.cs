bd8174520607cea0ce32d092eb4ad4d59e741149
MistaOmega <jack.nash6@gmail.com>
2024-05-24 15:59:46 +0100

    Recommended fixes

--- NitroxPatcher/Patches/Dynamic/GoTo_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/GoTo_Patch.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using HarmonyLib;
 using NitroxModel.Helper;
 using UnityEngine;
 
@@ -7,12 +6,12 @@ namespace NitroxPatcher.Patches.Dynamic;
 
 public sealed partial class GoTo_Patch : NitroxPatch, IDynamicPatch
 {
-    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((GotoConsoleCommand gotoConsoleCommand) => gotoConsoleCommand.GotoPosition(default, default));
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((GotoConsoleCommand t) => t.GotoPosition(default, default));
 
     public static bool Prefix(Vector3 position, bool gotoImmediate = false)
     {
         Vehicle currentMountedVehicle = Player.main.currentMountedVehicle;
-        if (currentMountedVehicle == null)
+        if (!currentMountedVehicle)
         {
             return true; // Normal GoTo behaviour if not in vehicle
         }
