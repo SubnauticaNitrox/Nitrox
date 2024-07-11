cd0c85ecdd35ef00420bc913a1980a4276685139
tornac1234 <24827220+tornac1234@users.noreply.github.com>
2024-07-11 18:11:24 +0200

    Add Instant Launch profile to automatize testing

---
+++ NitroxPatcher/Patches/Persistent/uGUI_MainMenu_Start_Patch.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Reflection;
+using HarmonyLib;
+using NitroxClient.MonoBehaviours.Gui.MainMenu;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Persistent;
+
+public sealed partial class uGUI_MainMenu_Start_Patch : NitroxPatch, IPersistentPatch
+{
+    private static readonly MethodInfo TARGET_METHOD_ENUMERATOR = Reflect.Method((uGUI_MainMenu t) => t.Start());
+    public static readonly MethodInfo TARGET_METHOD = AccessTools.EnumeratorMoveNext(TARGET_METHOD_ENUMERATOR);
+
+    private static bool Applied;
+
+    public static void Postfix()
+    {
+        if (Applied)
+        {
+            return;
+        }
+        Applied = true;
+
+        string[] args = Environment.GetCommandLineArgs();
+        Log.Info(string.Join(" ", args));
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].Equals("-instantlaunch", StringComparison.OrdinalIgnoreCase) && args.Length > i + 1)
+            {
+                Log.Info($"Detected instant launch, connecting to 127.0.0.1:11000");
+                _ = MainMenuMultiplayerPanel.OpenJoinServerMenuAsync("127.0.0.1", "11000", true);
+            }
+        }
+    }
+}
