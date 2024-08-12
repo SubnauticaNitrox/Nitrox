0dde824d268b9384a6f5dc0add0c17982544e2ea
dartasen <cleeeemove@gmail.com>
2024-08-12 13:25:26 +0200

    Add ConfigureCulture in launcher

--- Nitrox.Launcher/Program.cs
+++ Nitrox.Launcher/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using Avalonia;
 using Avalonia.ReactiveUI;
 using NitroxModel.Logger;
@@ -11,20 +13,20 @@ namespace Nitrox.Launcher;
 
 internal static class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
+    // Don't use any Avalonia, third-party APIs or any SynchronizationContext-reliant code before AppMain is called
+    // Things aren't initialized yet and stuff might break
     [STAThread]
     [MethodImpl(MethodImplOptions.NoInlining)]
     public static void Main(string[] args)
     {
+        ConfigureCultureInfo();
+
         AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolver.Handler;
         AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += AssemblyResolver.Handler;
 
-        LoadAvalonia(args);
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
     private static AppBuilder BuildAvaloniaApp()
     {
         Log.Setup();
@@ -34,8 +36,8 @@ internal static class Program
                                        .LogToTrace()
                                        .UseReactiveUI();
 
-        // The Wayland renderer on Linux using GPU rendering is not (yet) supported by Avalonia.
-        // Waiting on PR: https://github.com/AvaloniaUI/Avalonia/pull/11546 to enable rendering on GPU.
+        // The Wayland renderer on Linux using GPU rendering is not (yet) supported by Avalonia
+        // Waiting on PR: https://github.com/AvaloniaUI/Avalonia/pull/11546 to enable rendering on GPU
         if (Environment.GetEnvironmentVariable("WAYLAND_DISPLAY") is not null)
         {
             builder = builder.With(new X11PlatformOptions { RenderingMode = [X11RenderingMode.Software] });
@@ -44,10 +46,20 @@ internal static class Program
         return builder;
     }
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private static void LoadAvalonia(string[] args) =>
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    private static void ConfigureCultureInfo()
+    {
+        CultureInfo cultureInfo = new("en-US");
+
+        // Although we loaded the en-US cultureInfo, let's make sure to set these incase the
+        // default was overriden by the user
+        cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
+        cultureInfo.NumberFormat.NumberGroupSeparator = ",";
+
+        Thread.CurrentThread.CurrentCulture = cultureInfo;
+        Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+    }
 
     private static class AssemblyResolver
     {
