180bb35322aab94123b07e023bd413f2d204b394
Adrien B <bourdeaux.adrien@gmail.com>
2024-09-01 21:30:37 -0400

    Fixed FootstepPacket class to fit format(was crashing)

--- NitroxModel/Packets/FootstepPacket.cs
+++ NitroxModel/Packets/FootstepPacket.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace NitroxModel.Packets
 {
+    [Serializable]
     public class FootstepPacket : Packet
     {
-        public readonly ushort playerID;
-        public readonly byte assetIndex;
+        public ushort playerID { get; }
+        public byte assetIndex { get; }
         public FootstepPacket(ushort playerID, byte assetIndex)
         {
             Log.Info("Creating footstep packet");
