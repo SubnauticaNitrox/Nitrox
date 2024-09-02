e07a5c30a9988ddf19a5b13b812df599bd1d1c04
Adrien B <bourdeaux.adrien@gmail.com>
2024-09-01 20:38:38 -0400

    Footstep packets are forwarded from server to players within hearing range(20f)

--- NitroxServer/Communication/Packets/Processors/FootstepPacketProcessor.cs
+++ NitroxServer/Communication/Packets/Processors/FootstepPacketProcessor.cs
@@ -1,23 +1,32 @@
+using NitroxModel.DataStructures.Unity;
 using NitroxModel.Packets;
 using NitroxServer.Communication.Packets.Processors.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NitroxServer.GameLogic;
+using System.Numerics;
 
 namespace NitroxServer.Communication.Packets.Processors
 {
     public class FootstepPacketProcessor : AuthenticatedPacketProcessor<FootstepPacket>
     {
         private readonly float footstepAudioRange;
-        public FootstepPacketProcessor(float footstepAudioRange)
+        private readonly PlayerManager playerManager;
+        public FootstepPacketProcessor(float footstepAudioRange, PlayerManager playerManager)
         {
             this.footstepAudioRange = footstepAudioRange;
+            this.playerManager = playerManager;
         }
-        public override void Process(FootstepPacket packet, Player sendingPlayer)
+        public override void Process(FootstepPacket footstepPacket, Player sendingPlayer)
         {
             Log.Info("Processing footstep packet on server from " + sendingPlayer.Name);
+            var players = playerManager.GetAllPlayers();
+            foreach(Player player in players)
+            {
+                if(NitroxVector3.Distance(player.Position, sendingPlayer.Position) <= footstepAudioRange)
+                {
+                    // Forward footstep packet to players if they are within range to hear it
+                    playerManager.SendPacketToOtherPlayers(footstepPacket, sendingPlayer);
+                }
+            }
         }
     }
 }
