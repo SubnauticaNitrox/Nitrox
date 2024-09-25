0323efc85d6ca8bdcf813a45683f7927ecaa183f
MistaOmega <jack.nash6@gmail.com>
2024-09-25 20:37:03 +0100

    Standardises IP Getters to ones used in LogHowToConnectAsync. Does not fetch local IP port as it seems counter-intuitive to invite someone to your local session

--- NitroxClient/Communication/Packets/Processors/DiscordRequestIPProcessor.cs
+++ NitroxClient/Communication/Packets/Processors/DiscordRequestIPProcessor.cs
@@ -1,13 +1,33 @@
-﻿using NitroxClient.Communication.Packets.Processors.Abstract;
+﻿using System.Net;
+using System.Threading.Tasks;
+using NitroxClient.Communication.Packets.Processors.Abstract;
 using NitroxClient.MonoBehaviours.Discord;
+using NitroxModel.Helper;
 using NitroxModel.Packets;
 
 namespace NitroxClient.Communication.Packets.Processors;
 
 public class DiscordRequestIPProcessor : ClientPacketProcessor<DiscordRequestIP>
 {
-    public override void Process(DiscordRequestIP packet)
+    public override async void Process(DiscordRequestIP packet)
     {
-        DiscordClient.UpdateIpPort(packet.IpPort);
+        string ipPort = await GetIpPortAsync();
+        DiscordClient.UpdateIpPort(ipPort);
+    }
+
+    /** Get the WAN IP address or the Hamachi IP address if the WAN IP address is not available. */
+    private static async Task<string> GetIpPortAsync()
+    {
+        Task<IPAddress> wanIp = NetHelper.GetWanIpAsync();
+        Task<IPAddress> hamachiIp = Task.Run(NetHelper.GetHamachiIp);
+        if (await wanIp != null)
+        {
+            return wanIp.Result.ToString();
+        }
+        if (await hamachiIp != null)
+        {
+            return hamachiIp.Result.ToString();
+        }
+        return "127.0.0.1";
     }
 }
