5a4b34a99475a40c5b32ad543ce01dadaa5f4cbb
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-10-19 11:32:58 -0400

    Updated sound levels
    Co-authored-by: gussandst <160942469+gussandst@users.noreply.github.com>
    Co-authored-by: WerewolfsX <160942469+WerewolfsX@users.noreply.github.com>

--- NitroxServer/Communication/Packets/Processors/FootstepPacketProcessor.cs
+++ NitroxServer/Communication/Packets/Processors/FootstepPacketProcessor.cs
@@ -8,7 +8,7 @@ namespace NitroxServer.Communication.Packets.Processors;
 
 public class FootstepPacketProcessor : AuthenticatedPacketProcessor<FootstepPacket>
 {
-    private readonly float footstepAudioRange; // To modify this value, modify the last value in the SoundWhitelist_Subnautica.csv file
+    private readonly float footstepAudioRange; // To modify this value, modify the last value of the event:/player/footstep_precursor_base sound in the SoundWhitelist_Subnautica.csv file
     private readonly PlayerManager playerManager;
 
     public FootstepPacketProcessor(PlayerManager playerManager, FMODWhitelist whitelist)
