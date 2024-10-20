dc90f193fbab9cfcb61745a740fd78951c4e4f1c
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-10-20 07:52:17 -0400

    Sync footstep audio (#2177)
    * Created packet and processors for footstep audio(not functional at all yet)
    
    * prototype client-side footstep packet processing
    
    * Switch to using player's AnimationController to find velocity
    
    * Footstep packets are forwarded from server to players within hearing range(20f)
    
    * Fixed FootstepPacket class to fit format(was crashing)
    
    * Removed duplicate var
    
    * cleanup
    
    * Added transpiler to send packets when local client plays footstep sound(TYSM for the help Jannify)
    Co-authored-by: Jannify <24827220+Jannify@users.noreply.github.com>
    
    * Lower volume of other players' footsteps(suggested by NinjaPedroX)
    
    * Added same structure/submarine check for sending footstep packet to other player
    
    * Removed logs
    
    * Formatting changes
    
    * Formatting changes
    
    * Newline changes
    
    * newline changes
    
    * Split long line into shorter lines
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Remove unneeded this keyword
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Applying suggestions from Jannify's review
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Added comment in packet processors to keep values updated, trying to implement fetching the value from .csv file
    
    * Pull sound range from .csv file(UNTESTED)
    
    * Add whitespace
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Add whitespace
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Merged ifs and removed var type usage
    
    * Merged else + if into else if
    
    * Inverted if with early return condition
    
    * Compare subroot IDs using .Equals instead of 2 if statements
    
    * Overriding .Equals in Optional<> for consistency and performance
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Merged 2 if statements & fixed bad clause
    
    * Fixed bug(was getting all players instead of only connected ones)
    
    * Changed names of StepSound enum members
    
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    
    * removed unneeded this keyword
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    
    * Reordered and cleaned usings
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    
    * Did sort and clean usings with correct setting
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    
    * added extra if check
    
    * Change number of added instructions in test class
    
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    
    * equality methods can be used by systems depending on the IEquatable abstraction
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Inverted if statement
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    
    * Updated sound levels
    
    Co-authored-by: gussandst <160942469+gussandst@users.noreply.github.com>
    Co-authored-by: WerewolfsX <160942469+WerewolfsX@users.noreply.github.com>
    
    * Change references of exosuit steps asset path to land asset path
    
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    
    ---------
    
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>
    Co-authored-by: tornac1234 <tortornac@gmail.com>
    Co-authored-by: gussandst <160942469+gussandst@users.noreply.github.com>
    Co-authored-by: WerewolfsX <160942469+WerewolfsX@users.noreply.github.com>

--- NitroxPatcher/Patches/Dynamic/FootstepSounds_OnStep_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/FootstepSounds_OnStep_Patch.cs
@@ -1,19 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using FMOD.Studio;
 using HarmonyLib;
+using NitroxClient.Communication.Abstract;
+using NitroxClient.GameLogic;
 using NitroxClient.GameLogic.FMOD;
 using NitroxModel.GameLogic.FMOD;
 using NitroxModel.Helper;
+using NitroxModel.Packets;
 using UnityEngine;
 
 namespace NitroxPatcher.Patches.Dynamic;
 
 public sealed partial class FootstepSounds_OnStep_Patch : NitroxPatch, IDynamicPatch
 {
-    private const string EXO_STEP_SOUND_PATH = "event:/sub/exo/step";
+    private const string PRECURSOR_STEP_SOUND_PATH = "event:/player/footstep_precursor_base";
+    private const string METAL_STEP_SOUND_PATH = "event:/player/footstep_metal";
+    private const string LAND_STEP_SOUND_PATH = "event:/player/footstep_dirt";
 
     internal static readonly MethodInfo TARGET_METHOD = Reflect.Method((FootstepSounds t) => t.OnStep(default));
 
@@ -21,12 +26,18 @@ public sealed partial class FootstepSounds_OnStep_Patch : NitroxPatch, IDynamicP
     {
         /*
         From:
-
             evt.setVolume(volume);
 
         To:
-
             evt.setVolume(CalculateVolume(volume, this, asset, xform));
+
+
+        From:
+            event.release();
+
+        To:
+            event.release();
+            SendFootstepPacket(asset);
          */
 
         return new CodeMatcher(instructions)
@@ -40,6 +51,13 @@ public sealed partial class FootstepSounds_OnStep_Patch : NitroxPatch, IDynamicP
                    new CodeInstruction(OpCodes.Ldarg_1),
                    new CodeInstruction(OpCodes.Call, Reflect.Method(() => CalculateVolume(default, default, default, default)))
                )
+               .MatchEndForward(
+                   new CodeMatch(OpCodes.Call, Reflect.Method((EventInstance evt) => evt.release())),
+                   new CodeMatch(OpCodes.Pop)
+               )
+               .Advance(1)
+               .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc_0))
+               .Insert(new CodeInstruction(OpCodes.Call, Reflect.Method(() => SendFootstepPacket(default))))
                .InstructionEnumeration();
     }
 
@@ -47,7 +65,7 @@ public sealed partial class FootstepSounds_OnStep_Patch : NitroxPatch, IDynamicP
     private static float CalculateVolume(float originalVolume, FootstepSounds instance, FMODAsset asset, Transform xform)
     {
         // For exosuits only "event:/sub/exo/step" is used inside FootstepSounds
-        if (asset.path.Equals(EXO_STEP_SOUND_PATH, StringComparison.Ordinal) && (!Player.main.currentMountedVehicle || Player.main.currentMountedVehicle.gameObject != instance.gameObject))
+        if (asset.path.Equals(LAND_STEP_SOUND_PATH, StringComparison.Ordinal) && (!Player.main.currentMountedVehicle || Player.main.currentMountedVehicle.gameObject != instance.gameObject))
         {
             // Origin is Exosuit which is controlled by remote player
             return FMODSystem.CalculateVolume(xform.position, Player.main.transform.position, stepSoundRadius.Value, originalVolume);
@@ -58,7 +76,32 @@ public sealed partial class FootstepSounds_OnStep_Patch : NitroxPatch, IDynamicP
 
     private static readonly Lazy<float> stepSoundRadius = new(() =>
     {
-        Resolve<FMODWhitelist>().TryGetSoundData(EXO_STEP_SOUND_PATH, out SoundData soundData);
+        Resolve<FMODWhitelist>().TryGetSoundData(LAND_STEP_SOUND_PATH, out SoundData soundData);
         return soundData.Radius;
     });
+
+    private static void SendFootstepPacket(FMODAsset asset)
+    {
+        if (!Resolve<LocalPlayer>().PlayerId.HasValue)
+        {
+            return;
+        }
+        FootstepPacket.StepSounds assetIndex;
+        switch (asset.path)
+        {
+            case PRECURSOR_STEP_SOUND_PATH:
+                assetIndex = FootstepPacket.StepSounds.PRECURSOR;
+                break;
+            case METAL_STEP_SOUND_PATH:
+                assetIndex = FootstepPacket.StepSounds.METAL;
+                break;
+            case LAND_STEP_SOUND_PATH:
+                assetIndex = FootstepPacket.StepSounds.LAND;
+                break;
+            default:
+                return;
+        }
+        FootstepPacket footstepPacket = new(Resolve<LocalPlayer>().PlayerId.Value, assetIndex);
+        Resolve<IPacketSender>().Send(footstepPacket);
+    }
 }
