a76fd07ae5b5ea8deef0704eb5d6ed9b380929e1
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2024-11-17 20:26:18 -0500

    Remove unused import

--- NitroxClient/GameLogic/InitialSync/EquippedItemInitialSyncProcessor.cs
+++ NitroxClient/GameLogic/InitialSync/EquippedItemInitialSyncProcessor.cs
@@ -7,7 +7,6 @@ using NitroxClient.GameLogic.InitialSync.Abstract;
 using NitroxClient.MonoBehaviours;
 using NitroxClient.Unity.Helper;
 using NitroxModel.DataStructures;
-using NitroxModel.DataStructures.GameLogic;
 using NitroxModel.DataStructures.Util;
 using NitroxModel.Packets;
 using UnityEngine;
