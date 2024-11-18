9b1fa8b06ec7fb98a5c9ea48b27fe448c642738d
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2024-11-18 10:03:52 -0500

    Forgot a line in the decoy upgrade patch

--- NitroxPatcher/Patches/Dynamic/SubRoot_SetCyclopsUpgrades_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/SubRoot_SetCyclopsUpgrades_Patch.cs
@@ -4,10 +4,10 @@ using NitroxModel.Helper;
 namespace NitroxPatcher.Patches.Dynamic;
 
 /// <summary>
-/// The way Subnautica handles modules in Cyclops wrecks is pretty weird. If any module is added/removed, they
-/// are all instantly disabled, which deletes creature decoys in any slot except the first one. We want to keep
-/// the creature decoys around if the module is inserted, so we allow use of that module even when the Cyclops
-/// has been destroyed.
+/// The way Subnautica handles modules in Cyclops wrecks is pretty weird. if any module is added/removed (and when spawning
+/// the entity during joining), they are all instantly disabled, which deletes creature decoys in any slot except
+/// the first one. We want to keep the creature decoys around if the module is inserted, so we allow use of that
+/// module even when the Cyclops has been destroyed.
 /// </summary>
 public sealed partial class SubRoot_SetCyclopsUpgrades_Patch : NitroxPatch, IDynamicPatch
 {
@@ -17,6 +17,8 @@ public sealed partial class SubRoot_SetCyclopsUpgrades_Patch : NitroxPatch, IDyn
     {
         if (__instance.upgradeConsole != null)
         {
+            __instance.decoyTubeSizeIncreaseUpgrade = false;
+
             Equipment modules = __instance.upgradeConsole.modules;
             foreach (string slot in SubRoot.slotNames)
             {
