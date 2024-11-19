9d2ce1af2393bbfd05dfb0e1809d8dc4b584a91b
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2024-11-18 22:52:21 -0500

    Sync Cyclops loot (too much is spawning though)

--- NitroxPatcher/Patches/Dynamic/CyclopsDestructionEvent_SpawnLootAsync_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/CyclopsDestructionEvent_SpawnLootAsync_Patch.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
+using NitroxClient.Communication;
 using NitroxClient.GameLogic;
 using NitroxClient.MonoBehaviours;
 using NitroxModel_Subnautica.DataStructures;
@@ -10,6 +11,7 @@ using NitroxModel.DataStructures;
 using NitroxModel.DataStructures.GameLogic;
 using NitroxModel.DataStructures.GameLogic.Entities;
 using NitroxModel.Helper;
+using NitroxModel.Packets;
 using NitroxPatcher.PatternMatching;
 using UnityEngine;
 
@@ -19,7 +21,7 @@ public sealed partial class CyclopsDestructionEvent_SpawnLootAsync_Patch : Nitro
 {
     public static readonly MethodInfo TARGET_METHOD = AccessTools.EnumeratorMoveNext(Reflect.Method((CyclopsDestructionEvent t) => t.SpawnLootAsync()));
 
-    public static readonly InstructionsPattern PATTERN = new()
+    public static readonly InstructionsPattern PATTERN = new(expectedMatches: 2)
     {
         { Reflect.Method(() => UnityEngine.Object.Instantiate(default(GameObject), default(Vector3), default(Quaternion))), "SpawnObject" }
     };
@@ -35,20 +37,13 @@ public sealed partial class CyclopsDestructionEvent_SpawnLootAsync_Patch : Nitro
 
     public static void Callback(GameObject gameObject, CyclopsDestructionEvent __instance)
     {
-        NitroxEntity cyclopsEntity = __instance.GetComponent<NitroxEntity>();
+        NitroxId lootId = NitroxEntity.GenerateNewId(gameObject);
 
-        if (Resolve<SimulationOwnership>().HasAnyLockType(cyclopsEntity.Id))
-        {
-            Log.Debug("Spawning Cyclops loot");
+        LargeWorldEntity largeWorldEntity = gameObject.GetComponent<LargeWorldEntity>();
+        PrefabIdentifier prefabIdentifier = gameObject.GetComponent<PrefabIdentifier>();
+        Pickupable pickupable = gameObject.GetComponent<Pickupable>();
 
-            NitroxId lootId = NitroxEntity.GenerateNewId(gameObject);
-
-            LargeWorldEntity largeWorldEntity = gameObject.GetComponent<LargeWorldEntity>();
-            PrefabIdentifier prefabIdentifier = gameObject.GetComponent<PrefabIdentifier>();
-            Pickupable pickupable = gameObject.GetComponent<Pickupable>();
-
-            WorldEntity lootEntity = new(gameObject.transform.ToWorldDto(), (int)largeWorldEntity.cellLevel, prefabIdentifier.classId, false, lootId, pickupable.GetTechType().ToDto(), null, null, []);
-            Resolve<Entities>().BroadcastEntitySpawnedByClient(lootEntity);
-        }
+        WorldEntity lootEntity = new(gameObject.transform.ToWorldDto(), (int)largeWorldEntity.cellLevel, prefabIdentifier.classId, false, lootId, pickupable.GetTechType().ToDto(), null, null, []);
+        Resolve<Entities>().BroadcastEntitySpawnedByClient(lootEntity);
     }
 }
