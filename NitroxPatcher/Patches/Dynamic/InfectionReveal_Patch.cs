90db6d409d917c4e1530aeb7caa5f6b07e16d431
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2024-12-26 23:39:17 -0500

    Implemented transpiler that triggers when the infection sync animation is played

---
+++ NitroxPatcher/Patches/Dynamic/InfectionReveal_Patch.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+public sealed partial class InfectionReveal_Patch : NitroxPatch, IDynamicPatch
+{
+    internal static readonly MethodInfo TARGET_METHOD = AccessTools.EnumeratorMoveNext(Reflect.Method((Player t) => t.TriggerInfectionRevealAsync()));
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        /*
+From:
+            if (!this.infectionRevealed){
+    float num = this.armsController.StartHolsterTime(12f);
+
+To:
+            if (!this.infectionRevealed){
+    float num = this.armsController.StartHolsterTime(12f);
+    SendInfectAnimationStartPacket();
+ */
+        return new CodeMatcher(instructions)
+       .MatchStartForward(
+           new CodeMatch(OpCodes.Ldloc_1),
+           new CodeMatch(OpCodes.Ldfld),
+           new CodeMatch(OpCodes.Ldc_R4),
+           new CodeMatch(OpCodes.Callvirt, Reflect.Method((ArmsController arms) => arms.StartHolsterTime(default)))
+       )
+       .Insert(
+           new CodeInstruction(OpCodes.Call, Reflect.Method(() => SendInfectAnimationStartPacket()))
+       )
+       .InstructionEnumeration();
+    }
+    public static void SendInfectAnimationStartPacket()
+    {
+        Log.Debug("Infection animation transpiler hit");
+    }
+}
