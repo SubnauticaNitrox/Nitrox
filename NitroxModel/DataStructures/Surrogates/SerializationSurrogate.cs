0cec521f3b8a81727134839bae0cdab31e509295
dartasen <cleeeemove@gmail.com>
2024-12-28 11:59:22 +0100

    Discard warnings for Surrogates that uses Binary serialization

--- NitroxModel/DataStructures/Surrogates/SerializationSurrogate.cs
+++ NitroxModel/DataStructures/Surrogates/SerializationSurrogate.cs
@@ -1,21 +1,22 @@
-﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization;
 
-namespace NitroxModel.DataStructures.Surrogates
+namespace NitroxModel.DataStructures.Surrogates;
+
+#pragma warning disable SYSLIB0050 // Nitrox can depend on Binary serialization
+public abstract class SerializationSurrogate<T> : ISerializationSurrogate
 {
-    public abstract class SerializationSurrogate<T> : ISerializationSurrogate
+    public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
     {
-        public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
-        {
-            GetObjectData((T)obj, info);
-        }
-
-        public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
-        {
-            return SetObjectData((T)obj, info);
-        }
-
-        protected abstract void GetObjectData(T obj, SerializationInfo info);
-
-        protected abstract T SetObjectData(T obj, SerializationInfo info);
+        GetObjectData((T)obj, info);
     }
+
+    public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+    {
+        return SetObjectData((T)obj, info);
+    }
+
+    protected abstract void GetObjectData(T obj, SerializationInfo info);
+
+    protected abstract T SetObjectData(T obj, SerializationInfo info);
 }
+#pragma warning restore SYSLIB0050
