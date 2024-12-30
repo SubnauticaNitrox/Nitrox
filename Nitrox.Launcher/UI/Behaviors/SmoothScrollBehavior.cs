9dd09a40c929f6bfcdb6551bf5a806a5e7cf546c
NinjaPedroX <superninjapedro1@gmail.com>
2024-12-30 03:47:38 -0600

    Implemented Smooth Scrolling
    FINALLY... it's 3:30am and I got this figured out. I have no idea how I ended up with this black magic that you see before you, but it works pretty well. Please let me know if I've committed any unforgivable sins in these changes :)
    
    On a more serious note, I think the scroll animation looks way better than it did before, but it still feels slightly jittery. And I don't like how I had to rely on a "LastOffset" property for this to work properly, but it's the best that I could come up with seeing that "ScrollViewer.Offset" is already changed to the target scroll value when this code is being run.

---
+++ Nitrox.Launcher/UI/Behaviors/SmoothScrollBehavior.cs
@@ -0,0 +1,109 @@
+﻿using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Styling;
+using System;
+using System.Threading;
+
+namespace Nitrox.Launcher.UI.Behaviors;
+
+public abstract class SmoothScrollBehavior
+{
+    private static CancellationTokenSource animationTokenSource;
+    
+    public static readonly AttachedProperty<bool> EnableSmoothScrollingProperty =
+        AvaloniaProperty.RegisterAttached<SmoothScrollBehavior, ScrollViewer, bool>("EnableSmoothScrolling");
+    
+    public static bool GetEnableSmoothScrolling(ScrollViewer element) => element.GetValue(EnableSmoothScrollingProperty);
+
+    public static void SetEnableSmoothScrolling(ScrollViewer element, bool value) => element.SetValue(EnableSmoothScrollingProperty, value);
+
+    // lastOffsetProperty is needed here since the ScrollViewer.Offset property is already set to the target offset when the PointerWheelChanged event is raised
+    private static readonly AttachedProperty<Vector> lastOffsetProperty =
+        AvaloniaProperty.RegisterAttached<SmoothScrollBehavior, ScrollViewer, Vector>("LastOffset", new Vector(0, 0));
+
+    private static Vector GetLastOffset(ScrollViewer element) => element.GetValue(lastOffsetProperty);
+
+    private static void SetLastOffset(ScrollViewer element, Vector value) => element.SetValue(lastOffsetProperty, value);
+    
+    static SmoothScrollBehavior()
+    {
+        EnableSmoothScrollingProperty.Changed.Subscribe(OnEnableSmoothScrollingChanged);
+    }
+        
+    private static void OnEnableSmoothScrollingChanged(AvaloniaPropertyChangedEventArgs<bool> args)
+    {
+        if (args.Sender is not ScrollViewer scrollViewer)
+        {
+            return;
+        }
+        
+        if (args.NewValue.GetValueOrDefault())
+        {
+            scrollViewer.AddHandler(InputElement.PointerWheelChangedEvent, OnPointerWheelChanged, handledEventsToo: true);
+        }
+        else
+        {
+            scrollViewer.RemoveHandler(InputElement.PointerWheelChangedEvent, OnPointerWheelChanged);
+        }
+    }
+    
+    private static void OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
+    {
+        if (sender is not ScrollViewer scrollViewer)
+        {
+            return;
+        }
+
+        // Cancel ongoing animations
+        animationTokenSource?.Cancel();
+        animationTokenSource?.Dispose();
+
+        // Get new offset (already set within scrollviewer)
+        Vector newOffset = scrollViewer.Offset;
+
+        AnimateScroll(scrollViewer, newOffset);
+        SetLastOffset(scrollViewer, newOffset);
+    }
+
+    private static async void AnimateScroll(ScrollViewer scrollViewer, Vector targetOffset)
+    {
+        animationTokenSource = new CancellationTokenSource();
+        CancellationToken token = animationTokenSource.Token;
+
+        Animation animation = new()
+        {
+            Duration = TimeSpan.FromMilliseconds(250),
+            Easing = new ExponentialEaseOut(),
+        };
+
+        animation.Children.Add(new KeyFrame
+        {
+            Cue = new Cue(0),
+            Setters =
+            {
+                new Setter(ScrollViewer.OffsetProperty, GetLastOffset(scrollViewer))
+            }
+        });
+
+        animation.Children.Add(new KeyFrame
+        {
+            Cue = new Cue(1),
+            Setters =
+            {
+                new Setter(ScrollViewer.OffsetProperty, targetOffset)
+            }
+        });
+
+        try
+        {
+            if (GetLastOffset(scrollViewer) != targetOffset)
+            {
+                await animation.RunAsync(scrollViewer, token);
+            }
+        }
+        catch (OperationCanceledException) { }
+    }
+}
