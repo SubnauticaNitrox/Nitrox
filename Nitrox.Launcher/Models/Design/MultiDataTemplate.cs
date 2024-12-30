ad2d637a417d732434e5265ab0cb4b1297ff191b
Measurity <1107063+Measurity@users.noreply.github.com>
2024-12-30 15:46:56 +0100

    Added data template reuse for MultiDataTemplate
    Reusing views will allow scrollbar position to be remembered.

--- Nitrox.Launcher/Models/Design/MultiDataTemplate.cs
+++ Nitrox.Launcher/Models/Design/MultiDataTemplate.cs
@@ -1,4 +1,5 @@
 extern alias JB;
+using System;
 using System.Collections.Generic;
 using Avalonia.Collections;
 using Avalonia.Controls;
@@ -12,35 +13,47 @@ namespace Nitrox.Launcher.Models.Design;
 /// <summary>
 ///     Selects a <see cref="DataTemplate" /> based on its <see cref="DataTemplate.DataType" />.
 /// </summary>
-public class MultiDataTemplate : AvaloniaList<DataTemplate>, IDataTemplate
+public class MultiDataTemplate : AvaloniaList<DataTemplate>, IRecyclingDataTemplate
 {
     [Content]
     [UsedImplicitly]
     public List<DataTemplate> Content { get; set; } = new();
 
-    public bool Match(object data)
+    private readonly Dictionary<Type, Control> typeToControlCache = [];
+
+    public bool Match(object data) => GetTemplateForType(data?.GetType()) != null;
+
+    public Control Build(object data, Control existing)
     {
-        foreach (DataTemplate template in Content)
+        Type type = data?.GetType();
+        if (type != null && typeToControlCache.TryGetValue(type, out Control control))
         {
-            if (template.DataType?.IsInstanceOfType(data) ?? false)
-            {
-                return true;
-            }
+            return control;
+        }
+        Control build = GetTemplateForType(type)?.Build(data);
+        if (type != null && build != null)
+        {
+            typeToControlCache[type] = build;
         }
 
-        return false;
+        return build ?? existing;
     }
 
-    public Control Build(object param)
+    public Control Build(object data) => GetTemplateForType(data.GetType())?.Build(data) ?? new TextBlock { Text = "" };
+
+    private IDataTemplate GetTemplateForType(Type type)
     {
+        if (type == null)
+        {
+            return null;
+        }
         foreach (DataTemplate template in Content)
         {
-            if (template.DataType?.IsInstanceOfType(param) ?? false)
+            if (template.DataType?.IsAssignableTo(type) ?? false)
             {
-                return template.Build(param);
+                return template;
             }
         }
-
-        return new TextBlock { Text = "" };
+        return null;
     }
 }
