9cd852cb321b3b5d2b4d30c8dfd700c271ec1f8c
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-12 17:14:40 -0500

    Switch to using blank gameobjects instead of actual beacons

--- NitroxClient/Communication/Packets/Processors/PlayerDeathProcessor.cs
+++ NitroxClient/Communication/Packets/Processors/PlayerDeathProcessor.cs
@@ -39,24 +39,15 @@ public class DeathBeacon : MonoBehaviour
 
     public static IEnumerator SpawnDeathBeacon(NitroxVector3 location, string playerName)
     {
-        TaskResult<GameObject> result = new TaskResult<GameObject>();
-        yield return DefaultWorldEntitySpawner.RequestPrefab(TechType.Beacon, result);
-        GameObject beaconPrefab = result.Get();
-        if (beaconPrefab != null)
-        {
-            GameObject beacon = Instantiate(beaconPrefab);
-            Pickupable item = beacon.GetComponent<Pickupable>();
-            if (item != null)
-            {
-                using (PacketSuppressor<EntitySpawnedByClient>.Suppress()) // Prevent the beacon from syncing to other players or the server so it will disappear on disconnect
-                {
-                    item.Drop(location.ToUnity(), new Vector3(0, 0, 0), false);
-                }
-                beacon.GetComponent<Beacon>().label = $"{playerName}'s death";
-                beacon.GetComponent<WorldForces>().aboveWaterGravity = 0;
-                beacon.GetComponent<WorldForces>().aboveWaterDrag = 10; // Object has a miniscule velocity in a random error from floating point error, need to add drag or object will drift
-                beacon.AddComponent<DeathBeacon>();            }
-        }
+        GameObject beacon = new();
+        PingInstance signal = beacon.AddComponent<PingInstance>();
+        signal.pingType = PingType.Signal;
+        signal.displayPingInManager = true;
+        signal.origin = beacon.transform;
+        signal._label = $"{playerName}'s death";
+        beacon.transform.position = location.ToUnity();
+        beacon.AddComponent<DeathBeacon>();
+        signal.Initialize();
         yield break;
     }
 
