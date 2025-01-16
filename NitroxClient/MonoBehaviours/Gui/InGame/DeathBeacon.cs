481dc6a2ae17628d9313b1ae59766cb067c1c592
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-01-15 23:43:06 -0500

    Moved DeathBeacon class to its own file

---
+++ NitroxClient/MonoBehaviours/Gui/InGame/DeathBeacon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using NitroxModel.DataStructures.Unity;
+using NitroxModel_Subnautica.DataStructures;
+using UnityEngine;
+
+public class DeathBeacon : MonoBehaviour
+{
+    private static readonly float despawnDistance = 20f;
+
+    public static IEnumerator SpawnDeathBeacon(NitroxVector3 location, string playerName)
+    {
+        GameObject beacon = new();
+        PingInstance signal = beacon.AddComponent<PingInstance>();
+        signal.pingType = PingType.Signal;
+        signal.displayPingInManager = true;
+        signal.origin = beacon.transform;
+        signal._label = $"{playerName}'s death";
+        beacon.transform.position = location.ToUnity();
+        beacon.AddComponent<DeathBeacon>();
+        signal.Initialize();
+        yield break;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(Player.main.transform.position, transform.position) <= despawnDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
