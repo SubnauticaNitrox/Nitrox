f916df406dddc40fa9e237f68986ba537bbf1255
dartasen <cleeeemove@gmail.com>
2025-01-18 20:38:33 +0100

    wip

--- Nitrox.Test/Patcher/Patches/PatchesTranspilerTest.cs
+++ Nitrox.Test/Patcher/Patches/PatchesTranspilerTest.cs
@@ -78,6 +78,7 @@ public class PatchesTranspilerTest
         [typeof(uSkyManager_SetVaryingMaterialProperties_Patch), 0],
         [typeof(Welder_Weld_Patch), 1],
         [typeof(Poop_Perform_Patch), 1],
+        [typeof(PrisonPredatorSwimToPlayer_Perform_Patch), -34, true],
         [typeof(SeaDragonMeleeAttack_OnTouchFront_Patch), 9],
         [typeof(SeaDragonMeleeAttack_SwatAttack_Patch), 4],
         [typeof(SeaTreaderSounds_SpawnChunks_Patch), 3],
