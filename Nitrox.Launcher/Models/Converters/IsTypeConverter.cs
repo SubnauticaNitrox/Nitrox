9aab7ae71767f8c222c39aafe0058fe24fa69e56
Measurity <1107063+Measurity@users.noreply.github.com>
2025-02-09 00:56:42 +0100

    Fixed nav items not being checked when navigating to a view via code

--- Nitrox.Launcher/Models/Converters/IsTypeConverter.cs
+++ Nitrox.Launcher/Models/Converters/IsTypeConverter.cs
@@ -1,22 +1,34 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Avalonia.Data;
 
 namespace Nitrox.Launcher.Models.Converters;
 
 /// <summary>
-///     Returns true if values are equal to each other.
-///     Or if value is singular, if parameter is equal to the value.
+///     Returns true if value is of the type as given by parameter (or any if parameter is a collection of types).
 /// </summary>
 public class IsTypeConverter : Converter<IsTypeConverter>
 {
     public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (parameter is not Type typeParameter)
+        switch (parameter)
         {
-            return new BindingNotification(new ArgumentException($"Expected {nameof(parameter)} to be a {typeof(Type).FullName}"), BindingErrorType.Error);
+            case Type typeParameter:
+                return typeParameter.IsInstanceOfType(value);
+            case IEnumerable<Type> typeParameters:
+            {
+                foreach (Type type in typeParameters)
+                {
+                    if (type.IsInstanceOfType(value))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            default:
+                return new BindingNotification(new ArgumentException($"Expected {nameof(parameter)} to be a {typeof(Type).FullName}"), BindingErrorType.Error);
         }
-
-        return typeParameter.IsInstanceOfType(value);
     }
 }
