1a771f9e7628667f0257a92226e8002d72c59b38
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-02-08 23:30:24 -0500

    Display in-game log correctly when time is paused

---
+++ NitroxPatcher/Patches/Dynamic/ErrorMessage_OnLateUpdate_Patch.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NitroxModel.Helper;
+using NitroxPatcher.Patches;
+using UnityEngine;
+
+/// <summary>
+/// Ensures the in-game log is animated smoothly regardless of the time scale.
+/// </summary>
+public sealed partial class ErrorMessage_OnLateUpdate_Patch : NitroxPatch, IDynamicPatch
+{
+    private static readonly MethodInfo TARGET_METHOD_ON_LATE_UPDATE = Reflect.Method((ErrorMessage t) => t.OnLateUpdate());
+    private static readonly MethodInfo TARGET_METHOD_ADD_MESSAGE = Reflect.Method((ErrorMessage t) => t._AddMessage(default));
+    private static readonly MethodInfo TARGET_OPERAND_TIME = Reflect.Property(() => PDA.time).GetMethod;
+    private static readonly MethodInfo TARGET_OPERAND_DELTA_TIME = Reflect.Property(() => PDA.deltaTime).GetMethod;
+    private static readonly MethodInfo INJECTION_OPERAND_UNSCALED_TIME = Reflect.Property(() => Time.unscaledTime).GetMethod;
+    private static readonly MethodInfo INJECTION_OPERAND_UNSCALED_DELTA_TIME = Reflect.Property(() => Time.unscaledDeltaTime).GetMethod;
+
+
+    /*
+     * Replace all calls to PDA.time with Time.unscaledTime and to
+     * PDA.deltaTime with Time.unscaledDeltaTime
+     */
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        CodeMatcher matcher = new CodeMatcher(instructions);
+
+        while (matcher.MatchStartForward(new CodeMatch(OpCodes.Call, TARGET_OPERAND_TIME)).IsValid)
+        {
+            matcher.SetOperandAndAdvance(INJECTION_OPERAND_UNSCALED_TIME);
+        }
+
+        matcher.Start();
+
+        while (matcher.MatchStartForward(new CodeMatch(OpCodes.Call, TARGET_OPERAND_DELTA_TIME)).IsValid)
+        {
+            matcher.SetOperandAndAdvance(INJECTION_OPERAND_UNSCALED_DELTA_TIME);
+        }
+
+        return matcher.InstructionEnumeration();
+    }
+
+    public override void Patch(Harmony harmony)
+    {
+        MethodInfo transpilerInfo = Reflect.Method(() => Transpiler(default));
+
+        // TODO: modify sanity checks to support transpilers with multiple target methods
+        PatchTranspiler(harmony, TARGET_METHOD_ON_LATE_UPDATE, transpilerInfo);
+        PatchTranspiler(harmony, TARGET_METHOD_ADD_MESSAGE, transpilerInfo);
+    }
+}
