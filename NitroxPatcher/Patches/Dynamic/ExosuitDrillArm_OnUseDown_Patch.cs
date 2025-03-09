508df5a21885f728ad8adf5eb331cdd22e427e02
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-03-09 15:46:57 -0400

    Fix sync for drill arms

--- NitroxPatcher/Patches/Dynamic/ExosuitDrillArm_OnUseDown_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/ExosuitDrillArm_OnUseDown_Patch.cs
@@ -7,7 +7,7 @@ namespace NitroxPatcher.Patches.Dynamic;
 
 public sealed partial class ExosuitDrillArm_OnUseDown_Patch : NitroxPatch, IDynamicPatch
 {
-    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((ExosuitTorpedoArm t) => ((IExosuitArm)t).OnUseDown(out Reflect.Ref<float>.Field));
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((ExosuitDrillArm t) => ((IExosuitArm)t).OnUseDown(out Reflect.Ref<float>.Field));
 
     public static void Prefix(ExosuitDrillArm __instance)
     {
