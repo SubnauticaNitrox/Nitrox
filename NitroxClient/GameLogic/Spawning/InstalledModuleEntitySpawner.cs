d3d6dc44cff2385b66189b46588554854d03eeb4
LeanHydra84 <guslindell2@gmail.com>
2025-04-27 07:30:01 -0500

    Fixed issue with power cells and batteries being removed from chargers on resync

--- NitroxClient/GameLogic/Spawning/InstalledModuleEntitySpawner.cs
+++ NitroxClient/GameLogic/Spawning/InstalledModuleEntitySpawner.cs
@@ -80,16 +80,7 @@ public class InstalledModuleEntitySpawner : SyncEntitySpawner<InstalledModuleEnt
         Pickupable pickupable = gameObject.RequireComponent<Pickupable>();
         pickupable.Initialize();
 
-        InventoryItem inventoryItem = new(pickupable)
-        {
-            container = equipment
-        };
-        inventoryItem.item.Reparent(equipment.tr);
-
-        equipment.equipment[entity.Slot] = inventoryItem;
-
-        equipment.UpdateCount(pickupable.GetTechType(), true);
-        Equipment.SendEquipmentEvent(pickupable, 0, parentObject, entity.Slot);
-        equipment.NotifyEquip(entity.Slot, inventoryItem);
+        InventoryItem inventoryItem = new(pickupable);
+        equipment.AddItem(entity.Slot, inventoryItem, true);
     }
 }
