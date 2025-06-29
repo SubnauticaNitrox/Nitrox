a2cabdb74d5c67d4091bb745d35fdf4ca19320e2
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-06-29 10:51:26 -0500

    Temporarily disable PlayerCinematicController patches

--- NitroxPatcher/Patches/Dynamic/PlayerCinematicController_Start_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/PlayerCinematicController_Start_Patch.cs
@@ -1,3 +1,6 @@
+// Disabled because these patches cause certain animations to break (such as https://github.com/SubnauticaNitrox/Nitrox/issues/2287)
+// TODO: reenable after the 1.8 release and fix animations
+#if false
 using System.Reflection;
 using NitroxClient.MonoBehaviours;
 using NitroxClient.MonoBehaviours.CinematicController;
@@ -26,3 +29,4 @@ public sealed partial class PlayerCinematicController_Start_Patch : NitroxPatch,
         entity.gameObject.EnsureComponent<MultiplayerCinematicReference>().AddController(__instance);
     }
 }
+#endif
