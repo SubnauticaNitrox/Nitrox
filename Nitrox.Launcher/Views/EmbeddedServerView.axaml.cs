8f538970d66a960389f91ad6e95841373e0ff960
NinjaPedroX <superninjapedro1@gmail.com>
2025-07-16 19:56:19 -0500

    Improved horizontal scroll in ImbeddedServerView
    Because of the virtualization of ItemsControl, the horizontal scroll bar would always be changing its width as log lines came in and out of virtualization. Now, the scrollviewer will remember its maximum width and set its MinWidth property to that value, which gives the illusion of the list not being virtualized.
    
    The only thing this doesn't change its minimum width for is when Auto Scroll is turned off and new lines of output that are longer than the min width are not yet brought into view. But in that case, once the user scrolls to that line, the min width value will update properly.

--- Nitrox.Launcher/Views/EmbeddedServerView.axaml.cs
+++ Nitrox.Launcher/Views/EmbeddedServerView.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Nitrox.Launcher.ViewModels;
+﻿using Avalonia.Controls;
+using Nitrox.Launcher.ViewModels;
 using Nitrox.Launcher.Views.Abstract;
 
 namespace Nitrox.Launcher.Views;
@@ -8,5 +9,13 @@ internal partial class EmbeddedServerView : RoutableViewBase<EmbeddedServerViewM
     public EmbeddedServerView()
     {
         InitializeComponent();
+    }
+
+    private void ItemsControl_OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        if (e.WidthChanged)
+        {
+            ItemsControl.MinWidth = ItemsControl.Bounds.Width;
+        }
     }
 }
