b5104faf236e59d35b9b862bb264d2851b96d3ce
Christian Medina <Christian.Medina.ATX@gmail.com>
2025-07-29 14:56:44 -0700

    Add broadcast on time capsule destruction

---
+++ NitroxPatcher/Patches/Dynamic/TimeCapsule_Open_Patch.cs
@@ -0,0 +1,27 @@
+﻿using NitroxModel.Packets;
+using System.Reflection;
+using NitroxClient.Communication.Abstract;
+using NitroxModel.DataStructures;
+using NitroxModel.Helper;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+/// <summary>
+/// Ensures that when a player opens a time capsule, it is permanently destroyed on the server,
+/// preventing it from respawning.
+/// </summary>
+public sealed partial class TimeCapsule_Open_Patch : NitroxPatch, IDynamicPatch
+{
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((TimeCapsule t) => t.Open());
+    
+    public static void Postfix(TimeCapsule __instance)
+    {
+        if (!__instance.TryGetIdOrWarn(out NitroxId timeCapsuleId))
+        {
+            return;
+        }
+        
+        var packet = new EntityDestroyed(timeCapsuleId);
+        Resolve<IPacketSender>().Send(packet);
+    }
+}
