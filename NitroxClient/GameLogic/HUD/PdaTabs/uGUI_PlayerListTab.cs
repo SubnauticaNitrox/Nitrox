c9a3c431be6279a6b0cf173818f145945cb0e895
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-08-12 20:37:24 -0500

    Directly pass sprite to PDA tab manager

--- NitroxClient/GameLogic/HUD/PdaTabs/uGUI_PlayerListTab.cs
+++ NitroxClient/GameLogic/HUD/PdaTabs/uGUI_PlayerListTab.cs
@@ -75,7 +75,7 @@ public class uGUI_PlayerListTab : uGUI_PingTab
             {
                 if (asset.name.Equals("player_list_tab@3x"))
                 {
-                    nitroxPDATabManager.AddTabSprite(asset.name, Sprite.Create(sprite.texture, sprite.rect, sprite.pivot, sprite.pixelsPerUnit));
+                    nitroxPDATabManager.AddTabSprite(asset.name, sprite);
                 }
                 assets.Add(asset.name, sprite);
             }
