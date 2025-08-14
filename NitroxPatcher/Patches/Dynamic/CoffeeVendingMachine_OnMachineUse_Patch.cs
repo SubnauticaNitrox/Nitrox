bd5b873ac35408e8594019c9ae58e644e4a99e1c
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-08-14 10:38:57 -0400

    Update NitroxPatcher/Patches/Dynamic/CoffeeVendingMachine_OnMachineUse_Patch.cs
    Co-authored-by: Jannify <23176718+Jannify@users.noreply.github.com>

--- NitroxPatcher/Patches/Dynamic/CoffeeVendingMachine_OnMachineUse_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/CoffeeVendingMachine_OnMachineUse_Patch.cs
@@ -15,7 +15,7 @@ public sealed partial class CoffeeVendingMachine_OnMachineUse_Patch : NitroxPatc
     public static void Postfix(CoffeeVendingMachine __instance)
     {
         NitroxId machineId = __instance.RequireComponent<NitroxEntity>().Id;
-        CoffeeMachineUse packet = new CoffeeMachineUse(machineId, __instance.waterSoundSlot1.playing ? CoffeeMachineSlot.ONE : CoffeeMachineSlot.TWO);
-        Resolve<IPacketSender>().Send(packet);
+        CoffeeMachineSlot slot = __instance.waterSoundSlot1.playing ? CoffeeMachineSlot.ONE : CoffeeMachineSlot.TWO;
+        Resolve<IPacketSender>().Send(new CoffeeMachineUse(machineId, slot);
     }
 }
