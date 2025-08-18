b5b994164ab0189e2c35c4b1e5125e7d1db6a296
Measurity <1107063+Measurity@users.noreply.github.com>
2025-08-18 22:12:58 +0200

    Added docs to ScreenshotManager_Combine_Patch

--- NitroxPatcher/Patches/Persistent/ScreenshotManager_Combine_Patch.cs
+++ NitroxPatcher/Patches/Persistent/ScreenshotManager_Combine_Patch.cs
@@ -3,16 +3,19 @@ using System.Reflection;
 
 namespace NitroxPatcher.Patches.Persistent;
 
+/// <summary>
+///     Changes Subnautica screenshot save location to <see cref="NitroxUser.ScreenshotsPath" />.
+/// </summary>
 public partial class ScreenshotManager_Combine_Patch : NitroxPatch, IPersistentPatch
 {
     private static readonly MethodInfo TARGET_METHOD = Reflect.Method(() => ScreenshotManager.Combine(default(string), default(string)));
 
     public static bool Prefix(ref string? __result, string path1, string path2)
     {
-        __result = (path1, path2) switch
+        __result = path2 switch
         {
-            (_, ScreenshotManager.screenshotsFolderName) => NitroxUser.ScreenshotsPath,
-            (_, {} filename) when path1 == ScreenshotManager.savePath => Path.Combine(NitroxUser.ScreenshotsPath, Path.GetFileName(filename)),
+            ScreenshotManager.screenshotsFolderName => NitroxUser.ScreenshotsPath,
+            { } filename when path1 == ScreenshotManager.savePath => Path.Combine(NitroxUser.ScreenshotsPath, Path.GetFileName(filename)),
             _ => null
         };
         return __result == null; // Run original method if we didn't handle (result is null).
