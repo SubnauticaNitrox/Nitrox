8999fa62b03c1d75bf821b7c89d12e0312d62c16
rootcan <24827220+tornac1234@users.noreply.github.com>
2025-09-21 00:40:01 +0200

    Broaden battery sync to any charge modification

---
+++ NitroxPatcher/Patches/Dynamic/Battery_charge_set_Patch.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Reflection;
+using NitroxClient.GameLogic;
+using NitroxModel.DataStructures;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+public sealed partial class Battery_charge_set_Patch : NitroxPatch, IDynamicPatch
+{
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Property((Battery t) => t.charge).SetMethod;
+
+    public static void Prefix(Battery __instance, float value)
+    {
+        // Broadcast update only once per integer change
+        if (Math.Abs(Math.Floor(__instance.charge) - Math.Floor(value)) > 0.0 &&
+            __instance.TryGetIdOrWarn(out NitroxId id))
+        {
+            Resolve<Entities>().EntityMetadataChanged(__instance, id);
+        }
+    }
+}
