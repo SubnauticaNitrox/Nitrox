eb3e33d8ad3e756301d3ceb74120b7c484ccadd9
Measurity <1107063+Measurity@users.noreply.github.com>
2025-10-12 05:52:26 +0200

    Added save file upgrade targeting 1.8.0.1 to handle namespace changes

--- Nitrox.Server.Subnautica/Models/Serialization/SaveDataUpgrades/SaveDataUpgrade.cs
+++ Nitrox.Server.Subnautica/Models/Serialization/SaveDataUpgrades/SaveDataUpgrade.cs
@@ -5,46 +5,88 @@ using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using Nitrox.Server.Subnautica.Models.Serialization.Json;
 
-namespace Nitrox.Server.Subnautica.Models.Serialization.SaveDataUpgrades
+namespace Nitrox.Server.Subnautica.Models.Serialization.SaveDataUpgrades;
+
+public abstract class SaveDataUpgrade
 {
-    public abstract class SaveDataUpgrade
+    private static readonly JsonConverter[] converters = [new NitroxIdConverter(), new TechTypeConverter(), new VersionConverter(), new KeyValuePairConverter(), new StringEnumConverter()];
+
+    /// <summary>
+    ///     The version the save files will have after the upgrade is done.
+    /// </summary>
+    public abstract Version TargetVersion { get; }
+
+    public void UpgradeSaveFiles(string saveDir, string fileEnding)
     {
-        private readonly JsonConverter[] converters = { new NitroxIdConverter(), new TechTypeConverter(), new VersionConverter(), new KeyValuePairConverter(), new StringEnumConverter() };
+        Log.Info($"┌── Executing {GetType().Name}");
+        string baseDataPath = Path.Combine(saveDir, $"BaseData{fileEnding}");
+        string playerDataPath = Path.Combine(saveDir, $"PlayerData{fileEnding}");
+        string worldDataPath = Path.Combine(saveDir, $"WorldData{fileEnding}");
+        string entityDataPath = Path.Combine(saveDir, $"EntityData{fileEnding}");
+        string globalRootDataPath = Path.Combine(saveDir, $"GlobalRootData{fileEnding}");
 
-        public abstract Version TargetVersion { get; }
+        Log.Info("├── Parsing raw json");
+        JObject baseData = TryParseFile(baseDataPath);
+        JObject playerData = TryParseFile(playerDataPath);
+        JObject worldData = TryParseFile(worldDataPath);
+        JObject entityData = TryParseFile(entityDataPath);
+        JObject globalRootData = TryParseFile(globalRootDataPath);
 
-        public static readonly Version MinimumSaveVersion = new(1, 8, 0, 0);
-        
-        public void UpgradeSaveFiles(string saveDir, string fileEnding)
+        Log.Info("├── Applying upgrade scripts");
+        if (baseData != null)
         {
-            Log.Info($"┌── Executing {GetType().Name}");
-            string baseDataPath = Path.Combine(saveDir, $"BaseData{fileEnding}");
-            string playerDataPath = Path.Combine(saveDir, $"PlayerData{fileEnding}");
-            string worldDataPath = Path.Combine(saveDir, $"WorldData{fileEnding}");
-            string entityDataPath = Path.Combine(saveDir, $"EntityData{fileEnding}");
-
-            Log.Info("├── Parsing raw json");
-            JObject baseData = JObject.Parse(File.ReadAllText(baseDataPath));
-            JObject playerData = JObject.Parse(File.ReadAllText(playerDataPath));
-            JObject worldData = JObject.Parse(File.ReadAllText(worldDataPath));
-            JObject entityData = JObject.Parse(File.ReadAllText(entityDataPath));
-
-            Log.Info("├── Applying upgrade scripts");
             UpgradeBaseData(baseData);
+        }
+        if (playerData != null)
+        {
             UpgradePlayerData(playerData);
+        }
+        if (worldData != null)
+        {
             UpgradeWorldData(worldData);
+        }
+        if (entityData != null)
+        {
             UpgradeEntityData(entityData);
-
-            Log.Info("└── Saving to disk");
-            File.WriteAllText(baseDataPath, baseData.ToString(Formatting.None, converters));
-            File.WriteAllText(playerDataPath, playerData.ToString(Formatting.None, converters));
-            File.WriteAllText(worldDataPath, worldData.ToString(Formatting.None, converters));
-            File.WriteAllText(entityDataPath, entityData.ToString(Formatting.None, converters));
+        }
+        if (globalRootData != null)
+        {
+            UpgradeGlobalRootData(globalRootData);
         }
 
-        protected virtual void UpgradeBaseData(JObject data) { }
-        protected virtual void UpgradePlayerData(JObject data) { }
-        protected virtual void UpgradeWorldData(JObject data) { }
-        protected virtual void UpgradeEntityData(JObject data) { }
+        Log.Info("└── Saving to disk");
+        WriteJObjectIfNotNull(baseDataPath, baseData);
+        WriteJObjectIfNotNull(playerDataPath, playerData);
+        WriteJObjectIfNotNull(worldDataPath, worldData);
+        WriteJObjectIfNotNull(entityDataPath, entityData);
+        WriteJObjectIfNotNull(globalRootDataPath, globalRootData);
+    }
+
+    protected virtual void UpgradeBaseData(JObject data) { }
+    protected virtual void UpgradePlayerData(JObject data) { }
+    protected virtual void UpgradeWorldData(JObject data) { }
+    protected virtual void UpgradeEntityData(JObject data) { }
+    protected virtual void UpgradeGlobalRootData(JObject data) { }
+
+    private static JObject? TryParseFile(string filePath)
+    {
+        try
+        {
+            return JObject.Parse(File.ReadAllText(filePath));
+        }
+        catch (IOException)
+        {
+            return null;
+        }
+    }
+
+    private static void WriteJObjectIfNotNull(string filePath, JObject? obj)
+    {
+        string content = obj?.ToString(Formatting.None, converters);
+        if (content == null)
+        {
+            return;
+        }
+        File.WriteAllText(filePath, content);
     }
 }
