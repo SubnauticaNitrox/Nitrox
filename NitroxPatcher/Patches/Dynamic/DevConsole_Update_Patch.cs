e923bdc01c4b6903f9ea902089a248acf79ca67f
NinjaPedroX <pedro.gonzalez.nasa@gmail.com>
2025-10-18 14:06:14 -0500

    Allowed pressing Enter key for Dev Console
    Allowed for the pressing of the Enter/Return key to open the dev console as long as the player isn't focused on the Nitrox chat input.

--- NitroxPatcher/Patches/Dynamic/DevConsole_Update_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/DevConsole_Update_Patch.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using HarmonyLib;
 using NitroxPatcher.PatternMatching;
@@ -8,7 +10,7 @@ using static System.Reflection.Emit.OpCodes;
 namespace NitroxPatcher.Patches.Dynamic;
 
 /// <summary>
-///     Keeps DevConsole disabled when enter is pressed.
+///     Keeps DevConsole disabled when enter is pressed while the Nitrox chat input is selected.
 /// </summary>
 public sealed partial class DevConsole_Update_Patch : NitroxPatch, IDynamicPatch
 {
@@ -26,8 +28,35 @@ public sealed partial class DevConsole_Update_Patch : NitroxPatch, IDynamicPatch
 
     public static readonly MethodInfo TARGET_METHOD = Reflect.Method((DevConsole t) => t.Update());
 
+    private static readonly MethodInfo? shouldEnableConsoleMethod = typeof(DevConsole_Update_Patch).GetMethod(nameof(ShouldEnableConsole), BindingFlags.NonPublic | BindingFlags.Static);
+
     public static IEnumerable<CodeInstruction> Transpiler(MethodBase original, IEnumerable<CodeInstruction> instructions)
     {
-        return instructions.ChangeAtMarker(devConsoleSetStateTruePattern, "ConsoleEnableFlag", i => i.opcode = Ldc_I4_0);
+        return instructions.ChangeAtMarker(devConsoleSetStateTruePattern, "ConsoleEnableFlag", i =>
+        {
+            i.opcode = Call;
+            i.operand = shouldEnableConsoleMethod;
+        });
+    }
+    
+    private static bool ShouldEnableConsole()
+    {
+        try
+        {
+            Type pcmType = typeof(NitroxClient.GameLogic.ChatUI.PlayerChatManager);
+            object instance = pcmType.GetField("Instance", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)?.GetValue(null);
+            PropertyInfo? isChatSelectedProp = pcmType.GetProperty("IsChatSelected", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            object val = isChatSelectedProp?.GetValue(instance);
+            if (val is false)
+            {
+                return true;
+            }
+        }
+        catch
+        {
+            // ignore
+        }
+
+        return false;
     }
 }
