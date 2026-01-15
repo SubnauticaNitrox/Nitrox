12dd20e463420b9163babbe22c7ad4bd38d7601b
Measurity <1107063+Measurity@users.noreply.github.com>
2026-01-15 18:22:38 +0100

    Removed left-over debug log call in EscapePodChangedPacketProcessor

--- Nitrox.Server.Subnautica/Models/Packets/Processors/EscapePodChangedPacketProcessor.cs
+++ Nitrox.Server.Subnautica/Models/Packets/Processors/EscapePodChangedPacketProcessor.cs
@@ -1,24 +1,16 @@
 ﻿using Nitrox.Server.Subnautica.Models.Packets.Processors.Core;
 using Nitrox.Server.Subnautica.Models.GameLogic;
 
-namespace Nitrox.Server.Subnautica.Models.Packets.Processors
+namespace Nitrox.Server.Subnautica.Models.Packets.Processors;
+
+internal sealed class EscapePodChangedPacketProcessor(PlayerManager playerManager, ILogger<EscapePodChangedPacketProcessor> logger) : AuthenticatedPacketProcessor<EscapePodChanged>
 {
-    internal sealed class EscapePodChangedPacketProcessor : AuthenticatedPacketProcessor<EscapePodChanged>
+    private readonly PlayerManager playerManager = playerManager;
+    private readonly ILogger<EscapePodChangedPacketProcessor> logger = logger;
+
+    public override void Process(EscapePodChanged packet, Player player)
     {
-        private readonly PlayerManager playerManager;
-        private readonly ILogger<EscapePodChangedPacketProcessor> logger;
-
-        public EscapePodChangedPacketProcessor(PlayerManager playerManager, ILogger<EscapePodChangedPacketProcessor> logger)
-        {
-            this.playerManager = playerManager;
-            this.logger = logger;
-        }
-
-        public override void Process(EscapePodChanged packet, Player player)
-        {
-            logger.ZLogDebug($"{packet}");
-            player.SubRootId = packet.EscapePodId;
-            playerManager.SendPacketToOtherPlayers(packet, player);
-        }
+        player.SubRootId = packet.EscapePodId;
+        playerManager.SendPacketToOtherPlayers(packet, player);
     }
 }
