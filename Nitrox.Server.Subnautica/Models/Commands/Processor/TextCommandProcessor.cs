265c129378960cf01cfede0ceb384081578e2cf0
Measurity <1107063+Measurity@users.noreply.github.com>
2026-01-15 23:08:43 +0100

    Used file-scoped namespace in TextCommandProcessor

--- Nitrox.Server.Subnautica/Models/Commands/Processor/TextCommandProcessor.cs
+++ Nitrox.Server.Subnautica/Models/Commands/Processor/TextCommandProcessor.cs
@@ -4,76 +4,75 @@ using Nitrox.Model.DataStructures.GameLogic;
 using Nitrox.Server.Subnautica.Models.Commands.Abstract;
 using Nitrox.Server.Subnautica.Models.Exceptions;
 
-namespace Nitrox.Server.Subnautica.Models.Commands.Processor
+namespace Nitrox.Server.Subnautica.Models.Commands.Processor;
+
+public class TextCommandProcessor
 {
-    public class TextCommandProcessor
+    private readonly ILogger<TextCommandProcessor> logger;
+    private readonly Dictionary<string, Command> commands = new();
+    private readonly char[] splitChar = [' '];
+
+    public TextCommandProcessor(IEnumerable<Command> cmds, ILogger<TextCommandProcessor> logger)
     {
-        private readonly ILogger<TextCommandProcessor> logger;
-        private readonly Dictionary<string, Command> commands = new();
-        private readonly char[] splitChar = [' '];
-
-        public TextCommandProcessor(IEnumerable<Command> cmds, ILogger<TextCommandProcessor> logger)
+        this.logger = logger;
+        foreach (Command cmd in cmds)
         {
-            this.logger = logger;
-            foreach (Command cmd in cmds)
+            if (!commands.TryAdd(cmd.Name, cmd))
             {
-                if (!commands.TryAdd(cmd.Name, cmd))
-                {
-                    throw new DuplicateRegistrationException($"Command {cmd.Name} is registered multiple times.");
-                }
-
-                foreach (string alias in cmd.Aliases)
-                {
-                    if (!commands.TryAdd(alias, cmd))
-                    {
-                        throw new DuplicateRegistrationException($"Command {alias} is registered multiple times.");
-                    }
-                }
+                throw new DuplicateRegistrationException($"Command {cmd.Name} is registered multiple times.");
             }
-            if (commands.Count < 1)
+
+            foreach (string alias in cmd.Aliases)
             {
-                logger.ZLogWarning($"No commands registered");
+                if (!commands.TryAdd(alias, cmd))
+                {
+                    throw new DuplicateRegistrationException($"Command {alias} is registered multiple times.");
+                }
             }
         }
-
-        public void ProcessCommand(string msg, Optional<Player> sender, Perms permissions)
+        if (commands.Count < 1)
         {
-            if (string.IsNullOrWhiteSpace(msg))
-            {
-                return;
-            }
-            Span<string> parts = msg.Split(splitChar, StringSplitOptions.RemoveEmptyEntries);
-            if (!commands.TryGetValue(parts[0], out Command command))
-            {
-                Command.SendMessage(sender, $"Command not found: {parts[0]}");
-                return;
-            }
-            if (!sender.HasValue && command.Flags.HasFlag(PermsFlag.NO_CONSOLE))
-            {
-                logger.ZLogError($"This command cannot be used by CONSOLE");
-                return;
-            }
+            logger.ZLogWarning($"No commands registered");
+        }
+    }
 
-            if (command.CanExecute(permissions))
-            {
-                try
-                {
-                    command.TryExecute(sender, parts[1..]);
+    public void ProcessCommand(string msg, Optional<Player> sender, Perms permissions)
+    {
+        if (string.IsNullOrWhiteSpace(msg))
+        {
+            return;
+        }
+        Span<string> parts = msg.Split(splitChar, StringSplitOptions.RemoveEmptyEntries);
+        if (!commands.TryGetValue(parts[0], out Command command))
+        {
+            Command.SendMessage(sender, $"Command not found: {parts[0]}");
+            return;
+        }
+        if (!sender.HasValue && command.Flags.HasFlag(PermsFlag.NO_CONSOLE))
+        {
+            logger.ZLogError($"This command cannot be used by CONSOLE");
+            return;
+        }
 
-                }
-                catch (ArgumentException ex)
-                {
-                    Command.SendMessage(sender, $"Error: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    logger.ZLogError(ex, $"Fatal error while trying to execute the command");
-                }
-            }
-            else
+        if (command.CanExecute(permissions))
+        {
+            try
             {
-                Command.SendMessage(sender, "You do not have the required permissions for this command !");
+                command.TryExecute(sender, parts[1..]);
+
             }
+            catch (ArgumentException ex)
+            {
+                Command.SendMessage(sender, $"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                logger.ZLogError(ex, $"Fatal error while trying to execute the command");
+            }
+        }
+        else
+        {
+            Command.SendMessage(sender, "You do not have the required permissions for this command !");
         }
     }
 }
