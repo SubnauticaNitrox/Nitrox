a215c36f862eb994f81399ae3d34fa2b1c7b10b5
Measurity <1107063+Measurity@users.noreply.github.com>
2026-01-22 16:40:01 +0100

    Removed a packet unit test that didn't do anything useful

--- Nitrox.Test/Model/Packets/Processors/PacketProcessorTest.cs
+++ Nitrox.Test/Model/Packets/Processors/PacketProcessorTest.cs
@@ -2,11 +2,11 @@ using Nitrox.Model.Core;
 using Nitrox.Model.Packets.Core;
 using Nitrox.Model.Packets.Processors.Abstract;
 using Nitrox.Model.Platforms.Discovery;
+using Nitrox.Server.Subnautica.Models.Packets.Core;
+using Nitrox.Server.Subnautica.Models.Packets.Processors;
 using Nitrox.Test;
 using NitroxClient;
-using NitroxClient.Communication.Packets.Processors.Abstract;
-using Nitrox.Server.Subnautica.Models.Packets.Processors;
-using Nitrox.Server.Subnautica.Models.Packets.Processors.Core;
+using NitroxClient.Communication.Packets.Processors.Core;
 
 namespace Nitrox.Model.Packets.Processors
 {
@@ -16,58 +16,43 @@ namespace Nitrox.Model.Packets.Processors
         [TestMethod]
         public void ClientPacketProcessorSanity()
         {
-            typeof(ClientPacketProcessor<>).Assembly.GetTypes()
-                               .Where(p => typeof(PacketProcessor).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
-                               .ToList()
-                               .ForEach(processor =>
-                               {
-                                   // Make sure that each packet-processor is derived from the ClientPacketProcessor class,
-                                   //  so that it's packet-type can be determined.
-                                   Assert.IsNotNull(processor.BaseType, $"{processor} does not derive from any type!");
-                                   Assert.IsTrue(processor.BaseType.IsGenericType, $"{processor} does not derive from a generic type!");
-                                   Assert.IsTrue(processor.BaseType.IsAssignableToGenericType(typeof(ClientPacketProcessor<>)), $"{processor} does not derive from ClientPacketProcessor!");
+            typeof(IClientPacketProcessor<>).Assembly.GetTypes()
+                                            .Where(p => typeof(PacketProcessor).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
+                                            .ToList()
+                                            .ForEach(processor =>
+                                            {
+                                                // Make sure that each packet-processor is derived from the ClientPacketProcessor class,
+                                                //  so that it's packet-type can be determined.
+                                                Assert.IsNotNull(processor.BaseType, $"{processor} does not derive from any type!");
+                                                Assert.IsTrue(processor.BaseType.IsGenericType, $"{processor} does not derive from a generic type!");
+                                                Assert.IsTrue(processor.BaseType.IsAssignableToGenericType(typeof(IClientPacketProcessor<>)), $"{processor} does not derive from ClientPacketProcessor!");
 
-                                   // Check constructor availability:
-                                   int numCtors = processor.GetConstructors().Length;
-                                   Assert.IsTrue(numCtors == 1, $"{processor} should have exactly 1 constructor! (has {numCtors})");
-                               });
+                                                // Check constructor availability:
+                                                int numCtors = processor.GetConstructors().Length;
+                                                Assert.IsTrue(numCtors == 1, $"{processor} should have exactly 1 constructor! (has {numCtors})");
+                                            });
         }
 
         [TestMethod]
         public void ServerPacketProcessorSanity()
         {
             typeof(PacketProcessorsInvoker).Assembly.GetTypes()
-                                 .Where(p => typeof(PacketProcessor).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
-                                 .ToList()
-                                 .ForEach(processor =>
-                                 {
-                                     // Make sure that each packet-processor is derived from the ClientPacketProcessor class,
-                                     //  so that it's packet-type can be determined.
-                                     Assert.IsNotNull(processor.BaseType, $"{processor} does not derive from any type!");
-                                     Assert.IsTrue(processor.BaseType.IsGenericType, $"{processor} does not derive from a generic type!");
-                                     Assert.IsTrue(processor.BaseType.IsAssignableToGenericType(typeof(IPacketProcessor)), $"{processor} does not derive from (Un)AuthenticatedPacketProcessor!");
+                                           .Where(p => typeof(PacketProcessor).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
+                                           .ToList()
+                                           .ForEach(processor =>
+                                           {
+                                               // Make sure that each packet-processor is derived from the ClientPacketProcessor class,
+                                               //  so that it's packet-type can be determined.
+                                               Assert.IsNotNull(processor.BaseType, $"{processor} does not derive from any type!");
+                                               Assert.IsTrue(processor.BaseType.IsGenericType, $"{processor} does not derive from a generic type!");
+                                               Assert.IsTrue(processor.BaseType.IsAssignableToGenericType(typeof(IPacketProcessor)), $"{processor} does not derive from (Un)AuthenticatedPacketProcessor!");
 
-                                     // Check constructor availability:
-                                     int numCtors = processor.GetConstructors().Length;
-                                     Assert.IsTrue(numCtors == 1, $"{processor} should have exactly 1 constructor! (has {numCtors})");
+                                               // Check constructor availability:
+                                               int numCtors = processor.GetConstructors().Length;
+                                               Assert.IsTrue(numCtors == 1, $"{processor} should have exactly 1 constructor! (has {numCtors})");
 
-                                     // Unable to check parameters, these are defined in PacketHandler.ctor
-                                 });
-        }
-
-        [TestMethod]
-        public void SameAmountOfServerPacketProcessors()
-        {
-            IEnumerable<Type> processors = typeof(PacketProcessorsInvoker).Assembly.GetTypes()
-                                                                .Where(p => typeof(IPacketProcessor).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract);
-
-            List<Type> packetTypes = typeof(DefaultServerPacketProcessor).Assembly.GetTypes()
-                                                                         .Where(p => typeof(IPacketProcessor).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
-                                                                         .ToList();
-
-            int both = packetTypes.Count;
-            Assert.AreEqual(processors.Count(), both,
-                            $"Not all(Un) AuthenticatedPacketProcessors have been discovered by the runtime code (auth + unauth: {both} out of {processors.Count()}). Perhaps the runtime matching code is too strict, or a processor does not derive from ClientPacketProcessor (and will hence not be detected).");
+                                               // Unable to check parameters, these are defined in PacketHandler.ctor
+                                           });
         }
 
         [TestMethod]
@@ -79,13 +64,13 @@ namespace Nitrox.Model.Packets.Processors
 
             List<Type> abstractProcessorTypes = new();
 
-            abstractProcessorTypes.AddRange(typeof(ClientPacketProcessor<>)
+            abstractProcessorTypes.AddRange(typeof(IClientPacketProcessor<>)
                                             .Assembly.GetTypes()
-                                            .Where(p => p.IsClass && p.IsAbstract && p.IsAssignableToGenericType(typeof(ClientPacketProcessor<>))));
+                                            .Where(p => p.IsClass && p.IsAbstract && p.IsAssignableToGenericType(typeof(IClientPacketProcessor<>))));
 
-            abstractProcessorTypes.AddRange(typeof(AuthenticatedPacketProcessor<>)
+            abstractProcessorTypes.AddRange(typeof(IAuthPacketProcessor<>)
                                             .Assembly.GetTypes()
-                                            .Where(p => p.IsClass && p.IsAbstract && (p.IsAssignableToGenericType(typeof(AuthenticatedPacketProcessor<>)) || p.IsAssignableToGenericType(typeof(UnauthenticatedPacketProcessor<>)))));
+                                            .Where(p => p.IsClass && p.IsAbstract && (p.IsAssignableToGenericType(typeof(IAuthPacketProcessor<>)) || p.IsAssignableToGenericType(typeof(IAnonPacketProcessor<>)))));
 
             if (!GameInstallationFinder.FindGameCached(GameInfo.Subnautica))
             {
@@ -97,10 +82,10 @@ namespace Nitrox.Model.Packets.Processors
             foreach (Type packet in typeof(Packet).Assembly.GetTypes().Where(p => typeof(Packet).IsAssignableFrom(p) && p.IsClass && !p.IsAbstract).ToList())
             {
                 Assert.IsTrue(packetTypes.Contains(packet) || abstractProcessorTypes.Any(genericProcessor =>
-                {
-                    Type processorType = genericProcessor.MakeGenericType(packet);
-                    return NitroxServiceLocator.LocateOptionalService(processorType).HasValue;
-                }), $"Packet of type '{packet}' should have at least one processor.");
+                              {
+                                  Type processorType = genericProcessor.MakeGenericType(packet);
+                                  return NitroxServiceLocator.LocateOptionalService(processorType).HasValue;
+                              }), $"Packet of type '{packet}' should have at least one processor.");
             }
         }
 
