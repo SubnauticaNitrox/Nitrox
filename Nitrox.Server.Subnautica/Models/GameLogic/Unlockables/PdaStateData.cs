be6a10d9aa999c10f4b06b28e02791799e88297a
Measurity <1107063+Measurity@users.noreply.github.com>
2026-02-12 01:13:52 +0100

    Fixed thread safe issues with PdaStateData

--- Nitrox.Server.Subnautica/Models/GameLogic/Unlockables/PdaStateData.cs
+++ Nitrox.Server.Subnautica/Models/GameLogic/Unlockables/PdaStateData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Nitrox.Model.DataStructures;
 using Nitrox.Model.Subnautica.DataStructures.GameLogic;
@@ -7,8 +8,11 @@ namespace Nitrox.Server.Subnautica.Models.GameLogic.Unlockables;
 /// <summary>
 ///     Data container for everything Subnautica PDA related. Behavior should go in <see cref="PdaManager" />.
 /// </summary>
+/// <remarks>
+///     Important: Keep <see cref="GetFullCopy" /> in sync with any code changes!
+/// </remarks>
 [DataContract]
-internal sealed class PdaStateData
+internal sealed record PdaStateData
 {
     /// <summary>
     ///     Gets or sets the KnownTech construct which powers the popup shown to the user when a new TechType is discovered
@@ -17,22 +21,22 @@ internal sealed class PdaStateData
     ///     <see cref='KnownTechEntryAdd.EntryCategory.ANALYZED'>KnownTech.analyzedTech</see>
     /// </summary>
     [DataMember(Order = 1)]
-    public ThreadSafeList<NitroxTechType> KnownTechTypes { get; } = [];
+    public List<NitroxTechType> KnownTechTypes { get; } = [];
 
     [DataMember(Order = 2)]
-    public ThreadSafeList<NitroxTechType> AnalyzedTechTypes { get; } = [];
+    public List<NitroxTechType> AnalyzedTechTypes { get; } = [];
 
     /// <summary>
     ///     Gets or sets the log of story events present in the PDA
     /// </summary>
     [DataMember(Order = 3)]
-    public ThreadSafeList<PDALogEntry> PdaLog { get; } = [];
+    public List<PDALogEntry> PdaLog { get; } = [];
 
     /// <summary>
     ///     Gets or sets the entries that show up the the PDA's Encyclopedia
     /// </summary>
     [DataMember(Order = 4)]
-    public ThreadSafeList<string> EncyclopediaEntries { get; } = [];
+    public List<string> EncyclopediaEntries { get; } = [];
 
     /// <summary>
     ///     The ids of the already scanned entities.
@@ -43,17 +47,36 @@ internal sealed class PdaStateData
     ///     We can therefore use it as a list.
     /// </remarks>
     [DataMember(Order = 5)]
-    public ThreadSafeSet<NitroxId> ScannerFragments { get; } = [];
+    public HashSet<NitroxId> ScannerFragments { get; } = [];
 
     /// <summary>
     ///     Partially unlocked PDA entries (e.g. fragments)
     /// </summary>
     [DataMember(Order = 6)]
-    public ThreadSafeList<PDAEntry> ScannerPartial { get; } = [];
+    public List<PDAEntry> ScannerPartial { get; } = [];
 
     /// <summary>
     ///     Fully unlocked PDA entries
     /// </summary>
     [DataMember(Order = 7)]
-    public ThreadSafeList<NitroxTechType> ScannerComplete { get; } = [];
+    public List<NitroxTechType> ScannerComplete { get; } = [];
+
+    /// <summary>
+    ///     Gets a full copy of the entire <see cref="PdaStateData" /> which allows for thread safe access.
+    /// </summary>
+    public PdaStateData GetFullCopy()
+    {
+        PdaStateData copy = new();
+        copy.KnownTechTypes.AddRange(KnownTechTypes);
+        copy.AnalyzedTechTypes.AddRange(AnalyzedTechTypes);
+        copy.PdaLog.AddRange(PdaLog);
+        copy.EncyclopediaEntries.AddRange(EncyclopediaEntries);
+        foreach (NitroxId fragment in ScannerFragments)
+        {
+            copy.ScannerFragments.Add(fragment);
+        }
+        copy.ScannerPartial.AddRange(ScannerPartial);
+        copy.ScannerComplete.AddRange(ScannerComplete);
+        return copy;
+    }
 }
