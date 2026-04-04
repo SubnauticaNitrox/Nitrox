a1a06cedaad96fb330151740e89dfab8fbba3ba5
dartasen <10561268+dartasen@users.noreply.github.com>
2026-04-04 14:24:41 +0200

    Fix Query Command & Improve NitroxId

--- Nitrox.Server.Subnautica/Models/Commands/Debugging/QueryCommand.cs
+++ Nitrox.Server.Subnautica/Models/Commands/Debugging/QueryCommand.cs
@@ -1,5 +1,5 @@
-#if DEBUG
 using System.ComponentModel;
+using System.Text;
 using Nitrox.Model.DataStructures;
 using Nitrox.Model.DataStructures.GameLogic;
 using Nitrox.Model.Subnautica.DataStructures.GameLogic;
@@ -10,7 +10,7 @@ using Nitrox.Server.Subnautica.Models.GameLogic.Entities;
 
 namespace Nitrox.Server.Subnautica.Models.Commands.Debugging;
 
-[RequiresPermission(Perms.HOST)]
+[RequiresPermission(Perms.ADMIN)]
 internal sealed class QueryCommand(EntityRegistry entityRegistry, SimulationOwnershipData simulationOwnershipData, ILogger<QueryCommand> logger) : ICommandHandler<NitroxId>
 {
     private readonly EntityRegistry entityRegistry = entityRegistry;
@@ -26,21 +26,36 @@ internal sealed class QueryCommand(EntityRegistry entityRegistry, SimulationOwne
             return Task.CompletedTask;
         }
 
-        logger.ZLogInformation($"{entity}");
-        if (entity is WorldEntity worldEntity and not GlobalRootEntity)
+        StringBuilder builder = new();
+        builder.AppendLine("Entity");
+        builder.AppendLine($" └ Type: {entity.GetType().Name}");
+        builder.AppendLine($" └ Id: {entity.Id}");
+        builder.AppendLine($" └ TechType: {entity.TechType}");
+        builder.AppendLine($" └ ParentId: {entity.ParentId?.ToString() ?? "none"}");
+        builder.AppendLine($" └ Children: {entity.ChildEntities.Count}");
+        builder.AppendLine($" └ Metadata: {entity.Metadata?.ToString() ?? "none"}");
+
+        if (entity is WorldEntity worldEntity)
         {
-            logger.ZLogInformation($"{worldEntity.AbsoluteEntityCell}");
+            builder.AppendLine("World");
+            builder.AppendLine($" └ ClassId: {worldEntity.ClassId}");
+            builder.AppendLine($" └ Level: {worldEntity.Level}");
+            builder.AppendLine($" └ SpawnedByServer: {worldEntity.SpawnedByServer}");
+            builder.AppendLine($" └ {worldEntity.Transform}");
+            builder.AppendLine($" └ Cell: {(worldEntity is GlobalRootEntity ? "global root" : worldEntity.AbsoluteEntityCell.ToString())}");
         }
-        if (simulationOwnershipData.TryGetLock(entityId, out SimulationOwnershipData.PlayerLock playerLock))
-        {
-            logger.ZLogInformation($"Lock owner: {playerLock.Player.Name}");
-        }
-        else
-        {
-            logger.ZLogInformation($"Not locked");
-        }
+
+        bool isLocked = simulationOwnershipData.TryGetLock(entityId, out SimulationOwnershipData.PlayerLock playerLock);
+
+        builder.AppendLine("Lock status");
+        builder.AppendLine($" └ Locked: {isLocked}");
+        builder.AppendLine($" └ Owner: {(isLocked ? playerLock.Player.Name : "none")}");
+
+        builder.AppendLine("Raw Data");
+        builder.AppendLine(entity.ToString());
+
+        logger.ZLogInformation($"{builder.ToString()}");
 
         return Task.CompletedTask;
     }
 }
-#endif
