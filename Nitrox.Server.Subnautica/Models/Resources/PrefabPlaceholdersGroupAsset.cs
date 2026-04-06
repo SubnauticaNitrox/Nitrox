6642654d901a53eaea02f24391b364a12c8e67b6
dartasen <10561268+dartasen@users.noreply.github.com>
2026-04-06 21:32:22 +0200

    Improve PrefabPlaceHolder cache handling (#2704)

--- Nitrox.Server.Subnautica/Models/Resources/PrefabPlaceholdersGroupAsset.cs
+++ Nitrox.Server.Subnautica/Models/Resources/PrefabPlaceholdersGroupAsset.cs
@@ -3,9 +3,12 @@ using Nitrox.Server.Subnautica.Models.Resources.Core;
 
 namespace Nitrox.Server.Subnautica.Models.Resources;
 
-
-[Serializable]
 /// <param name="PrefabAssets">
 /// All attached PrefabPlaceholders (and PrefabPlaceholdersGroup). Is in sync with PrefabPlaceholdersGroup.prefabPlaceholders
 /// </param>
-public record struct PrefabPlaceholdersGroupAsset(string ClassId, IPrefabAsset[] PrefabAssets, NitroxTransform Transform = null) : IPrefabAsset;
+[Serializable]
+public record struct PrefabPlaceholdersGroupAsset(
+    string ClassId,
+    IPrefabAsset[] PrefabAssets,
+    NitroxTransform? Transform = null
+) : IPrefabAsset;
