28ef596b16723767121a937654c76bf6a735b4f4
dartasen <10561268+dartasen@users.noreply.github.com>
2026-04-06 15:30:48 +0200

    Fix window not being draggable

--- Nitrox.Launcher/Models/Extensions/VisualExtensions.cs
+++ Nitrox.Launcher/Models/Extensions/VisualExtensions.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls;
+using Nitrox.Launcher.Models.Design;
 using Nitrox.Model.Platforms.OS.Windows;
 
 namespace Nitrox.Launcher.Models.Extensions;
@@ -28,6 +30,22 @@ public static class VisualExtensions
         }
 
         WindowsApi.EnableDefaultWindowAnimations(windowHandle.Value, window.CanResize);
+    }
+
+    public static void ApplyPlatformWindowChrome(this Window window)
+    {
+        if (OperatingSystem.IsLinux())
+        {
+            window.SystemDecorations = SystemDecorations.Full;
+            NitroxAttached.SetUseCustomTitleBar(window, false);
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            window.SystemDecorations = SystemDecorations.Full;
+            window.ExtendClientAreaToDecorationsHint = false;
+            window.ExtendClientAreaTitleBarHeightHint = -1;
+            NitroxAttached.SetUseCustomTitleBar(window, false);
+        }
     }
 
     public static Window? GetWindow(this Visual visual) => TopLevel.GetTopLevel(visual) as Window;
