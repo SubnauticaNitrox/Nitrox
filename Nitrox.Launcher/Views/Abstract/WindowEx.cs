ba56d9a91d7169e055a23f82804dfcc87bc8cb60
dartasen <10561268+dartasen@users.noreply.github.com>
2026-04-09 12:43:41 +0200

    Fix windows not being draggable (#2703)

--- Nitrox.Launcher/Views/Abstract/WindowEx.cs
+++ Nitrox.Launcher/Views/Abstract/WindowEx.cs
@@ -1,6 +1,4 @@
-﻿using System;
 using Avalonia.Controls;
-using Nitrox.Launcher.Models.Design;
 using Nitrox.Launcher.ViewModels.Abstract;
 
 namespace Nitrox.Launcher.Views.Abstract;
@@ -9,14 +7,6 @@ internal abstract class WindowEx<T> : Window where T : ViewModelBase
 {
     protected override void OnInitialized()
     {
-        this.ApplyOsWindowStyling();
-
-        // On Linux systems, Avalonia has trouble allowing windows to resize without "decorations". So we enable it in full, but hide the custom titlebar as it'll look bad.
-        // On macOS, we need the native toolbar as every app is using it
-        if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
-        {
-            SystemDecorations = SystemDecorations.Full;
-            NitroxAttached.SetUseCustomTitleBar(this, false);
-        }
+        this.ApplyPlatformWindowStyle();
     }
 }
