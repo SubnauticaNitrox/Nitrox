a33197826f8ea58e697384b477bff275e002d1c7
NinjaPedroX <superninjapedro1@gmail.com>
2026-05-06 16:21:49 -0500

    Fixed ManageServerView bug w/ seed capitalization

--- Nitrox.Launcher/ViewModels/ManageServerViewModel.cs
+++ Nitrox.Launcher/ViewModels/ManageServerViewModel.cs
@@ -197,7 +197,7 @@ internal partial class ManageServerViewModel : RoutableViewModelBase
                                   ServerIcon != Server.ServerIcon ||
                                   ServerPassword != Server.Password ||
                                   ServerGameMode != Server.GameMode ||
-                                  ServerSeed != Server.Seed ||
+                                  !string.Equals(ServerSeed, Server.Seed, StringComparison.OrdinalIgnoreCase) ||
                                   ServerDefaultPlayerPerm != Server.PlayerPermissions ||
                                   ServerAutoSaveInterval != Server.AutoSaveInterval ||
                                   ServerMaxPlayers != Server.MaxPlayers ||
