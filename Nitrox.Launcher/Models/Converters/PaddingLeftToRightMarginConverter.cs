5bcd48726038204b900edada7aafc007ad28ed79
NinjaPedroX <superninjapedro1@gmail.com>
2026-05-07 20:35:26 -0500

    Fixed broken RadioButton spacing for default style
    Seems a bit of a hacky way to do this, but I can't figure out a better way to make the RadioButton icon be equally spaced on both sides (or have a controllable width between the icon and the button content)

---
+++ Nitrox.Launcher/Models/Converters/PaddingLeftToRightMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+
+namespace Nitrox.Launcher.Models.Converters;
+
+/// <summary>
+///     Maps a padding's left value to the right side of a margin.
+/// </summary>
+public class PaddingLeftToRightMarginConverter : Converter<PaddingLeftToRightMarginConverter>
+{
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not Thickness padding)
+        {
+            return new Thickness();
+        }
+
+        return new Thickness(0, 0, padding.Left, 0);
+    }
+
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
