72c5e3ff42c0a6998009523d1ab675aa00e83d9f
Mark George <markgeorge0512@gmail.com>
2026-08-04 03:50:53 -0700

    Define the Scanner Room client lock abstraction

--- NitroxClient/GlobalUsings.cs
+++ NitroxClient/GlobalUsings.cs
@@ -4,3 +4,8 @@ global using Nitrox.Model.Extensions;
 global using Nitrox.Model.Logger;
 global using Nitrox.Model.Subnautica.Extensions;
 global using Task = System.Threading.Tasks.Task;
+#if NET
+global using LockObject = System.Threading.Lock;
+#else
+global using LockObject = object;
+#endif
