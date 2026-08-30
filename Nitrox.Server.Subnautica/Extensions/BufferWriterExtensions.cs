0469ad0d424b27ab1fef9ef1c682ccf5a97dc0a3
dartasen <cleeeemove@gmail.com>
2026-08-30 13:39:54 +0200

    Fix BufferWritter ArgumentOutOfRange

--- Nitrox.Server.Subnautica/Extensions/BufferWriterExtensions.cs
+++ Nitrox.Server.Subnautica/Extensions/BufferWriterExtensions.cs
@@ -5,30 +5,15 @@ namespace Nitrox.Server.Subnautica.Extensions;
 
 internal static class BufferWriterExtensions
 {
-    public static void Write(this IBufferWriter<byte> writer, ReadOnlySpan<char> utf16Value)
+    /// <summary>
+    /// Encodes the specified <see cref="ReadOnlySpan{Char}"/> to <see langword="byte"/> s using
+    /// <see cref="Encoding.UTF8"/> and writes the result to <paramref name="writer"/>.
+    /// </summary>
+    /// <param name="chars">The <see cref="ReadOnlySpan{Char}"/> to encode to <see langword="byte"/>s.</param>
+    /// <param name="writer">The buffer to which the encoded bytes will be written.</param>
+    /// <exception cref="EncoderFallbackException">Thrown if <paramref name="chars"/> contains data that cannot be encoded</exception>
+    public static void Write(this IBufferWriter<byte> writer, ReadOnlySpan<char> chars)
     {
-        const int MAX_STACK_ALLOC = 64;
-        Debug.Assert(MAX_STACK_ALLOC % 4 == 0);
-        int byteCount = Encoding.Default.GetByteCount(utf16Value);
-        Span<byte> buffer = stackalloc byte[int.Min(MAX_STACK_ALLOC, byteCount)];
-        if (Encoding.Default.TryGetBytes(utf16Value, buffer, out int written))
-        {
-            writer.Write(buffer[..written]);
-            return;
-        }
-
-        // Write in steps so we can keep the buffer in the stack.
-        const byte SEGMENT_CHAR_STEP = MAX_STACK_ALLOC / 4; // UTF16 can be 4 bytes per char.
-        int index = 0;
-        do
-        {
-            ReadOnlySpan<char> segment = utf16Value.Slice(index, int.Min(SEGMENT_CHAR_STEP, utf16Value.Length - index));
-            if (!Encoding.Default.TryGetBytes(segment, buffer, out written))
-            {
-                throw new InvalidOperationException("Buffer too small");
-            }
-            writer.Write(buffer.Slice(0, written));
-            index += written;
-        } while (written > 0);
+        EncodingExtensions.GetBytes(Encoding.UTF8, chars, writer);
     }
 }
