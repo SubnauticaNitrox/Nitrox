ff2e7669b15d81096a90d432ac65267a7bd51ad3
misterbubb <dillonmcm296@gmail.com>
2026-08-30 15:33:40 -0500

    Use RealTimeElapsed for flare energy calculations (#2870)
    Implement time-skip independent energy sync using TimeManager.RealTimeElapsed instead of DayNightCycle time. This makes flares immune to time skip commands (night, day) and provides accurate energy sync across clients.
    
    Co-authored-by: tornac1234

--- NitroxPatcher/Patches/Dynamic/Flare_Update_Patch.cs
+++ NitroxPatcher/Patches/Dynamic/Flare_Update_Patch.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using NitroxClient.GameLogic;
 using NitroxPatcher.Helper;
@@ -8,7 +9,8 @@ using UnityEngine;
 namespace NitroxPatcher.Patches.Dynamic;
 
 /// <summary>
-/// Replaces local use of <see cref="Time.deltaTime"/> by <see cref="TimeManager.DeltaTime"/>
+/// Replaces energy consumption calculation to use <see cref="TimeManager.RealTimeElapsed"/>
+/// instead of <see cref="Time.deltaTime"/> to make energy drain immune to time skips
 /// </summary>
 public sealed partial class Flare_Update_Patch : NitroxPatch, IDynamicPatch
 {
@@ -16,7 +18,37 @@ public sealed partial class Flare_Update_Patch : NitroxPatch, IDynamicPatch
 
     public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        return new CodeMatcher(instructions).ReplaceDeltaTime()
-                                            .InstructionEnumeration();
+        /*
+         * REPLACE:
+         * this.energyLeft = Mathf.Max(this.energyLeft - Time.deltaTime, 0f);
+         * 
+         * WITH:
+         * this.energyLeft = 1800f - ((float)Resolve<TimeManager>().RealTimeElapsed - this.flareActivateTime);
+         */
+
+        CodeMatcher matcher = new(instructions);
+
+        matcher.MatchEndForward([
+                   new CodeMatch(OpCodes.Ldarg_0),
+                   new CodeMatch(OpCodes.Ldfld, Reflect.Field((Flare t) => t.energyLeft)),
+                   new CodeMatch(OpCodes.Call, Reflect.Property(() => Time.deltaTime).GetGetMethod()),
+                   new CodeMatch(OpCodes.Sub),
+                   new CodeMatch(OpCodes.Ldc_R4, 0f),
+                   new CodeMatch(OpCodes.Call, Reflect.Method(() => UnityEngine.Mathf.Max(default(float), default(float))))
+               ])
+               .Advance(-5)
+               .RemoveInstructions(6)
+               .Insert([
+                   new CodeInstruction(OpCodes.Ldc_R4, 1800f), // Max energy
+                   new CodeInstruction(OpCodes.Call, Reflect.Method(() => Resolve<TimeManager>())),
+                   new CodeInstruction(OpCodes.Callvirt, Reflect.Property((TimeManager t) => t.RealTimeElapsed).GetGetMethod()),
+                   new CodeInstruction(OpCodes.Conv_R4),
+                   new CodeInstruction(OpCodes.Ldarg_0),
+                   new CodeInstruction(OpCodes.Ldfld, Reflect.Field((Flare t) => t.flareActivateTime)),
+                   new CodeInstruction(OpCodes.Sub),
+                   new CodeInstruction(OpCodes.Sub)
+               ]);
+
+        return matcher.InstructionEnumeration();
     }
 }
