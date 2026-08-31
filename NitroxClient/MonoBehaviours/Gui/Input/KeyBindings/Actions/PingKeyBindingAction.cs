1844c25fefae9d69af945da7788527bb402b70bb
Measurity <1107063+Measurity@users.noreply.github.com>
2026-08-31 17:32:54 +0200

    Changed PingKeyBindingAction type to internal

--- NitroxClient/MonoBehaviours/Gui/Input/KeyBindings/Actions/PingKeyBindingAction.cs
+++ NitroxClient/MonoBehaviours/Gui/Input/KeyBindings/Actions/PingKeyBindingAction.cs
@@ -1,9 +1,8 @@
-using NitroxClient.MonoBehaviours;
 using UnityEngine.InputSystem;
 
 namespace NitroxClient.MonoBehaviours.Gui.Input.KeyBindings.Actions;
 
-public class PingKeyBindingAction : KeyBinding
+internal sealed class PingKeyBindingAction : KeyBinding
 {
     public PingKeyBindingAction() : base("Nitrox_Settings_Keybind_Ping", "p") { }
 
